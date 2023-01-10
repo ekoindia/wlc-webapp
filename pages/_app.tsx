a34cf1bad473e3c0e4c83bac258a237c2b430c19
shubhmjain30 <shubhmjain30@gmail.com>
2023-01-10 15:53:08 +0530

    ChakraUI added with theme support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { light } from "../styles/themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<ChakraProvider theme={light}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
---
+++ styles/themes.tsx
@@ -0,0 +1,53 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const light = extendTheme({
+	colors: {
+		primary: {
+			light: "#ffad23",
+			DEFAULT: "#FE9F00",
+			dark: "#f38300",
+		},
+		accent: {
+			light: "#1d3ac9",
+			DEFAULT: "#11299E",
+			dark: "#0d2289",
+		},
+		secondary: {
+			light: "#2d6cbf",
+			DEFAULT: "#1F5AA7",
+			dark: "#164d94",
+		},
+		success: "#00C341",
+		error: "#FF4081",
+		divider: "#E9EDF1",
+		hint: "#D2D2D2",
+		dark: "#0F0F0F",
+		light: "#555555",
+	},
+});
+
+export const dark = extendTheme({
+	colors: {
+		primary: {
+			light: "#ffad23",
+			DEFAULT: "#FE9F00",
+			dark: "#f38300",
+		},
+		accent: {
+			light: "#1d3ac9",
+			DEFAULT: "#11299E",
+			dark: "#0d2289",
+		},
+		secondary: {
+			light: "#2d6cbf",
+			DEFAULT: "#1F5AA7",
+			dark: "#164d94",
+		},
+		success: "#00C341",
+		error: "#FF4081",
+		divider: "#E9EDF1",
+		hint: "#D2D2D2",
+		dark: "#0F0F0F",
+		light: "#555555",
+	},
+});
