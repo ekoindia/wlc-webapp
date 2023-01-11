7bdb644a3521c16dd009e7b61b6f11abc2c6dc55
DEEPAK6265 <deepaksharma934029@gmail.com>
2023-01-11 16:18:00 +0530

    Nav and Header done

---
+++ components/index.js
@@ -0,0 +1,13 @@
+/* PLOP_INJECT_IMPORT */
+import { Network } from "./Network";
+import { Layout } from "./Layout";
+import { Sidebar } from "./Sidebar";
+import { Navbar } from "./Navbar";
+
+export {
+	Network,
+	Layout,
+	Sidebar,
+	Navbar,
+	/* PLOP_INJECT_EXPORT */
+};
