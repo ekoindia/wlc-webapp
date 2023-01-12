05655d66b9515bb692c83fba9d3c5e30e9b9b7b3
Divxsh <guptadivash70@gmail.com>
2023-01-12 21:06:49 +0530

    Added : Input Component

---
+++ components/Input/Input.jsx
@@ -0,0 +1,75 @@
+import { Center, Flex, Input } from "@chakra-ui/react";
+import { InputLabel, InputMsg } from "../";
+
+const Inputs = ({
+	label,
+	id,
+	name,
+	placeholder,
+	description,
+	value,
+	type = "text",
+	disabled = false,
+	hidden = false,
+	invalid = false,
+	errorMsg = "",
+	onChange,
+	isNumInput = false,
+	labelStyle,
+	errorStyle,
+	inputContStyle,
+	inputNumStyle,
+	...props
+}) => {
+	// TODO: Edit state as required
+
+	return (
+		<Flex direction="column" {...props}>
+			{label ? <InputLabel {...labelStyle}>{label}</InputLabel> : null}
+			<Flex pos="relative" {...inputContStyle}>
+				<Input
+					borderColor={errorMsg && invalid ? "error" : "hint"}
+					name={name}
+					placeholder={placeholder}
+					type={type}
+					disabled={disabled}
+					hidden={hidden}
+					value={value}
+					w="100%"
+					pl={isNumInput ? "7.6rem" : ""}
+					height="100%"
+					_focus={{
+						bg: "focusbg",
+						boxShadow: "0px 3px 6px #0000001A",
+						borderColor: "hint",
+						transition: "box-shadow 0.3s ease-out",
+					}}
+				/>
+				{isNumInput && (
+					<Center
+						as="span"
+						pos="absolute"
+						top="0"
+						left="0"
+						height="100%"
+						w="5.6rem"
+						borderRight="1px solid"
+						borderColor="hint"
+						zIndex="1100"
+						{...inputNumStyle}
+					>
+						+91
+					</Center>
+				)}
+			</Flex>
+
+			{(invalid && errorMsg) || description ? (
+				<InputMsg error={invalid && errorMsg} {...errorStyle}>
+					{invalid && errorMsg ? errorMsg : description}
+				</InputMsg>
+			) : null}
+		</Flex>
+	);
+};
+
+export default Inputs;
---
+++ components/InputMsg/InputMsg.jsx
@@ -0,0 +1,19 @@
+import { Text } from "@chakra-ui/react";
+
+/**
+ * A <InputMsg> component
+ * TODO: Write more description here
+ * @arg 	{Object}	prop	Properties passed to the component
+ * @param	{string}	[prop.className]	Optional classes to pass to this component.
+ * @example	`<InputMsg></InputMsg>`
+ */
+const InputMsg = ({ error = false, children, ...props }) => {
+	if (!children) return null;
+	return (
+		<Text pl="6px" mt="4px" fontSize="xs" color="error" {...props}>
+			{children}
+		</Text>
+	);
+};
+
+export default InputMsg;
