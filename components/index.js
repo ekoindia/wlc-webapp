a5d699173b9509e64914fc06e89c956357af5adb
shubhmjain30 <shubhmjain30@gmail.com>
2023-01-13 13:36:38 +0530

    index.js for path updated

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 /* PLOP_INJECT_IMPORT */
-import { Tags } from "./Tags";
 import { Buttons } from "./Buttons";
 import { Cards } from "./Cards";
+import { Filter } from "./Filter";
 import { IconButtons } from "./IconButtons";
 import { Input } from "./Input";
 import { InputLabel } from "./InputLabel";
@@ -10,7 +10,9 @@ import { Layout } from "./Layout";
 import { LoginPanel } from "./LoginPanel";
 import { Network } from "./Network";
 import { ProfilePanel } from "./Profile";
-import { SearchFilterSort } from "./SearchFilterSort";
+import { SearchBar } from "./SearchBar";
+import { Sort } from "./Sort";
+import { Tags } from "./Tags";
 
 export {
 	Tags,
@@ -19,11 +21,13 @@ export {
 	InputLabel,
 	Input,
 	LoginPanel,
-	SearchFilterSort,
 	Network,
 	Layout,
 	Cards,
 	Buttons,
 	ProfilePanel,
+	Filter,
+	Sort,
+	SearchBar,
 	/* PLOP_INJECT_EXPORT */
 };
