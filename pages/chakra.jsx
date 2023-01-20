15d0e984d80346f27860fd39e6b65d23b5243dc2
Divxsh <guptadivash70@gmail.com>
2023-01-20 13:27:52 +0530

    Updated Theme

---
+++ pages/chakra.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Button, Center } from "@chakra-ui/react";
+
+const chakra = () => {
+	return (
+		<Center>
+			<Button size={"md"} variant="accent">
+				CLick me{" "}
+			</Button>
+		</Center>
+	);
+};
+
+export default chakra;
