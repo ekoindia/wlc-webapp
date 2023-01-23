0eff05fd28d04b297feba3c286e52c1484cc7a1a
shubhmjain30 <shubhmjain30@gmail.com>
2023-01-23 10:26:30 +0530

    minor table and profile updates

--- components/Profile/AddressPane.jsx
+++ components/Profile/AddressPane.jsx
@@ -10,7 +10,7 @@ import { Buttons, Cards, IconButtons } from "../";
 
 const AddressPane = () => {
 	return (
-		<Cards w="530px">
+		<Cards>
 			<Heading fontSize={18} fontWeight="semibold" color={"light"}>
 				Address Details
 			</Heading>
@@ -32,7 +32,7 @@ const AddressPane = () => {
 					<Box mt={8} mb={5}>
 						<iframe
 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.7367686993844!2d77.24740425137843!3d28.547630782365214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3cfc6b8ded5%3A0xce26da05dcf72035!2sEko%20India%20Financial%20Services%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1673455350818!5m2!1sen!2sin"
-							width="490"
+							width="450"
 							height="200"
 						></iframe>
 					</Box>
