dc1085c061fad94ef12ec947c4ac49fdf22f134b
Divxsh <guptadivash70@gmail.com>
2023-01-27 12:15:44 +0530

    Updated: Search Field

--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -17,12 +17,68 @@ import { Box, Input } from "@chakra-ui/react";
 
 export function SearchBar({ onChangeHandler }) {
 	return (
-		<Box>
+		<Box position="relative">
 			<Input
 				placeholder="Search by name or mobile"
 				size="lg"
 				width="600px"
 			/>
+			<Center
+				position="absolute"
+				top="0"
+				left="0"
+				width="60px"
+				height="100%"
+				zIndex="1"
+				color="light"
+				h="48px"
+			>
+				<Icon name="search" height="20px" width="20px" />
+			</Center>
+			{value !== "" && (
+				<Box
+					position="absolute"
+					top="106%"
+					width="100%"
+					h="214px"
+					zIndex="1"
+					bg="white"
+					borderRadius="10"
+					border="1px solid #D2D2D2"
+					p="20px 29px 24px 20px "
+				>
+					<VStack
+						align="flex-start"
+						divider={<StackDivider />}
+						spacing="15px"
+					>
+						<Box>
+							<Text as="p" fontSize="sm">
+								9891745076
+							</Text>
+							<Text as="p" fontSize="10px" color="#11299E">
+								Rajesh Enterprises
+							</Text>
+						</Box>
+						<Box>
+							<Text as="p" fontSize="sm">
+								9891745076
+							</Text>
+							<Text as="p" fontSize="10px" color="#11299E">
+								Rajesh Enterprises
+							</Text>
+						</Box>
+						<Box>
+							<Text as="p" fontSize="sm">
+								9891745076
+							</Text>
+							<Text as="p" fontSize="10px" color="#11299E">
+								Rajesh Enterprises
+							</Text>
+						</Box>
+					</VStack>
+				</Box>
+			)}
 		</Box>
 	);
 }
