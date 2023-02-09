edbb4a6ce7fb5d33c636dd2651a273418b4090fa
shubhmjain30 <shubhmjain30@gmail.com>
2023-02-09 15:11:29 +0530

    profile panes updated

--- components/Profile/DocPane.jsx
+++ components/Profile/DocPane.jsx
@@ -4,14 +4,46 @@ import {
 	Flex,
 	Heading,
 	Image,
+	Stack,
+	StackDivider,
 	Text,
-	useMediaQuery,
 } from "@chakra-ui/react";
 import { Buttons, Cards, Icon, IconButtons } from "../";
-import Router from "next/router";
 
 const DocPane = () => {
-	const [isSmallerThan440] = useMediaQuery("(max-width:440px)");
+	const docData = [
+		{
+			image: "/images/seller_logo.jpg",
+			name: "Customer Photo",
+			available: true,
+		},
+		{
+			image: "/images/seller_logo.jpg",
+			name: "Aadhaar front",
+			available: true,
+		},
+		{
+			image: "/images/seller_logo.jpg",
+			name: "Aadhaar back",
+			available: true,
+		},
+		{
+			image: "/images/seller_logo.jpg",
+			name: "PAN Card",
+			available: true,
+		},
+		{
+			image: "/images/seller_logo.jpg",
+			name: "Post dated cheque",
+			available: false,
+		},
+		{
+			image: "/images/seller_logo.jpg",
+			name: "e-Sign Agreement",
+			available: false,
+		},
+	];
+
 	return (
 		<Cards>
 			<Heading
@@ -22,172 +54,71 @@ const DocPane = () => {
 				Document Center
 			</Heading>
 
-			<Flex direction={"column"} spacing="15px" mt={"5"}>
-				{/* <Box h="439px"> */}
-				<Box h="439px" fontSize={{ base: "14px", md: "16px" }}>
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							onClick={() =>
-								Router.push(
-									"/admin/my-network/profile/up-per-info"
-								)
-							}
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
-					<Box
-						display={"flex"}
-						alignContent={"center"}
-						justifyContent={"space-between"}
-					>
-						<Flex align={"center"}>
-							<Image
-								src="/images/seller_logo.jpg"
-								h={42}
-								w={42}
-								borderRadius={"5"}
-							/>
-							<Text>Customer Photo</Text>
-						</Flex>
-						<IconButtons
-							title="Download"
-							iconPos="left"
-							iconName="file-download"
-							iconStyle={{
-								width: "14px",
-								height: "14px",
-							}}
-						/>
-					</Box>
-					<Divider my={2} />
+			<Flex direction={"column"}>
+				<Box h="474px" fontSize={{ base: "14px", md: "16px" }}>
+					<Stack direction="column" divider={<StackDivider />} mt="5">
+						{docData.map((item, index) => (
+							<Box
+								display={"flex"}
+								alignContent={"center"}
+								justifyContent={"space-between"}
+								key={index}
+								my="5px"
+							>
+								<Flex align={"center"}>
+									<Image
+										src="/images/seller_logo.jpg"
+										h={42}
+										w={42}
+										borderRadius={"5"}
+									/>
+									<Box>
+										<Text>{item.name}</Text>
+										{item.available ? (
+											<></>
+										) : (
+											<>
+												<Text
+													color="error"
+													fontSize="12px"
+												>
+													Not Available
+												</Text>
+											</>
+										)}
+									</Box>
+								</Flex>
+								<IconButtons
+									title={
+										item.available ? "Download" : "Upload"
+									}
+									iconPos="left"
+									variant={
+										item.available ? "primary" : "accent"
+									}
+									iconName={
+										item.available
+											? "file-download"
+											: "file-upload"
+									}
+									iconStyle={{
+										width: "14px",
+										height: "14px",
+									}}
+									textStyle={{
+										fontSize: "16px",
+									}}
+								/>
+							</Box>
+						))}
+					</Stack>
+					<Divider mt="10px" />
 				</Box>
 
-				<Box mt="15px" justifySelf="end">
-					<Buttons w={{ base: "100%", md: "215px" }} h="60px">
-						<Icon name="file-download" width="18px" />
-						&nbsp; Download All
-					</Buttons>
-				</Box>
+				<Buttons w={{ base: "100%", md: "215px" }} h="60px">
+					<Icon name="file-download" width="18px" />
+					&nbsp; Download All
+				</Buttons>
 			</Flex>
 		</Cards>
 	);
