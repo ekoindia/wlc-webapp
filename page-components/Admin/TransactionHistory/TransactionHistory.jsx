2337c8e44f71d6835478e3be085f3d6b94a479dc
shubhmjain30 <shubhmjain30@gmail.com>
2023-02-16 17:19:35 +0530

    transaction history moved to page-components

---
+++ page-components/Admin/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import { Box } from "@chakra-ui/react";
+import { SearchBar } from "components";
+import { useState } from "react";
+import { TransactionHistoryTable } from ".";
+
+/**
+ * A <TransactionHistory> component
+ * TODO: Write more description here
+ * @arg 	{Object}	prop	Properties passed to the component
+ * @param	{string}	[prop.className]	Optional classes to pass to this component.
+ * @example	`<TransactionHistory></TransactionHistory>`
+ */
+
+const TransactionHistory = ({ className = "", ...props }) => {
+	const [searchValue, setSearchValue] = useState(""); // TODO: Edit state as required
+
+	function onChangeHandler(e) {
+		setSearchValue(e);
+	}
+
+	return (
+		<Box w="100%" px={{ base: "16px", md: "initial" }} pb={"20px"}>
+			<Box>
+				<SearchBar
+					onChangeHandler={onChangeHandler}
+					value={searchValue}
+				/>
+			</Box>
+			<Box>
+				<TransactionHistoryTable />
+			</Box>
+		</Box>
+	);
+};
+
+export default TransactionHistory;
---
+++ page-components/Admin/TransactionHistory/index.js
@@ -0,0 +1,4 @@
+export { default as TransactionHistory } from "./TransactionHistory";
+
+export { TransactionHistoryTable } from "./TransactionHistoryTable";
+export { TransactionHistoryCard } from "./TransactionHistoryCard";
