ff71815249216e8b052660a052228ec220e05eeb
DEEPAK6265 <deepaksharma934029@gmail.com>
2023-02-28 16:27:26 +0530

    some changes in update personal info

--- page-components/Admin/UpdatePersonalInfo/UpdatePersonalInfo.jsx
+++ page-components/Admin/UpdatePersonalInfo/UpdatePersonalInfo.jsx
@@ -35,9 +35,7 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 	const EnterRef = useRef();
 	const [values, setValues] = React.useState("1");
 	// const [isSmallerThan500] = useMediaQuery("(max-width: 1400px)");
-	const [isTablet] = useMediaQuery("(max-width: 770px)");
-
-	const [count, setCount] = useState(0); // TODO: Edit state as required
+	// TODO: Edit state as required
 	const handleDragOver = (event) => {
 		event.preventDefault();
 	};
@@ -153,7 +151,7 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 							mt={{ base: "0px", lg: "3.1rem" }}
 							w={{
 								base: "100%",
-								lg: "77.5vw",
+								lg: "71.5vw",
 								xl: "66vw",
 								"2xl": "54.5vw",
 							}}
@@ -321,12 +319,6 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 											pos: "relative",
 											alignItems: "center",
 										}}
-										// isNumInput={true}
-										// inputProps={{ maxLength: 12 }}
-										// onFocus={() => {
-										// 	setInvalid(false);
-										// }}
-										// onKeyDown={onkeyHandler}
 									/>
 								</Box>
 								<Box
@@ -362,12 +354,6 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 											},
 											pos: "relative",
 										}}
-										// isNumInput={true}
-										// inputProps={{ maxLength: 12 }}
-										// onFocus={() => {
-										// 	setInvalid(false);
-										// }}
-										// onKeyDown={onkeyHandler}
 									/>
 								</Box>
 								<Box
@@ -404,12 +390,6 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 											},
 											pos: "relative",
 										}}
-										// isNumInput={true}
-										// inputProps={{ maxLength: 12 }}
-										// onFocus={() => {
-										// 	setInvalid(false);
-										// }}
-										// onKeyDown={onkeyHandler}
 									/>
 								</Box>
 							</Flex>
@@ -458,13 +438,6 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 											},
 											pos: "relative",
 										}}
-
-										// isNumInput={true}
-										// inputProps={{ maxLength: 12 }}
-										// onFocus={() => {
-										// 	setInvalid(false);
-										// }}
-										// onKeyDown={onkeyHandler}
 									/>
 								</Box>
 							</Flex>
@@ -612,12 +585,6 @@ const UpdatePersonalInfo = ({ className = "", ...props }) => {
 											},
 											pos: "relative",
 										}}
-										// isNumInput={true}
-										// inputProps={{ maxLength: 12 }}
-										// onFocus={() => {
-										// 	setInvalid(false);
-										// }}
-										// onKeyDown={onkeyHandler}
 									/>
 								</Box>
 								<Box
