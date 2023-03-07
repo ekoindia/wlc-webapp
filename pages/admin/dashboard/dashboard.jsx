4c35ad7e7b4f7e28991d8c0f73bccb827ed7160a
DEEPAK6265 <deepaksharma934029@gmail.com>
2023-03-07 11:07:20 +0530

    Dashboard component and icon added

---
+++ pages/admin/dashboard/dashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import { Layout } from "components";
+
+const Dashboard = () => {
+	return (
+		<>
+			<Head>
+				<title>Dashboard | Eko API Marketplace</title>
+			</Head>
+			<Layout>
+				<h1>Add Page Content Here</h1>
+			</Layout>
+		</>
+	);
+};
+
+export default Dashboard;
