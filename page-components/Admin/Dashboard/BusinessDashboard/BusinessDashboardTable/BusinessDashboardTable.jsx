de04679eea8343107931ea215b0c1d4b42ba851d
DEEPAK6265 <deepaksharma934029@gmail.com>
2023-03-09 15:28:37 +0530

    table added in Business Dashboard

---
+++ page-components/Admin/Dashboard/BusinessDashboard/BusinessDashboardTable/BusinessDashboardTable.jsx
@@ -0,0 +1,53 @@
+import { Table } from "components";
+import { mockData } from "constants/mockTableData";
+
+/**
+ * A <NetworkTable> component
+ * TODO: This is my network table with clickable rows
+ * @arg 	{Object}	prop	Properties passed to the component
+ * @param	{string}	[prop.className]	Optional classes to pass to this component.
+ * @example	`<NetworkTable></NetworkTable>`
+ */
+
+const NetworkTable = () => {
+	const recordCound = 10;
+
+	const renderer = [
+		{ name: "", field: "Sr. No." },
+		{ name: "name", field: "Name", sorting: true, show: "Avatar" },
+		{
+			name: "mobile_number",
+			field: "GTV",
+			sorting: true,
+		},
+		{ name: "type", field: "Total Transaction", sorting: true },
+		{
+			name: "account_status",
+			field: "Status",
+			show: "Tag",
+			sorting: true,
+		},
+		{
+			name: "createdAt",
+			field: "RA Cases",
+			sorting: true,
+		},
+		{ name: "ekocsp_code", field: "Onboarding Date", sorting: true },
+
+		{ name: "name", field: "Distributor Mapped", sorting: true },
+	];
+
+	return (
+		<>
+			<Table
+				pageLimit={recordCound}
+				renderer={renderer}
+				data={mockData}
+				variant="evenStripedClickableRow"
+				tableName="Network"
+			/>
+		</>
+	);
+};
+
+export default NetworkTable;
