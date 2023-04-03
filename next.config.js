29ba107c3696b14a370ca5394040ee74a746f81a
Divxsh <guptadivash70@gmail.com>
2023-04-03 15:36:12 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
-	env: {
-		production: process.env.PRODUCTION,
-		development: process.env.DEVELOPMENT,
-		client_id: process.env.CLIENT_ID,
-	},
 };
 
 module.exports = nextConfig;
