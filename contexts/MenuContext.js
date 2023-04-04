b84c9298c84b43842f2c4b279333c1d9f4edc2ef
shubhmjain30 <shubhmjain30@gmail.com>
2023-04-04 13:17:12 +0530

    Update: updated MenuContext: if transaction_list and role_list is present in local storage then it will not fetch, and if any of those two is missing or both are missing in local storage then it will fetch and set it in local storage.

--- contexts/MenuContext.js
+++ contexts/MenuContext.js
@@ -1,4 +1,3 @@
-import { Endpoints } from "constants/EndPoints";
 import { processTransactionData } from "helpers";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useUser } from "./UserContext";
@@ -13,29 +12,45 @@ const MenuProvider = ({ children }) => {
 	});
 
 	useEffect(() => {
+		let local_interaction_list = localStorage.getItem("interaction_list");
+		let local_role_tx_list = localStorage.getItem("role_tx_list");
 		if (userData?.is_org_admin === 0) {
-			//API call /transaction to fetch menu list
-			fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/transactions/wlc", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-					Authorization: `Bearer ${userData?.access_token}`,
-				},
-				body: JSON.stringify({ org_id: -1 }),
-			})
-				.then((res) => res.json())
-				.then((data) => {
-					if (data.length) {
-						const processedData = processTransactionData(data);
-						setInteractions(processedData);
+			if (local_interaction_list && local_role_tx_list) {
+				setInteractions({
+					interaction_list: JSON.parse(local_interaction_list),
+					role_tx_list: JSON.parse(local_role_tx_list),
+				});
+			} else {
+				fetch(
+					process.env.NEXT_PUBLIC_API_BASE_URL + "/transactions/wlc",
+					{
+						method: "POST",
+						headers: {
+							"Content-type": "application/json",
+							Authorization: `Bearer ${userData?.access_token}`,
+						},
+						body: JSON.stringify({ org_id: -1 }),
 					}
-				})
-				.catch((err) => console.log(err));
+				)
+					.then((res) => res.json())
+					.then((data) => {
+						if (data.length) {
+							const processedData = processTransactionData(data);
+							setInteractions(processedData);
+							localStorage.setItem(
+								"interaction_list",
+								JSON.stringify(processedData.interaction_list)
+							);
+							localStorage.setItem(
+								"role_tx_list",
+								JSON.stringify(processedData.role_tx_list)
+							);
+						}
+					})
+					.catch((err) => console.log(err));
+			}
 		}
 	}, [userData?.is_org_admin]);
-
-	// console.log("interactions", interactions);
-	//TODO fetch menu data,process data, set in local storage
 
 	return (
 		<MenuContext.Provider value={interactions}>
