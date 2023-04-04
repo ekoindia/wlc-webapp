d076d76e84a0cbb7d643460a260e64b662fac84e
shubhmjain30 <shubhmjain30@gmail.com>
2023-04-05 01:21:10 +0530

    next js config updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const { RegExpIgnorePlugin } = require("webpack");
+
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
+	webpack: (config, { isServer }) => {
+		// Exclude Storybook from being compiled in production builds
+		if (!isServer && process.env.NODE_ENV === "production") {
+			config.plugins.push(
+				new RegExpIgnorePlugin([
+					// Ignore Storybook files
+					/stories\//,
+				])
+			);
+		}
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;
