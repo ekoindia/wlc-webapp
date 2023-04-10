c09082c5d3732897d7952774f9503ba0cec4f05f
Divxsh <guptadivash70@gmail.com>
2023-04-10 16:12:23 +0530

    Updated Navbar

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -117,7 +117,7 @@ const NavContent = ({ setNavOpen, setIsCardOpen }) => {
 							orgDetail.logo ||
 							"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 56'%3E%3Crect fill='%23bbb' width='200' height='56' rx='6' ry='6'/%3E%3C/svg%3E"
 						}
-						alt={orgDetail.app_name + "'s logo" || "logo"}
+						alt={orgDetail.app_name + " logo"}
 						maxH={{
 							base: "35px",
 							sm: "34px",
