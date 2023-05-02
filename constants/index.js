4a8e4d4112d11a837f2ebee890e46a1220221164
shubhmjain30 <shubhmjain30@gmail.com>
2023-05-02 17:37:36 +0530

    constant index updated - UserType, userTypeLabel added

--- constants/index.js
+++ constants/index.js
@@ -15,4 +15,5 @@ export { table_data_mock } from "./table_data_mock";
 export { transactions_mock } from "./transactions_mock";
 export { transaction_history_mock } from "./transaction_history_mock";
 export { ParamMeta, ParamType } from "./trxnFramework";
+export { UserType, UserTypeLabel } from "./UserTypes";
 export { baseRoute, initialRoute, publicLinks } from "./validRoutes";
