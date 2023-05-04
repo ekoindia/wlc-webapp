a48b9666a6f56a002be777f2870b4eb4cec9d722
Kumar Abhishek <manustays@users.noreply.github.com>
2023-05-04 18:41:08 +0530

    fix: logo dimensions

--- page-components/LoginPanel/LoginPanel.jsx
+++ page-components/LoginPanel/LoginPanel.jsx
@@ -57,8 +57,8 @@ const LoginPanel = () => {
 						}
 						alt={orgDetail.app_name + " logo"}
 						pl={{ base: 4, md: "0" }}
-						mw={{ base: "100%", md: "28rem", "2xl": "43.75rem" }}
-						h={{ base: 16, "2xl": "4.5rem" }}
+						mw={{ base: "9rem", md: "14rem", "2xl": "19rem" }}
+						height={{ base: "2.2rem", md: 16 }}
 					/>
 				</Box>
 
