f2563d6db33d387b1b9248db63225f5568326298
Kumar Abhishek <manustays@users.noreply.github.com>
2023-05-06 09:47:32 +0530

    fix: remove duplicates from menu

--- contexts/MenuContext.js
+++ contexts/MenuContext.js
@@ -27,50 +27,57 @@ const MenuProvider = ({ children }) => {
 				let local_role_tx_list =
 					sessionStorage.getItem("role_tx_list") || "{}";
 
+				// Data cached? Use it...
 				if (
 					local_interaction_list &&
 					local_role_tx_list &&
 					local_interaction_list !== "[]" &&
 					local_role_tx_list !== "{}"
 				) {
-					setInteractions({
+					const context_data = {
 						interaction_list: JSON.parse(local_interaction_list),
 						role_tx_list: JSON.parse(local_role_tx_list),
-					});
+					};
+					setInteractions(context_data);
 					setLoading(false);
-				} else {
-					fetcher(
-						process.env.NEXT_PUBLIC_API_BASE_URL +
-							"/transactions/wlc",
-						{
-							token: userData?.access_token,
-							// body: { org_id: -1 },
-						}
-					)
-						// .then((res) => res.json())
-						.then((data) => {
-							setLoading(false);
-							if (data.length) {
-								const processedData =
-									processTransactionData(data);
-								setInteractions(processedData);
-								sessionStorage.setItem(
-									"interaction_list",
-									JSON.stringify(
-										processedData.interaction_list
-									)
-								);
-								sessionStorage.setItem(
-									"role_tx_list",
-									JSON.stringify(processedData.role_tx_list)
-								);
-							}
-						})
-						.catch((err) =>
-							console.error("MenuProvider error: ", err)
-						)
-						.finally(() => setLoading(false));
+					console.log(
+						"[MenuProvider]: data loaded from cache: ",
+						context_data
+					);
+					return;
 				}
+
+				// Data not cached? Fetch from API...
+				fetcher(
+					process.env.NEXT_PUBLIC_API_BASE_URL + "/transactions/wlc",
+					{
+						token: userData?.access_token,
+						// body: { org_id: -1 },
+					}
+				)
+					// .then((res) => res.json())
+					.then((data) => {
+						setLoading(false);
+						if (data.length) {
+							const processedData = processTransactionData(data);
+							setInteractions(processedData);
+							sessionStorage.setItem(
+								"interaction_list",
+								JSON.stringify(processedData.interaction_list)
+							);
+							sessionStorage.setItem(
+								"role_tx_list",
+								JSON.stringify(processedData.role_tx_list)
+							);
+
+							console.log(
+								"[MenuProvider]: data loaded from API: ",
+								processedData
+							);
+						}
+					})
+					.catch((err) => console.error("MenuProvider error: ", err))
+					.finally(() => setLoading(false));
 			}
 		}
 		// Commentted this beacuse there is no mean of this.
