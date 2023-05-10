ce8e13a9747fbd71f3b93f3ffa8cdd5e91423d1c
shubhmjain30 <shubhmjain30@gmail.com>
2023-05-10 23:21:10 +0530

    product detail constant added

---
+++ constants/ProductDetails.js
@@ -0,0 +1,15 @@
+/* Slab Details */
+export const slabs = {
+	DMT: { min: 100, max: 5000 },
+	AEPS: [
+		{ min: 100, max: 100 },
+		{ min: 101, max: 499 },
+		{ min: 500, max: 999 },
+		{ min: 1000, max: 1999 },
+		{ min: 2000, max: 2999 },
+		{ min: 3000, max: 6999 },
+		{ min: 7000, max: 9999 },
+		{ min: 10000, max: 10000 },
+	],
+	AADHAARPAY: { min: 1, max: 10000 },
+};
