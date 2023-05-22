d1d4744d60cf2f07c945eb63a6276f71e29cf28b
shubhmjain30 <shubhmjain30@gmail.com>
2023-05-22 11:54:50 +0530

    Update: 1. Removed page-components imports from Table components. 2. Removed redundant comments

--- page-components/Admin/AccountStatement/AccountStatementCard/AccountStatementCard.jsx
+++ page-components/Admin/AccountStatement/AccountStatementCard/AccountStatementCard.jsx
@@ -9,16 +9,6 @@ import { Box, Flex } from "@chakra-ui/react";
  */
 
 const AccountStatementCard = ({ item }) => {
-	// const item = {
-	// 	transactionId: 3027555195,
-	// 	datetime: "29/11/2022, 09:58:48 AM",
-	// 	activity: "DMT Commission",
-	// 	description:
-	// 		"Saurabh Mullick, 9654110669 A/C:20082437069 STATE BANK OF INDIA",
-	// 	amount: "20",
-	// 	type: "credit",
-	// 	balance: "123123",
-	// };
 	return (
 		<Flex direction="column" fontSize="12px" rowGap="2">
 			<Flex gap="2">
@@ -26,7 +16,6 @@ const AccountStatementCard = ({ item }) => {
 					Transaction ID:
 				</Box>
 				<Box as="span" color="dark" fontWeight="medium">
-					{/* {item.agent_mobile} */}
 					{item.transaction_id}
 				</Box>
 			</Flex>
@@ -35,7 +24,6 @@ const AccountStatementCard = ({ item }) => {
 					Date & Time:
 				</Box>
 				<Box as="span" color="dark" fontWeight="medium">
-					{/* {item.agent_mobile} */}
 					{item.date_time}
 				</Box>
 			</Flex>
@@ -44,7 +32,6 @@ const AccountStatementCard = ({ item }) => {
 					Activity:
 				</Box>
 				<Box as="span" color="dark" fontWeight="medium">
-					{/* {item.agent_mobile} */}
 					{item.activity}
 				</Box>
 			</Flex>
@@ -53,13 +40,11 @@ const AccountStatementCard = ({ item }) => {
 					Description:
 				</Box>
 				<Box as="span" color="dark" fontWeight="medium">
-					{/* {item.agent_mobile} */}
 					{item.description}
 				</Box>
 			</Flex>
 			<Flex mt="10px" fontSize="16px" fontWeight="medium">
 				<Box as="span" color="dark">
-					{/* {item.agent_mobile} */}
 					&#x20B9;{item.amount}
 				</Box>
 			</Flex>
