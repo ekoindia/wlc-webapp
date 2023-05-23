7bfe9852d5b054ff9146c881994aa009cef0f3d9
Kumar Abhishek <manustays@users.noreply.github.com>
2023-05-24 00:24:41 +0530

    feat: provision for onboarding

--- components/RouteProtecter/RouteProtecter.jsx
+++ components/RouteProtecter/RouteProtecter.jsx
@@ -24,6 +24,8 @@ const RouteProtecter = (props) => {
 
 	console.log("%cRoute-Protecter: Start\n", "color:green", {
 		isLoggedIn: isLoggedIn,
+		isAdmin: isAdmin,
+		userId: userId,
 		authorized: authorized,
 		loading: loading,
 	});
@@ -72,7 +74,7 @@ const RouteProtecter = (props) => {
 			setLoading(false);
 			setAuthorized(true);
 		}
-	}, [router.asPath, loading, isLoggedIn]);
+	}, [router.asPath, loading, isLoggedIn, userId]);
 
 	/**
 	 * Remove the flash of private pages when user is not nonLogged
