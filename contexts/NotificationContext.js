7aff4ebb982918e3bdd4f0cc8e78058817b6ea56
Kumar Abhishek <manustays@users.noreply.github.com>
2023-05-29 16:40:56 +0530

    feat: notifications in global search

--- contexts/NotificationContext.js
+++ contexts/NotificationContext.js
@@ -1,9 +1,11 @@
 import { CloseButton, Flex, Image, Text, useToast } from "@chakra-ui/react";
 import { Icon } from "components";
+import { ActionIcon } from "components/GlobalSearch";
 import { TransactionTypes } from "constants";
 import { useSession } from "contexts";
 import { fetcher } from "helpers";
 import { useLocalStorage } from "hooks";
+import { Priority, useRegisterActions } from "kbar";
 import {
 	createContext,
 	useCallback,
@@ -65,12 +67,75 @@ const useNotifications = () => {
 	const toast = useToast();
 	const toastIdRef = useRef();
 
+	// Register notification actions with KBar Search
+	const [notifActions, setNotifActions] = useState([]);
+	useRegisterActions(notifActions, [notifActions]);
+
 	// Process latest notification...
 	useEffect(() => {
 		if (notifications.length > 0) {
 			processLatestNotification(notifications[0]);
 		}
+		setupKbarNotificationActions(notifications);
 	}, [notifications]);
+
+	/**
+	 * Register notification actions with KBar Search
+	 * @param {Array} notifications List of all notifications
+	 */
+	const setupKbarNotificationActions = (notifications) => {
+		console.log("setupKbarNotificationActions:", notifications);
+		if (!notifications?.length) {
+			setNotifActions([]);
+			return;
+		}
+
+		let unread_count = 0;
+
+		const _notifActions = notifications.map((notif) => {
+			if (notif.read === 0) {
+				unread_count++;
+			}
+			return {
+				id: "notification/" + notif.id,
+				name: notif.title,
+				subtitle: notif.desc,
+				priority: notif.read === 1 ? Priority.LOW : Priority.NORMAL,
+				section: "Notifications",
+				// keywords: notif.title,
+				icon: (
+					<ActionIcon
+						icon="notifications"
+						ext_icon={notif.image}
+						color="#c026d3"
+					/>
+				),
+				parent: "notifications",
+				perform: () => {
+					openNotification(notif.id);
+				},
+			};
+		});
+
+		setNotifActions([
+			{
+				id: "notifications",
+				name: unread_count
+					? " You have unread notifications..."
+					: "View Notifications",
+				subtitle: unread_count
+					? unread_count +
+					  " unread out of total " +
+					  notifications.length
+					: "",
+				// section: "Notifications",
+				keywords: "inbox mail notice",
+				priority: unread_count ? Priority.HIGH : Priority.NORMAL,
+				icon: <Icon name="notifications" />,
+			},
+			..._notifActions,
+		]);
+	};
 
 	/**
 	 * Find a notification by ID
