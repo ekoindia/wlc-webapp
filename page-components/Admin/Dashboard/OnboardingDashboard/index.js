2170d1f77091650d0fde3be50fcaddeb45711b9e
shubhmjain30 <shubhmjain30@gmail.com>
2023-05-31 01:14:28 +0530

    Remove: OnboardingDashboardTable

--- page-components/Admin/Dashboard/OnboardingDashboard/index.js
+++ page-components/Admin/Dashboard/OnboardingDashboard/index.js
@@ -2,4 +2,3 @@ export { default as Filter } from "./Filter";
 export { default as OnboardedMerchants } from "./OnboardedMerchants";
 export { default as OnboardingDashboard } from "./OnboardingDashboard";
 export { OnboardingDashboardCard } from "./OnboardingDashboardCard";
-export { OnboardingDashboardTable } from "./OnboardingDashboardTable";
