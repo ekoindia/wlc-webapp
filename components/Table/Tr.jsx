e6d6c61a96edbedf7748f3dd29e735d452fd1180
shubhmjain30 <shubhmjain30@gmail.com>
2023-06-04 13:54:56 +0530

    removed Divider from expanded table

--- components/Table/Tr.jsx
+++ components/Table/Tr.jsx
@@ -95,7 +95,7 @@ const Tr = ({
 							}}
 							gap="8"
 						>
-							{extra?.map((column, rendererIndex) =>
+							{extra?.map((column /* rendererIndex */) =>
 								item[column.name] ? (
 									<>
 										<Flex direction="column">
@@ -119,12 +119,6 @@ const Tr = ({
 												)}
 											</Text>
 										</Flex>
-										{rendererIndex < extra.length - 1 && (
-											<Divider
-												orientation="vertical"
-												h="auto"
-											/>
-										)}
 									</>
 								) : null
 							)}
