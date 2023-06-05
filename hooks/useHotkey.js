2039aceb9809e4669b72df2e196a09d55e8567c6
Kumar Abhishek <manustays@users.noreply.github.com>
2023-06-05 15:16:42 +0530

    doc: added examples

--- hooks/useHotkey.js
+++ hooks/useHotkey.js
@@ -2,9 +2,33 @@ import { useEffect } from "react";
 import tinykeys from "tinykeys";
 
 /**
- * Hook for Hotkey
+ * Hook for setting Hotkey actions.
+ * Uses [tinykeys library](https://github.com/jamiebuilds/tinykeys).
  * @param {Object} keyFunctionMap - Map of key and function to be executed.
  * @param {Object} target - Target element to listen for keypress (Default: window).
+ * @returns {void}
+ * @example
+ * // Example usage:
+ * const keyFunctionMap = {
+ * 	"Escape": () => {
+ * 		console.log("Escape pressed");
+ * 	},
+ * 	"Shift+Enter": () => {
+ * 		console.log("Shift+Enter pressed");
+ * 	},
+ * };
+ * useHotkey(keyFunctionMap);
+ *
+ * // Example usage with target:
+ * const target = document.getElementById("myElement");
+ * useHotkey(keyFunctionMap, target);
+ *
+ * // Example key configs:
+ * "d" or "KeyD" - the "d" key
+ * "$mod+D" - Meta/Control+D
+ * "$mod+Shift+D" - Meta/Control+Shift+D
+ * "g i" - the "g" key followed by the "i" key (sequence)
+ * Other key examples: Space, Enter, ArrowUp, Control
  */
 const useHotkey = (keyFunctionMap, target = window) => {
 	useEffect(() => {
