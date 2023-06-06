149074bc96fd2238e780269aa35d3edb68987dd8
shubhmjain30 <shubhmjain30@gmail.com>
2023-06-06 11:34:35 +0530

    Heading updated

--- page-components/Admin/Dashboard/DashboardHeading.jsx
+++ page-components/Admin/Dashboard/DashboardHeading.jsx
@@ -9,84 +9,57 @@ import { Flex, Text } from "@chakra-ui/react";
  */
 const DashboardHeading = ({ pageId, headingList, handleHeadingClick }) => {
 	return (
-		<Flex m="20px" fontSize="sm" justify="space-between" w="100%">
+		<Flex
+			direction={{ base: "column", md: "row" }}
+			align={{ base: "flex-start", md: "center" }}
+			gap={{ base: "2", md: "8" }}
+			w="100%"
+			m="20px"
+			fontSize="sm"
+			justify="space-between"
+		>
+			<Text fontWeight="semibold" fontSize="2xl">
+				Dashboard
+			</Text>
 			<Flex
-				direction={{ base: "column", md: "row" }}
-				align={{ base: "flex-start", md: "center" }}
-				gap={{ base: "2", md: "8" }}
+				p="0.5"
+				gap="4"
 				w="100%"
+				h={{ base: "36px", md: "40px" }}
+				bg={{ base: "divider", md: "inherit" }}
+				borderRadius={{ base: "80px", md: "0px" }}
+				justify={{ base: "space-between", md: "flex-start" }}
 			>
-				<Text fontWeight="semibold" fontSize="2xl">
-					Dashboard
-				</Text>
-				<Flex
-					p="0.5"
-					gap="4"
-					w={{ base: "100%" }}
-					h={{ base: "36px", md: "40px" }}
-					bg={{ base: "divider", md: "inherit" }}
-					borderRadius={{ base: "80px", md: "0px" }}
-					justify={{ base: "space-between", md: "flex-start" }}
-				>
-					{headingList?.map((item, index) => {
-						const isActive = index === pageId;
-						return (
-							<Flex
-								key={item}
-								justify="center"
-								align="center"
-								w={{ base: "50%", md: "120px" }}
-								fontSize={{ base: "xs", md: "sm" }}
-								bg={{
-									base: isActive
-										? "accent.DEFAULT"
-										: "inherit",
-									md: isActive ? "accent.DEFAULT" : "white",
-								}}
-								boxShadow={
-									isActive ? "0px 3px 6px #11299E33" : null
-								}
-								border={{
-									base: "none",
-									md: !isActive ? "card" : null,
-								}}
-								color={isActive ? "white" : "dark"}
-								borderRadius={{ base: "80px" }}
-								onClick={() => handleHeadingClick(index)}
-								cursor="pointer"
-							>
-								<span>{item}</span>
-							</Flex>
-						);
-					})}
-				</Flex>
+				{headingList?.map((item, index) => {
+					const isActive = index === pageId;
+					return (
+						<Flex
+							key={item}
+							justify="center"
+							align="center"
+							w={{ base: "50%", md: "120px" }}
+							fontSize={{ base: "xs", md: "sm" }}
+							bg={{
+								base: isActive ? "accent.DEFAULT" : "inherit",
+								md: isActive ? "accent.DEFAULT" : "white",
+							}}
+							boxShadow={
+								isActive ? "0px 3px 6px #11299E33" : null
+							}
+							border={{
+								base: "none",
+								md: !isActive ? "card" : null,
+							}}
+							color={isActive ? "white" : "dark"}
+							borderRadius={{ base: "80px" }}
+							onClick={() => handleHeadingClick(index)}
+							cursor="pointer"
+						>
+							<span>{item}</span>
+						</Flex>
+					);
+				})}
 			</Flex>
-			{/* <Flex>
-					<DateView
-						date="2017-01-13T16:14:24+05:30"
-						format="dd-MM-yyyy"
-					/>
-					<span>&nbsp;-&nbsp;</span>
-					<DateView
-						date="2017-07-13T16:14:24+05:30"
-						format="dd-MM-yyyy"
-					/>
-				</Flex> */}
-			{/* <Flex
-				align="center"
-				justify="space-between"
-				w={{ base: "40%", xl: "25%" }}
-			>
-				<Flex>Last 7 Days</Flex>
-				<Flex>Last 30 Days</Flex>
-				<IcoButton
-					iconName="calender"
-					size="md"
-					rounded="full"
-					bg="primary.DEFAULT"
-					color="white"
-				/>
-			</Flex> */}
 		</Flex>
 	);
 };
