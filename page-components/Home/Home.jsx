8e015fd43716efb905b1fbd6340272a23fbb107c
Kumar Abhishek <manustays@users.noreply.github.com>
2023-06-07 19:27:41 +0530

    perf: pre-load Connect Widget on homepage

--- page-components/Home/Home.jsx
+++ page-components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Grid } from "@chakra-ui/react";
 import { useSession, useTodos } from "contexts";
+import Head from "next/head";
+import { useMemo } from "react";
 import {
 	BillPaymentWidget,
 	CommonTrxnWidget,
@@ -20,6 +22,22 @@ import {
 const Home = () => {
 	const { isLoggedIn } = useSession();
 	const { todos, deleteTodo } = useTodos();
+
+	// Check network speed on page load...
+	const isFastNetwork = useMemo(() => {
+		// Get network connection strength
+		const effectiveConnectionType =
+			navigator && "connection" in navigator
+				? navigator["connection"]["effectiveType"]
+				: "unknown";
+		if (
+			effectiveConnectionType === "slow-2g" ||
+			effectiveConnectionType === "2g"
+		) {
+			return false;
+		}
+		return true;
+	}, []);
 
 	if (!isLoggedIn) return null;
 
@@ -42,21 +60,41 @@ const Home = () => {
 	widgets.push({ id: 6, component: QueryWidget });
 
 	return (
-		<Grid
-			templateColumns={{
-				base: "repeat(auto-fit,minmax(280px,1fr))",
-				md: "repeat(auto-fit,minmax(340px,1fr))",
-				// xl: "repeat(auto-fit,minmax(440px,1fr))",
-			}}
-			justifyContent="center"
-			py={{ base: "20px", md: "0px" }}
-			gap={{ base: 4, lg: 5, "2xl": 8 }}
-			width={"100%"}
-		>
-			{widgets.map(({ id, component: Component }) => (
-				<Component key={id} />
-			))}
-		</Grid>
+		<>
+			{isFastNetwork ? (
+				<Head>
+					<link
+						rel="prefetch"
+						href={
+							process.env.NEXT_PUBLIC_CONNECT_WIDGET_URL +
+							"/elements/tf-eko-connect-widget/tf-wlc-widget.html"
+						}
+						as="script"
+					></link>
+					<link
+						rel="prefetch"
+						href="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.24/webcomponents-lite.min.js"
+						as="script"
+					></link>
+				</Head>
+			) : null}
+
+			<Grid
+				templateColumns={{
+					base: "repeat(auto-fit,minmax(280px,1fr))",
+					md: "repeat(auto-fit,minmax(340px,1fr))",
+					// xl: "repeat(auto-fit,minmax(440px,1fr))",
+				}}
+				justifyContent="center"
+				py={{ base: "20px", md: "0px" }}
+				gap={{ base: 4, lg: 5, "2xl": 8 }}
+				width={"100%"}
+			>
+				{widgets.map(({ id, component: Component }) => (
+					<Component key={id} />
+				))}
+			</Grid>
+		</>
 	);
 };
 
