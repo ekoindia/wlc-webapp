5451a712c589afd8cd5383c7bb16d235e68c61d4
jalajgoyaleko <jalaj.goyal@eko.co.in>
2023-06-08 08:59:16 +0530

    Fixed secret pin and updated package version

--- utils/pintwinFormat.js
+++ utils/pintwinFormat.js
@@ -1,11 +1,18 @@
 export const createPintwinFormat = (val, { pintwin_key, key_id }) => {
 	try {
-		console.log("pintwin 1", val, pintwin_key, key_id);
-		const encoderIndices = `${val}`?.split("")?.map((it) => {
-			it = pintwin_key.indexOf(it);
-			return it;
+		console.log(
+			"pintwin 1",
+			val,
+			`${val}`?.split(""),
+			pintwin_key,
+			`${pintwin_key}`?.split(""),
+			key_id
+		);
+		let encodedKey = "";
+		`${val}`?.split("")?.forEach((it) => {
+			encodedKey = encodedKey + `${pintwin_key}`.split("")[it];
 		});
-		return `${encoderIndices?.join("")}|${key_id}`;
+		return `${encodedKey}|${key_id}`;
 	} catch (err) {
 		console.error("In PintwinFormat => ", err);
 	}
