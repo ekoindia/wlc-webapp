f8ce32fbb7a592b5e8d2358b832dd23b7a053636
Kumar Abhishek <manustays@users.noreply.github.com>
2023-06-11 11:14:21 +0530

    feat: transactions page (connect widget) for admin

---
+++ pages/admin/transaction/[...id].js
@@ -0,0 +1,22 @@
+import { EkoConnectWidget } from "components/EkoConnectWidget";
+import { useRouter } from "next/router";
+
+/**
+ * [For Admin] The transaction page component to render all financial transaction flows.
+ * Currently, it loads the connect-wlc-widget.
+ */
+const TransactionPage = () => {
+	const router = useRouter();
+	const { id } = router.query;
+
+	if (!(id && id.length > 0)) {
+		return null;
+	}
+
+	// Starting transaction-type-id
+	const start_id = parseInt(id[0]);
+
+	return <EkoConnectWidget start_id={start_id} paths={id} />;
+};
+
+export default TransactionPage;
