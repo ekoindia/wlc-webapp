8d3e98777ce34b1270b87d4f3f11929cd9eb11e5
shubhmjain30 <shubhmjain30@gmail.com>
2023-06-14 12:08:02 +0530

    AirtelCms updated

--- page-components/Admin/PricingCommission/AirtelCms/AirtelCms.jsx
+++ page-components/Admin/PricingCommission/AirtelCms/AirtelCms.jsx
@@ -1,13 +1,66 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Button, Dropzone } from "components";
+import { useState } from "react";
+
 /**
- * A <AirtelCms> component
+ * A AirtelCms page-component
  * TODO: Write more description here
  * @param 	{object}	prop	Properties passed to the component
  * @param	{string}	prop.prop1	TODO: Property description.
  * @param	{...*}	rest	Rest of the props passed to this component.
  * @example	`<AirtelCms></AirtelCms>` TODO: Fix example
  */
-const AirtelCms = ({ prop1, ...rest }) => {
-	return <div {...rest}>AirtelCms</div>;
+const AirtelCms = () => {
+	const [file, setFile] = useState(null);
+
+	const handleFileUpload = async (event) => {
+		event.preventDefault();
+
+		try {
+			if (!file) return;
+			const formData = new FormData();
+			formData.append("file", file);
+
+			// hitQuery()
+			// // const response = await fetch("/api/upload", {
+			// // 	method: "POST",
+			// // 	body: formData,
+			// // });
+
+			// if (response.ok) {
+			// 	console.log("File uploaded successfully");
+			// } else {
+			// 	console.error("Error uploading file");
+			// }
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<Flex
+			direction="column"
+			gap="10"
+			fontSize="md"
+			w={{ base: "100%", md: "500px" }}
+		>
+			<Flex direction="column" gap="2">
+				<Text fontWeight="semibold">Download your current pricing</Text>
+				{/* <Button variant="link">Download</Button> */}
+			</Flex>
+			<Flex direction="column" gap="2">
+				<Text fontWeight="semibold">Upload your pricing</Text>
+				<Dropzone
+					file={file}
+					setFile={setFile}
+					accept="image/jpeg, image/jpg, image/png"
+				/>
+			</Flex>
+			<Button onClick={handleFileUpload} size="lg" h="64px" w="215px">
+				Upload
+			</Button>
+		</Flex>
+	);
 };
 
 export default AirtelCms;
