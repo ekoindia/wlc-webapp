871e5c4063b77819fb63c90392e6ef8beca72586
Kumar Abhishek <manustays@users.noreply.github.com>
2023-06-14 22:00:00 +0530

    feat: enabled search for commissions

--- contexts/CommissionContext.js
+++ contexts/CommissionContext.js
@@ -1,11 +1,13 @@
-// import { ActionIcon } from "components/CommandBar";
+import { ActionIcon } from "components/CommandBar";
 import { Endpoints } from "constants/EndPoints";
 import { useSession } from "contexts/UserContext";
 import { fetcher } from "helpers/apiHelper";
 import { useDailyCacheState } from "hooks";
-import { createContext, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { createContext, useContext, useEffect, useMemo } from "react";
+import { formatCurrency } from "utils";
 import { validateResp } from "utils/validateResponse";
-// import { useBusinessSearchActions } from "./GlobalSearchContext";
+import { useBusinessSearchActions } from "./GlobalSearchContext";
 
 const CommissionContext = createContext();
 
@@ -46,13 +48,53 @@ const formatCommissionData = (data) => {
 			};
 		}
 
-		newData[slug].slabs.push(item);
+		const tx_value = !(item.slab_from || item.slab_to)
+			? "Any"
+			: `₹${item.slab_from || 0} - ₹${item.slab_to || "Any"}`;
+
+		let commission;
+		if (item.calc_type === 1) {
+			commission = `${item.value || 0}%`;
+			if (item.min_value || item.max_value) {
+				commission += " (";
+				if (item.min_value) {
+					commission += `min: ${formatCurrency(
+						item.min_value,
+						"INR",
+						false,
+						true
+					)}`;
+				}
+				if (item.max_value) {
+					if (item.min_value) {
+						commission += ", ";
+					}
+					commission += `max: ${formatCurrency(
+						item.max_value,
+						"INR",
+						false,
+						true
+					)}`;
+				}
+				commission += ")";
+			}
+		} else {
+			commission = item.value;
+		}
+
+		newData[slug].slabs.push({
+			transaction_value: tx_value,
+			commission,
+			biller_name: item.biller_name || "",
+		});
 	});
 
 	return newData;
 };
 
 export const CommissionSummaryProvider = ({ children }) => {
+	const router = useRouter();
+
 	const [userCommission, setUserCommission, isValid] = useDailyCacheState(
 		"inf-commission",
 		{
@@ -122,22 +164,98 @@ export const CommissionSummaryProvider = ({ children }) => {
 		userId,
 	]);
 
-	// const CommissionAction = useMemo(() => {
-	// 	const actionData = userCommission?.pricing_commission_data?.map(
-	// 		({ product }) => ({
-	// 			id: `know-your-commission-${product}`,
-	// 			name: product,
-	// 			subtitle: `${product} commissions`,
-	// 			keywords: `business commission ${product}`,
-	// 			icon: <ActionIcon iconSize="md" color={"success"} />,
-	// 		})
-	// 	);
-	// 	return userCommission?.userId && userCommission?.userId === userId
-	// 		? actionData
-	// 		: [];
-	// }, [userCommission]);
+	// Generate search actions for CommandBar
+	const CommissionAction = useMemo(() => {
+		const actionList = [
+			{
+				id: "know-your-commissions",
+				name: "Know Your Commissions",
+				subtitle: "Get product-wise commission details",
+				keywords: `earning pricing`,
+				icon: (
+					<ActionIcon
+						icon="high-commission"
+						iconSize="28px"
+						// color={"success"}
+					/>
+				),
+				perform: false,
+				priority: 1,
+			},
+		];
 
-	// useBusinessSearchActions(CommissionAction, [CommissionAction]);
+		const commissionProdIds = Object.keys(userCommission?.data || {});
+
+		if (!commissionProdIds.length) {
+			return [];
+		}
+
+		// const commissionProducts = commissionProdIds.map((id) => ({
+		// 	id,
+		// 	label: commissionData?.data[id].label || id,
+		// }));
+
+		commissionProdIds.forEach((id) => {
+			const prod = userCommission?.data[id];
+			if (!prod) {
+				return;
+			}
+
+			let len = prod.slabs.length;
+			if (len > 3) {
+				len = 3;
+			}
+			let prodSlabs = "";
+			for (let i = 0; i < len; i++) {
+				const slab = prod.slabs[i];
+				prodSlabs +=
+					(prodSlabs === "" ? "" : ", ") +
+					`${slab.commission} for ${slab.transaction_value} ${
+						slab.biller_name ? `(${slab.biller_name})` : ""
+					}`;
+			}
+			prodSlabs = prodSlabs.substring(0, 50) + "...";
+
+			actionList.push({
+				id: `know-your-commission-${id}`,
+				name: "View " + prod.label + " Commissions",
+				subtitle: prodSlabs,
+				keywords: `${prod.label} commission earning pricing`,
+				icon: (
+					<ActionIcon
+						icon="high-commission"
+						iconSize="28px"
+						color="#d946ef"
+					/>
+				),
+				parent: "know-your-commissions",
+				perform: () => router.push("/commissions/" + id),
+				priority: 1,
+			});
+
+			// const prodSlabs = prod.slabs.map(({ transaction_value }) => ({
+			// 	id: `${id}-${transaction_value}`,
+			// 	label: transaction_value,
+			// }));
+		});
+
+		// const actionData = userCommission?.data?.map(({ product }) => ({
+		// 	id: `know-your-commission-${product}`,
+		// 	name: product,
+		// 	subtitle: `${product} commissions`,
+		// 	keywords: `business commission ${product}`,
+		// 	icon: <ActionIcon iconSize="md" color={"success"} />,
+		// }));
+		// return userCommission?.userId && userCommission?.userId === userId
+		// 	? actionData
+		// 	: [];
+
+		console.log("actionList:::: ", actionList);
+
+		return actionList;
+	}, [userCommission]);
+
+	useBusinessSearchActions(CommissionAction, [CommissionAction]);
 
 	return (
 		<CommissionContext.Provider value={userCommission}>
--- page-components/Commissions/Commissions.jsx
+++ page-components/Commissions/Commissions.jsx
@@ -3,7 +3,6 @@ import { Headings, Tags } from "components";
 import { tableRowLimit } from "constants";
 import { useCommissionSummary } from "contexts";
 import { useEffect, useState } from "react";
-import { formatCurrency } from "utils/numberFormat";
 import { CommissionsTable } from ".";
 
 const limit = tableRowLimit?.XLARGE; // Page size
@@ -25,44 +24,46 @@ const Commissions = ({ prod_id }) => {
 	}, [prod_id]);
 
 	useEffect(() => {
-		if (!tagValue || !commissionData) return;
+		if (!tagValue || !commissionData || !(tagValue in commissionData.data))
+			return;
 
 		const tagData = commissionData?.data[tagValue].slabs;
 
 		if (tagData?.length > 0) {
 			setTableData(
-				tagData.map(
-					({
-						slab_from,
-						slab_to,
-						value,
-						biller_name,
-						calc_type,
-						min_value,
-						max_value,
-					}) => ({
-						transaction_value: !(slab_from || slab_to)
-							? "Any"
-							: `${slab_from ? `₹ ${slab_from}` : "Any"} - ${
-									slab_to ? `₹ ${slab_to || 0}` : "Any"
-							  }`,
-						commission:
-							calc_type === 1
-								? `${value}% (min: ${formatCurrency(
-										min_value,
-										"INR",
-										false,
-										true
-								  )}, max: ${formatCurrency(
-										max_value,
-										"INR",
-										false,
-										true
-								  )})`
-								: value,
-						biller_name: biller_name || "-",
-					})
-				)
+				tagData
+				// .map(
+				// 	({
+				// 		slab_from,
+				// 		slab_to,
+				// 		value,
+				// 		biller_name,
+				// 		calc_type,
+				// 		min_value,
+				// 		max_value,
+				// 	}) => ({
+				// 		transaction_value: !(slab_from || slab_to)
+				// 			? "Any"
+				// 			: `${slab_from ? `₹ ${slab_from}` : "Any"} - ${
+				// 					slab_to ? `₹ ${slab_to || 0}` : "Any"
+				// 			  }`,
+				// 		commission:
+				// 			calc_type === 1
+				// 				? `${value}% (min: ${formatCurrency(
+				// 						min_value,
+				// 						"INR",
+				// 						false,
+				// 						true
+				// 				  )}, max: ${formatCurrency(
+				// 						max_value,
+				// 						"INR",
+				// 						false,
+				// 						true
+				// 				  )})`
+				// 				: value,
+				// 		biller_name: biller_name || "-",
+				// 	})
+				// )
 			);
 		}
 	}, [tagValue, commissionData]);
