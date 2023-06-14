b821540e5140ba7ff98aafb1b9cb2c5182ca99c5
shubhmjain30 <shubhmjain30@gmail.com>
2023-06-14 12:08:24 +0530

    Bbps updated

--- page-components/Admin/PricingCommission/Bbps/Bbps.jsx
+++ page-components/Admin/PricingCommission/Bbps/Bbps.jsx
@@ -1,5 +1,5 @@
 import { Flex, Radio, RadioGroup, Text } from "@chakra-ui/react";
-import { Button, Input } from "components";
+import { Button, Dropzone } from "components";
 import { useState } from "react";
 
 /**
@@ -12,7 +12,6 @@ import { useState } from "react";
  */
 const Bbps = () => {
 	const [file, setFile] = useState(null);
-	console.log("file", file);
 	const modeObj = {
 		0: "Online",
 		1: "Offline",
@@ -42,7 +41,12 @@ const Bbps = () => {
 	};
 
 	return (
-		<Flex direction="column" gap="10" fontSize="md">
+		<Flex
+			direction="column"
+			gap="10"
+			fontSize="md"
+			w={{ base: "100%", md: "500px" }}
+		>
 			<Flex direction="column" gap="2">
 				<Text fontWeight="semibold">Select Mode</Text>
 				<RadioGroup
@@ -56,9 +60,7 @@ const Bbps = () => {
 					>
 						{Object.entries(modeObj).map(([key, value]) => (
 							<Radio size="lg" key={key} value={key}>
-								<Text fontSize={{ base: "sm", sm: "md" }}>
-									{value}
-								</Text>
+								<Text fontSize="sm">{value}</Text>
 							</Radio>
 						))}
 					</Flex>
@@ -70,9 +72,13 @@ const Bbps = () => {
 			</Flex>
 			<Flex direction="column" gap="2">
 				<Text fontWeight="semibold">Upload your pricing</Text>
-				<DropZone {...{ setFile }} />
+				<Dropzone
+					file={file}
+					setFile={setFile}
+					accept=".xls,.xlsx,.xlsm,.xlsb,.csv,.xlt,.xltx,.xlam"
+				/>
 			</Flex>
-			<Button onClick={handleFileUpload} size="md">
+			<Button onClick={handleFileUpload} size="lg" h="64px" w="215px">
 				Upload
 			</Button>
 		</Flex>
@@ -80,64 +86,3 @@ const Bbps = () => {
 };
 
 export default Bbps;
-
-const DropZone = ({ setFile }) => {
-	const [inDropZone, setInDropZone] = useState(false);
-	console.log("inDropZone", inDropZone);
-
-	const handleFileInputChange = (event) => {
-		const file = event.target.files[0];
-		const fileUrl = URL.createObjectURL(file);
-		setFile(fileUrl);
-	};
-
-	const handleDragOver = (event) => {
-		event.preventDefault();
-		setInDropZone(true);
-	};
-
-	const handleDragLeave = (event) => {
-		event.preventDefault();
-		setInDropZone(false);
-	};
-
-	const handleDrop = (event) => {
-		event.preventDefault();
-		setInDropZone(false);
-		const file = event.dataTransfer.files[0];
-		const fileUrl = URL.createObjectURL(file);
-		setFile(fileUrl);
-	};
-	return (
-		<Flex
-			w={{ base: "100%", md: "500px" }}
-			h="200px"
-			// bg="divider"
-			align="center"
-			onDragOver={handleDragOver}
-			onDragLeave={handleDragLeave}
-			onDrop={handleDrop}
-			border="2px dashed #d7dbe3"
-		>
-			<Flex direction="column" align="center" w="100%" gap="4">
-				<Input
-					hidden
-					type="file"
-					onChange={handleFileInputChange}
-					id="fileInput"
-					padding="120px"
-					accept=".xls,.xlms" //TODO check on MIME type
-				/>
-				<Button
-					variant="primary"
-					size="md"
-					onClick={() => document.getElementById("fileInput").click()}
-				>
-					You can select your file
-				</Button>
-				<Text fontSize="sm">or drag &amp; drop your file here</Text>
-			</Flex>
-		</Flex>
-	);
-};
-//.doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf,
