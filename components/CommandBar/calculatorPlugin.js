2c5631ed0901194d17cbea9fe3e1a65161434f95
Kumar Abhishek <manustays@users.noreply.github.com>
2023-07-03 09:01:12 +0530

    refactor: massive cmd bar perf improvements

---
+++ components/CommandBar/calculatorPlugin.js
@@ -0,0 +1,74 @@
+import { getKBarAction } from ".";
+
+/**
+ * Get KBar result action after evaluating a math expression
+ * @param {object} options
+ * @param {string} options.expr - The math expression
+ * @param {function} options.copy - The copy function from useClipboard
+ * @param {function} options.toast - Function to show a toast message
+ * @param {function} options.parse - Function to parse a math expression
+ * @param {function} options.setParse - Function to set the parse function after loading it dynamically
+ * @param {string} options.parseLoadState - The load state of the parse function
+ * @param {function} options.setParseLoadState - Function to set the load state of the parse function
+ */
+export const getCalculatorAction = ({
+	expr,
+	copy,
+	toast,
+	parse,
+	setParse,
+	parseLoadState,
+	setParseLoadState,
+}) => {
+	let result;
+
+	// Lazy load the math parser
+	if (parseLoadState === "") {
+		setParseLoadState("loading");
+		import("/utils/exprParser.js")
+			.then((exprParser) => {
+				// Update context values once exprParser is loaded
+				setParse(() => exprParser.parse);
+				setParseLoadState("loaded");
+			})
+			.catch((error) => {
+				console.error(
+					"[CalculatorAction] Error loading exprParser:",
+					error
+				);
+				setParseLoadState("error");
+			});
+	}
+
+	// Parse the expression if the parser is loaded
+	if (expr && parse && parseLoadState === "loaded") {
+		result = parse(expr);
+	}
+
+	return getKBarAction({
+		id: "math/parse",
+		name: result ? `${result}` : "Calculator",
+		subtitle: result
+			? `Result of ${expr}  (⏎ to copy)`
+			: parseLoadState === "loading"
+			? "Loading calculator..."
+			: parseLoadState === "error"
+			? "Error loading calculator!"
+			: expr
+			? "Error: Invalid expression!"
+			: "Start typing an expression to calculate... (eg: =2+2)",
+		icon: "calculator",
+		iconSize: "lg",
+		iconColor: "#7c3aed",
+		perform: () => {
+			if (result) {
+				copy(result.toString());
+				toast({
+					title: "Copied: " + result,
+					status: "success",
+					duration: 2000,
+				});
+			}
+		},
+	});
+};
---
+++ components/CommandBar/historySearchPlugin.js
@@ -0,0 +1,119 @@
+import { getKBarAction } from ".";
+
+/**
+ * Get Kbar result actions for searching in Transaction History based on the numeric query input.
+ * For example, if the user types a number matching a phone number pattern, then show an action to search History by phone number.
+ * @param {Object} Options
+ * @param {string} Options.queryValue - The query value to search for
+ * @param {boolean} Options.isAdmin - Whether the user is an admin
+ * @param {object} Options.router - The router object
+ */
+export const getHistorySearchActions = ({
+	queryValue = "",
+	isAdmin,
+	router,
+}) => {
+	// Show other actions based on numeric search...
+
+	// Not available for Admins
+	if (isAdmin) return [];
+
+	// Show Genric Transaction History Search Action if no query is entered
+	if (queryValue?.length === 0) {
+		// Show Genric Transaction History Search Action for Non-Admin users only
+		return [
+			getKBarAction({
+				id: "historySearch",
+				name: "View Transaction History",
+				subtitle:
+					"Start typing TID, mobile, amount, etc., to search in History...",
+				keywords: "",
+				icon: "", // "tid"
+				// section: "History",
+				perform: () => router.push("/history"),
+			}),
+		];
+	}
+
+	// Remove formatters (commas and spaces) from the number query
+	const unformattedNumberQuery = queryValue?.replace(/(?<=[0-9])[ ,]/g, "");
+	const len = unformattedNumberQuery?.length ?? 0;
+	const isDecimal = unformattedNumberQuery?.includes(".");
+	const numQueryVal = Number(unformattedNumberQuery);
+
+	// Check if the query is a valid number
+	const isValidNumQuery =
+		numQueryVal && Number.isFinite(numQueryVal) && len >= 2 && len <= 18;
+
+	const results = [];
+
+	if (isValidNumQuery) {
+		// Show Transaction History Search Actions for Non-Admin users only
+
+		if (len === 10 && /^[6-9]/.test(queryValue) && !isDecimal) {
+			// Mobile number
+			results.push(
+				getKBarAction({
+					id: "historySearch/mobile",
+					name: "Search Transaction History by Mobile",
+					subtitle: `Customer's Mobile = ${numQueryVal}`,
+					// keywords: queryValue,
+					icon: "", // "mobile"
+					perform: () => {
+						console.log("Mobile search clicked", router);
+						router.push(`/history?customer_mobile=${numQueryVal}`);
+					},
+				})
+			);
+		}
+
+		if (len <= 7) {
+			// Amount
+			results.push(
+				getKBarAction({
+					id: "historySearch/amount",
+					name: "Search Transaction History by Amount",
+					subtitle: `Amount = ₹${numQueryVal}`,
+					// keywords: queryValue,
+					icon: "", // "amount"
+					// section: "History",
+					perform: () =>
+						router.push(`/history?amount=${numQueryVal}`),
+				})
+			);
+		}
+
+		if (len === 10 && !isDecimal) {
+			// TID
+			results.push(
+				getKBarAction({
+					id: "historySearch/tid",
+					name: "Search Transaction History by TID",
+					subtitle: `TID = ${numQueryVal}`,
+					// keywords: queryValue,
+					icon: "", // "tid"
+					// section: "History",
+					perform: () => router.push(`/history?tid=${numQueryVal}`),
+				})
+			);
+		}
+
+		if (len >= 9 && len <= 18 && !isDecimal) {
+			// Account Number
+			results.push(
+				getKBarAction({
+					id: "historySearch/account",
+					name: "Search Transaction History by Account Number",
+					subtitle: `Account Number = ${numQueryVal}`,
+					// keywords: queryValue,
+					icon: "", // "tid"
+					// section: "History",
+					perform: () =>
+						router.push(`/history?account=${numQueryVal}`),
+				})
+			);
+		}
+	}
+
+	return results;
+};
