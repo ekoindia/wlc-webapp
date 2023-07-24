abd2f7ce1c172b561933553f4d648f6f360501a1
Kumar Abhishek <manustays@users.noreply.github.com>
2023-07-24 11:18:54 +0530

    feat: added predefined color-themes constant file

---
+++ constants/colorThemes.js
@@ -0,0 +1,51 @@
+/**
+ * A list of predefined color-themes for the app
+ */
+export const colorThemes = [
+	// {
+	// 	name: "Eko",
+	// 	primary: "#FE9F00",
+	// 	primary_dark: "#f38300",
+	// 	primary_light: "#ffad23",
+	// 	accent: "#11299E",
+	// 	accent_dark: "#091968",
+	// 	accent_light: "#1F3ABC",
+	// },
+	{
+		name: "Persian Blue",
+		// navstyle: "dark",
+		primary: "#ed7802",
+		primary_dark: "#be5f00",
+		primary_light: "#f19333",
+		accent: "#015a8c",
+		accent_dark: "#024d78",
+		accent_light: "#0167a1",
+	},
+	{
+		name: "Twilight Treasure",
+		primary: "#8C6B00",
+		primary_dark: "#A07C00",
+		primary_light: "#735800",
+		accent: "#354d5b",
+		accent_dark: "#283b47",
+		accent_light: "#57788c",
+	},
+	{
+		name: "Raspberry",
+		primary: "#4c526f",
+		primary_dark: "#1e274b",
+		primary_light: "#636881",
+		accent: "#821f50",
+		accent_dark: "#5d1639",
+		accent_light: "#ba2c73",
+	},
+	{
+		name: "Twilight Sienna",
+		primary: "#E97451",
+		primary_dark: "#FF896A",
+		primary_light: "#C15F41",
+		accent: "#354d5b",
+		accent_dark: "#283b47",
+		accent_light: "#57788c",
+	},
+];
