6bff005174623a7842eabef5873e93fd3bd4581e
shubhmjain30 <shubhmjain30@gmail.com>
2023-07-25 14:16:37 +0530

    AdminChangeRole updated

--- constants/AdminChangeRole.js
+++ constants/AdminChangeRole.js
@@ -5,6 +5,7 @@
  * @property {string} slug - A unique identifier for the role change option.
  * @property {string} label - The display label for the role change option.
  * @property {string} path - The URL path for navigating to the role change page with a specific tab.
+ * @property {Array} visible - List of user types to show this tab for
  */
 
 /**
@@ -16,20 +17,28 @@ export const ChangeRoleMenuList = [
 		slug: "transfer-retailer",
 		label: "Transfer Retailers",
 		path: "/admin/my-network/profile/change-role",
+		visible: [2, 3],
+		visibleString: ["Retailer", "I-CSP"],
 	},
 	{
 		slug: "retailer-to-distributor",
 		label: "Promote Retailer To Distributor",
 		path: "/admin/my-network/profile/change-role",
+		visible: [2, 3],
+		visibleString: ["Retailer", "I-CSP"],
 	},
-	// {
-	// 	slug: "demote-distributor",
-	// 	label: "Demote Distributor",
-	// 	path: "/admin/my-network/profile/change-role",
-	// },
+	{
+		slug: "demote-distributor",
+		label: "Demote Distributor",
+		path: "/admin/my-network/profile/change-role",
+		visible: [1],
+		visibleString: ["Distributor"],
+	},
 	{
 		slug: "retailer-to-iretailer",
 		label: "Upgrade Retailer To iRetailer",
 		path: "/admin/my-network/profile/change-role",
+		visible: [2],
+		visibleString: ["Retailer", "I-CSP"],
 	},
 ];
