ce15aea1503eda6a863ab533f1a4abd9e23e70b4
shubhmjain30 <shubhmjain30@gmail.com>
2023-07-31 12:39:56 +0530

    Temporarily commenting configuration page.

--- constants/SidebarMenu.ts
+++ constants/SidebarMenu.ts
@@ -89,11 +89,11 @@ export const adminSidebarMenu: sidebarMenuType[] = [
 		link: "/admin/transaction/" + TransactionIds.INVOICE_DOWNLOAD,
 		trxn_id: TransactionIds.INVOICE_DOWNLOAD,
 	},
-	{
-		icon: "manage",
-		name: "Configurations",
-		link: "/admin/configurations",
-	},
+	// {
+	// 	icon: "manage",
+	// 	name: "Configurations",
+	// 	link: "/admin/configurations",
+	// },
 ];
 
 /**
