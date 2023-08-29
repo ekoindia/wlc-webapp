7ab89461546b1cc1e2b191ef3529d2236aba637c
shubhmjain30 <shubhmjain30@gmail.com>
2023-08-29 10:34:13 +0530

    major: Single Onboarding updated (re-structured)

--- page-components/Admin/Onboarding/SingleOnboarding/SingleOnboarding.jsx
+++ page-components/Admin/Onboarding/SingleOnboarding/SingleOnboarding.jsx
@@ -1,20 +1,15 @@
-import {
-	Flex,
-	FormControl,
-	Radio,
-	RadioGroup,
-	Text,
-	useToast,
-} from "@chakra-ui/react";
-import { Button, Input } from "components";
+import { Flex, FormControl, FormLabel } from "@chakra-ui/react";
+import { Button, Input, Radio, Select } from "components";
 import { ParamType } from "constants";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { SingleOnboardingSuccess } from ".";
+import { Controller, useForm } from "react-hook-form";
+
+const AGENT_TYPE = {
+	RETAILERS: "0",
+	DISTRIBUTOR: "2",
+};
 
 /**
- * A <SingleOnboarding> component
- * TODO: Write more description here
+ * A SingleOnboarding page-component
  * @param 	{object}	prop	Properties passed to the component
  * @param	{string}	prop.prop1	TODO: Property description.
  * @param	{...*}	rest	Rest of the props passed to this component.
@@ -22,241 +17,178 @@ import { SingleOnboardingSuccess } from ".";
  */
 
 const SingleOnboarding = () => {
-	const toast = useToast();
-	const [applicantType, setApplicantType] = useState("0");
-
-	const [submittedVal, setSubmittedVal] = useState();
-	console.log("submittedVal", submittedVal);
-
-	const [ready, setReady] = useState(false);
-	console.log("ready", ready);
-	const [isOpen, setIsOpen] = useState(null);
-
-	const onSubmit = (submittedData) => {
-		let submitted = submittedData;
-		submitted["applicantType"] = applicantType;
-		setSubmittedVal(submitted);
-
-		setReady(true);
-		//setIsOpen(true);
-
-		handleClose();
-
-		// Create Request Body
-		// API Call
-		// Send Response to next screen( Success/Failure )
-
-		toast({
-			title: "Onboarded SuccessFully",
-			status: "success",
-			duration: 6000,
-			isClosable: true,
-		});
-		resetForm();
-	};
-
-	const applicantTypeList = [
-		{ value: "0", label: "Seller" },
-		{ value: "2", label: "Distributor" },
-	];
-
 	const {
 		register,
 		handleSubmit,
-		formState: { errors, isSubmitting },
-		reset,
-	} = useForm();
+		formState: { /*  errors, */ isSubmitting },
+		// reset,
+		watch,
+		control,
+	} = useForm({
+		defaultValues: {
+			onboardAgentType: AGENT_TYPE.RETAILERS,
+		},
+	});
 
-	const emptyValue = {
-		mobileNumber: "",
-		distributorMobileNum: "",
-		name: "",
-	};
+	const onboardAgentTypeList = [
+		{ value: AGENT_TYPE.RETAILERS, label: "Onboard Retailers" },
+		{ value: AGENT_TYPE.DISTRIBUTOR, label: "Onboard Distributor" },
+	];
 
-	const handleClose = () => {
-		setIsOpen(true);
-	};
+	const onboardAgentType = watch("onboardAgentType");
 
-	const resetForm = () => {
-		setReady(null);
-		reset({ ...emptyValue });
-	};
+	// const watchAll = watch();
+	// console.log("watchAll", watchAll);
 
-	const formListObj = [
+	const onboard_seller_request_structure = [
 		{
-			id: "mobileNumber",
+			name: "mobile_number",
 			label: "Mobile Number",
 			required: true,
 			parameter_type_id: ParamType.NUMERIC,
 		},
 		{
-			id: "name",
+			name: "name",
 			label: "Name",
 			required: true,
 			parameter_type_id: ParamType.TEXT,
 		},
 		{
-			id: "distributorMobileNum",
-			label: "Distributor Mobile Number (Optional)",
+			name: "scsp_mobile_number",
+			label: "Distributor's Mobile Number",
 			required: false,
 			parameter_type_id: ParamType.NUMERIC,
 		},
 	];
+	const onboard_distributor_request_structure = [
+		{
+			name: "mobile_number",
+			label: "Mobile Number",
+			required: true,
+			parameter_type_id: ParamType.NUMERIC,
+		},
+		{
+			name: "name",
+			label: "Name",
+			required: true,
+			parameter_type_id: ParamType.TEXT,
+		},
+	];
+
+	const onSubmit = (data) => {
+		console.log("data", data);
+	};
 
 	return (
-		<>
-			{isOpen && <SingleOnboardingSuccess setIsOpen={setIsOpen} />}
-			{!isOpen && (
-				<Flex
-					direction="column"
-					w="100%"
-					bg="white"
-					borderRadius={8}
-					fontSize="md"
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Flex direction="column" gap="8">
+				<Controller
+					name="onboardAgentType"
+					control={control}
+					render={({ field: { onChange, value } }) => (
+						<Radio
+							value={value}
+							options={onboardAgentTypeList}
+							onChange={onChange}
+						/>
+					)}
+				/>
+
+				<Form
+					{...{
+						parameter_list:
+							onboardAgentType === AGENT_TYPE.RETAILERS
+								? onboard_seller_request_structure
+								: onboard_distributor_request_structure,
+						register,
+					}}
+				/>
+
+				<Button
+					loading={isSubmitting}
+					w={{ base: "100%", md: "215px" }}
+					h="64px"
+					type="submit"
+					size="lg"
 				>
-					<Flex
-						direction="column"
-						gap={4}
-						w={{ base: "100%", md: "500px" }}
-					>
-						<Flex direction="column" gap="2">
-							<RadioGroup
-								defaultValue="0"
-								value={applicantType}
-								onChange={(value) => {
-									setApplicantType(value);
-									resetForm();
-								}}
-							>
-								<Flex
-									direction={{ base: "column", sm: "row" }}
-									gap={{ base: "4", md: "16" }}
-								>
-									{applicantTypeList.map((item) => (
-										<Radio
-											size="lg"
-											key={item.value}
-											value={item.value}
-										>
-											<Text fontSize="sm">
-												{"Onboard "}
-												{item.label}
-											</Text>
-										</Radio>
-									))}
-								</Flex>
-							</RadioGroup>
-						</Flex>
-						<form onSubmit={handleSubmit(onSubmit)}>
-							{formListObj.map(
-								({
-									id,
-									label,
-									//required,
-									parameter_type_id,
-								}) => {
-									switch (parameter_type_id) {
-										case ParamType.NUMERIC:
-											return (
-												<Flex direction="column" mt={4}>
-													<FormControl
-														key={id}
-														w={{
-															base: "100%",
-															md: "500px",
-														}}
-													>
-														{!(
-															id ===
-																"distributorMobileNum" &&
-															applicantType == 2
-														) && (
-															<Input
-																id={id}
-																label={label}
-																required
-																leftAddon="+91"
-																type="numeric"
-																fontSize="md"
-																invalid={
-																	errors.id
-																		? true
-																		: false
-																}
-																errorMsg={
-																	errors?.id
-																		?.message
-																}
-																maxLength={10}
-																{...register(
-																	id,
-																	{
-																		required: true,
-																	}
-																)}
-															/>
-														)}
-													</FormControl>
-												</Flex>
-											);
-										case ParamType.TEXT:
-											return (
-												<Flex direction="column" mt={4}>
-													<FormControl
-														key={id}
-														w={{
-															base: "100%",
-															md: "500px",
-														}}
-													>
-														<Input
-															id={id}
-															label={label}
-															required
-															invalid={
-																errors.id
-																	? true
-																	: false
-															}
-															errorMsg={
-																errors?.id
-																	?.message
-															}
-															type="text"
-															fontSize="md"
-															{...register(id, {
-																required: true,
-															})}
-														/>
-													</FormControl>
-												</Flex>
-											);
-									}
-								}
-							)}
-
-							<Button
-								loading={isSubmitting}
-								mt={8}
-								type="submit"
-								size="lg"
-							>
-								Submit
-							</Button>
-
-							{/* {submittedVal && (
-								<div>
-									Submitted Data:
-									<br />
-									{JSON.stringify(submittedVal)}
-								</div>
-							)} */}
-						</form>
-					</Flex>
-				</Flex>
-			)}
-		</>
+					Submit
+				</Button>
+			</Flex>
+		</form>
 	);
 };
 
 export default SingleOnboarding;
+
+const Form = ({ parameter_list, register, control }) => {
+	return (
+		<Flex direction="column" gap="8">
+			{parameter_list?.map(
+				({
+					name,
+					label,
+					required,
+					value,
+					disabled,
+					list_elements,
+					parameter_type_id,
+				}) => {
+					switch (parameter_type_id) {
+						case ParamType.LIST:
+							return (
+								<FormControl
+									id={name}
+									w={{ base: "100%", md: "500px" }}
+									// isInvalid={errors.priority}
+								>
+									<FormLabel>{label}</FormLabel>
+									<Controller
+										name={name}
+										control={control}
+										render={({
+											field: { onChange, value },
+										}) => {
+											return (
+												<Select
+													value={value}
+													options={list_elements}
+													onChange={onChange}
+												/>
+											);
+										}}
+									/>
+								</FormControl>
+							);
+						case ParamType.NUMERIC:
+						case ParamType.TEXT:
+							return (
+								<FormControl
+									key={name}
+									w={{
+										base: "100%",
+										md: "500px",
+									}}
+								>
+									<Input
+										id={name}
+										label={label}
+										required={required}
+										value={value}
+										type={
+											parameter_type_id ===
+											ParamType.NUMERIC
+												? "number"
+												: "text"
+										}
+										fontSize="sm"
+										disabled={disabled}
+										{...register(name)}
+									/>
+								</FormControl>
+							);
+					}
+				}
+			)}
+		</Flex>
+	);
+};
