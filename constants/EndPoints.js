598d9812c6fb98ac11df270753b6eed9c16320d8
shubhmjain30 <shubhmjain30@gmail.com>
2023-09-19 01:19:53 +0530

    added dojson (temporarily) in endpoints

--- constants/EndPoints.js
+++ constants/EndPoints.js
@@ -6,6 +6,7 @@ export const Endpoints = {
 	GENERATE_TOKEN: "/authentication/token",
 	REFRESH_PROFILE: "/authentication/refresh-profile",
 	TRANSACTION: "/transactions/do",
+	TRANSACTION_JSON: "/transactions/dojson",
 	UPLOAD: "/transactions/upload",
 	UPLOAD_CUSTOM_URL: "/transactions/customupload", //TODO: merge functionality into upload in node.js app
 	GET_ORG_FROM_DOMAIN: "/wlctransactions/wlcorgmeta",
