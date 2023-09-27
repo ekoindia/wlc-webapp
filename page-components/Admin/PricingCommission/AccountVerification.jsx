3ff04b2572647cac7ebd63431ce6f689bdf1bbbd
shubhmjain30 <shubhmjain30@gmail.com>
2023-09-27 17:58:09 +0530

    update: Account Verification api integrated

--- page-components/Admin/PricingCommission/AccountVerification.jsx
+++ page-components/Admin/PricingCommission/AccountVerification.jsx
@@ -1,11 +1,25 @@
-import { Flex, FormControl, FormLabel } from "@chakra-ui/react";
+import { Flex, FormControl, FormLabel, useToast } from "@chakra-ui/react";
 import { Button, Radio } from "components";
 import { Endpoints } from "constants";
 import { useSession } from "contexts";
 import { fetcher } from "helpers";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+
+const OPERATION = {
+	SUBMIT: 1,
+	FETCH: 0,
+};
+
+const getStatus = (status) => {
+	switch (status) {
+		case 0:
+			return "success";
+		default:
+			return "error";
+	}
+};
 
 const accountVerificationOptions = [
 	{
@@ -34,49 +48,67 @@ const AccountVerification = () => {
 	const {
 		handleSubmit,
 		// register,
-		// watch,
+		watch,
 		// formState: { errors /* isSubmitting */ },
 		control,
 		// setValue,
+		reset,
 	} = useForm();
 
+	const toast = useToast();
 	const { accessToken } = useSession();
-
-	// const watchAccountVerification = watch("account_verification");
-
 	const router = useRouter();
+	const [accountVerificationStatus, setAccountVerificationStatus] =
+		useState(null);
+
+	const watchAccountVerificationStatus = watch("account_verification");
 
 	// const pricingTypeList = [{ value: "1", label: "Fixed (₹)" }];
 
 	useEffect(() => {
-		console.log("RUNNING");
 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
 			body: {
 				interaction_type_id: 737,
-				operation: 0,
+				operation: OPERATION.FETCH,
 			},
 			token: accessToken,
 		})
 			.then((res) => {
-				console.log("res", res);
+				const _accountVerification = res?.data?.otp_verification_token;
+				setAccountVerificationStatus(_accountVerification);
 			})
 			.catch((err) => {
 				console.error("error", err);
 			});
 	}, []);
 
+	useEffect(() => {
+		if (accountVerificationStatus !== null) {
+			let defaultValues = {};
+			defaultValues.account_verification = accountVerificationStatus;
+			reset({ ...defaultValues });
+		}
+	}, [accountVerificationStatus]);
+
 	const handleFormSubmit = (data) => {
-		console.log("data", data);
+		const { account_verification } = data ?? {};
 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
 			body: {
 				interaction_type_id: 737,
-				operation: 1,
-				otp_verification_token: data?.account_verification,
+				operation: OPERATION.SUBMIT,
+				otp_verification_token: account_verification,
 			},
 			token: accessToken,
 		})
 			.then((res) => {
-				console.log("res", res);
+				const _accountVerification = res?.data?.otp_verification_token;
+				setAccountVerificationStatus(_accountVerification);
+				toast({
+					title: res.message,
+					status: getStatus(res.status),
+					duration: 5000,
+					isClosable: true,
+				});
 			})
 			.catch((err) => {
 				console.error("error", err);
@@ -178,6 +210,10 @@ const AccountVerification = () => {
 						w={{ base: "100%", md: "250px" }}
 						fontWeight="bold"
 						borderRadius={{ base: "none", md: "10" }}
+						disabled={
+							accountVerificationStatus ===
+							watchAccountVerificationStatus
+						}
 					>
 						Set
 					</Button>
