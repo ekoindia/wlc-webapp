30f5e27142c0c87e7dabf459745b7c87eb5397f3
shubhmjain30 <shubhmjain30@gmail.com>
2023-09-27 15:05:34 +0530

    Form updated

--- tf-components/Form/Form.jsx
+++ tf-components/Form/Form.jsx
@@ -1,7 +1,13 @@
-import { Flex, FormControl, FormLabel } from "@chakra-ui/react";
+import {
+	Flex,
+	FormControl,
+	FormErrorMessage,
+	FormHelperText,
+} from "@chakra-ui/react";
 import { Input, MultiSelect, Radio, Select } from "components";
 import { ParamType } from "constants";
 import { Controller } from "react-hook-form";
+import { getFormErrorMessage } from "utils";
 
 /**
  * A Form component
@@ -10,26 +16,40 @@ import { Controller } from "react-hook-form";
  * @param	{...*}	rest	Rest of the props passed to this component.
  * @example	`<Form></Form>` TODO: Fix example
  */
-const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
+const Form = ({
+	parameter_list,
+	register,
+	formValues,
+	control,
+	errors,
+	...rest
+}) => {
 	return (
 		<Flex direction="column" gap="8" {...rest}>
 			{parameter_list?.map(
-				({
-					name,
-					label,
-					required = true,
-					value,
-					disabled,
-					list_elements,
-					defaultValue,
-					parameter_type_id,
-					is_multi_select,
-					is_radio,
-					multiSelectRenderer,
-					validations,
-					visible_on_param_name,
-					visible_on_param_value,
-				}) => {
+				(
+					{
+						name,
+						label,
+						required = true,
+						value,
+						disabled,
+						list_elements,
+						defaultValue,
+						parameter_type_id,
+						is_multi,
+						meta = {},
+						multiSelectRenderer,
+						validations,
+						helperText,
+						visible_on_param_name,
+						visible_on_param_value,
+						...rest
+					},
+					index
+				) => {
+					const { force_dropdown } = meta;
+
 					if (visible_on_param_name && visible_on_param_value) {
 						const _shouldBeVisible = visible_on_param_value.test(
 							formValues?.[visible_on_param_name]
@@ -41,10 +61,14 @@ const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
 					switch (parameter_type_id) {
 						case ParamType.NUMERIC:
 						case ParamType.TEXT:
+							console.log("INPUT", label);
 							return (
 								<FormControl
+									key={`${name}-${label}-${index}`}
 									id={name}
 									w={{ base: "auto", md: "500px" }}
+									isRequired={required}
+									isInvalid={errors[name]}
 								>
 									<Input
 										id={name}
@@ -59,24 +83,35 @@ const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
 										}
 										fontSize="sm"
 										disabled={disabled}
+										{...rest}
 										{...register(name, {
 											...validations,
 										})}
 									/>
+									{errors[name] ? (
+										<FormErrorMessage>
+											{getFormErrorMessage(name, errors)}
+										</FormErrorMessage>
+									) : helperText !== undefined ? (
+										<FormHelperText>
+											{helperText}
+										</FormHelperText>
+									) : null}
 								</FormControl>
 							);
 						case ParamType.LIST:
 							if (list_elements) {
-								if (is_multi_select) {
+								if (is_multi) {
 									return (
 										<FormControl
+											key={`${name}-${label}-${index}`}
 											id={name}
 											w={{
 												base: "auto",
 												md: "500px",
 											}}
+											isRequired={required}
 										>
-											<FormLabel>{label}</FormLabel>
 											<Controller
 												name={name}
 												control={control}
@@ -85,21 +120,45 @@ const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
 													field: { onChange },
 												}) => (
 													<MultiSelect
-														options={list_elements}
-														renderer={
-															multiSelectRenderer
-														}
-														onChange={onChange}
+														{...{
+															// value,
+															id: name,
+															label,
+															onChange,
+															options:
+																list_elements,
+															renderer:
+																multiSelectRenderer,
+														}}
+														{...rest}
 													/>
 												)}
 												rules={{ ...validations }}
 											/>
+											{errors[name] ? (
+												<FormErrorMessage>
+													{getFormErrorMessage(
+														name,
+														errors
+													)}
+												</FormErrorMessage>
+											) : helperText !== undefined ? (
+												<FormHelperText>
+													{helperText}
+												</FormHelperText>
+											) : null}
 										</FormControl>
 									);
-								} else if (is_radio) {
+								} else if (
+									list_elements.length < 4 &&
+									!force_dropdown
+								) {
 									return (
-										<FormControl id={name}>
-											<FormLabel>{label}</FormLabel>
+										<FormControl
+											key={`${name}-${label}-${index}`}
+											id={name}
+											isRequired={required}
+										>
 											<Controller
 												name={name}
 												control={control}
@@ -108,25 +167,50 @@ const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
 													field: { onChange, value },
 												}) => (
 													<Radio
-														options={list_elements}
-														onChange={onChange}
-														value={value}
+														{...{
+															id: name,
+															label,
+															labelStyle: {
+																pb: {
+																	base: "0.5rem",
+																	md: "0.25rem",
+																},
+															},
+															value,
+															onChange,
+															options:
+																list_elements,
+														}}
+														{...rest}
 													/>
 												)}
 												rules={{ ...validations }}
 											/>
+											{errors[name] ? (
+												<FormErrorMessage>
+													{getFormErrorMessage(
+														name,
+														errors
+													)}
+												</FormErrorMessage>
+											) : helperText !== undefined ? (
+												<FormHelperText>
+													{helperText}
+												</FormHelperText>
+											) : null}
 										</FormControl>
 									);
 								} else {
 									return (
 										<FormControl
+											key={`${name}-${label}-${index}`}
 											id={name}
 											w={{
 												base: "auto",
 												md: "500px",
 											}}
+											isRequired={required}
 										>
-											<FormLabel>{label}</FormLabel>
 											<Controller
 												name={name}
 												control={control}
@@ -136,16 +220,32 @@ const Form = ({ parameter_list, register, formValues, control, ...rest }) => {
 												}) => {
 													return (
 														<Select
-															value={value}
-															options={
-																list_elements
-															}
-															onChange={onChange}
+															{...{
+																id: name,
+																label,
+																value,
+																onChange,
+																options:
+																	list_elements,
+															}}
+															{...rest}
 														/>
 													);
 												}}
 												rules={{ ...validations }}
 											/>
+											{errors[name] ? (
+												<FormErrorMessage>
+													{getFormErrorMessage(
+														name,
+														errors
+													)}
+												</FormErrorMessage>
+											) : helperText !== undefined ? (
+												<FormHelperText>
+													{helperText}
+												</FormHelperText>
+											) : null}
 										</FormControl>
 									);
 								}
