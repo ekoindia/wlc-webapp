dc17b56752c48573a950a15edce6a05763f5109e
shubhmjain30 <shubhmjain30@gmail.com>
2023-10-01 15:57:34 +0530

    CreditCardBillPayment added

---
+++ page-components/Admin/PricingCommission/CreditCardBillPayment/CreditCardBillPayment.jsx
@@ -0,0 +1,46 @@
+import { Flex } from "@chakra-ui/react";
+import { Radio } from "components";
+import { useState } from "react";
+import {
+	CreditCardBillPaymentDistributor,
+	CreditCardBillPaymentRetailer,
+} from ".";
+
+const AGENT_TYPE = {
+	RETAILERS: "0",
+	DISTRIBUTOR: "2",
+};
+
+const agent_type_list = [
+	{ value: AGENT_TYPE.RETAILERS, label: "Retailers" },
+	{ value: AGENT_TYPE.DISTRIBUTOR, label: "Distributors" },
+];
+
+/**
+ * A CreditCardBillPayment tab page-component
+ * @example	<CreditCardBillPayment/>
+ */
+const CreditCardBillPayment = () => {
+	const [agentType, setAgentType] = useState(AGENT_TYPE.RETAILERS);
+
+	return (
+		<Flex direction="column" gap="8">
+			<Radio
+				label="Select Agent Type"
+				value={agentType}
+				options={agent_type_list}
+				onChange={(val) => {
+					setAgentType(val);
+				}}
+			/>
+
+			{agentType === AGENT_TYPE.RETAILERS ? (
+				<CreditCardBillPaymentRetailer />
+			) : (
+				<CreditCardBillPaymentDistributor />
+			)}
+		</Flex>
+	);
+};
+
+export default CreditCardBillPayment;
