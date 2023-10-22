4ee1c5a2f96038dbe5b5b705f27f0e99971a4b15
shubhmjain30 <shubhmjain30@gmail.com>
2023-10-22 15:43:08 +0530

    update: forwardRef in textarea

--- components/Textarea/Textarea.jsx
+++ components/Textarea/Textarea.jsx
@@ -1,71 +1,78 @@
 import { Flex, Textarea as ChakraTextarea } from "@chakra-ui/react";
-import { useId } from "react";
+import { forwardRef, useId } from "react";
 import { InputLabel } from "..";
 
 /**
  * A Textarea component
  * @example	`<Textarea></Textarea>` TODO: Fix example
  */
-const Textarea = ({
-	id,
-	value,
-	label,
-	labelStyle,
-	placeholder,
-	required,
-	onChange,
-	noOfLines = 2,
-	maxLength = 100,
-	resize = "none",
-	isDisabled,
-	isInvalid,
-	styles,
-	...rest
-}) => {
-	const _id = useId();
-	return (
-		<Flex direction="column" w="100%" {...rest}>
-			{label ? (
-				<InputLabel
-					htmlFor={id ?? _id}
-					required={required}
-					{...labelStyle}
-				>
-					{label}
-				</InputLabel>
-			) : null}
-			<ChakraTextarea
-				id={id ?? _id}
-				resize={resize}
-				noOfLines={noOfLines}
-				maxLength={maxLength}
-				onChange={onChange}
-				isDisabled={isDisabled}
-				isInvalid={isInvalid}
-				_hover={{
-					borderColor: "primary.DEFAULT",
-					borderWidth: "1px",
-					borderStyle: "solid",
-				}}
-				_active={{
-					borderColor: "primary.DEFAULT",
-					borderWidth: "1px",
-					borderStyle: "solid",
-				}}
-				_focusVisible={{
-					borderColor: "primary.DEFAULT",
-					borderWidth: "1px",
-					borderStyle: "solid",
-				}}
-				_invalid={{
-					borderColor: "error",
-					borderWidth: "1px",
-					borderStyle: "solid",
-				}}
-				{...styles}
-			/>
-		</Flex>
-	);
-};
+const Textarea = forwardRef(
+	(
+		{
+			id,
+			value,
+			label,
+			labelStyle,
+			placeholder,
+			required,
+			onChange,
+			noOfLines = 2,
+			maxLength = 10,
+			resize = "none",
+			disabled,
+			isInvalid,
+			styles,
+			...rest
+		},
+		ref
+	) => {
+		const _id = useId();
+		return (
+			<Flex direction="column" w="100%" {...rest}>
+				{label ? (
+					<InputLabel
+						htmlFor={id ?? _id}
+						required={required}
+						{...labelStyle}
+					>
+						{label}
+					</InputLabel>
+				) : null}
+				<ChakraTextarea
+					ref={ref}
+					id={id ?? _id}
+					resize={resize}
+					noOfLines={noOfLines}
+					maxLength={maxLength}
+					onChange={onChange}
+					isDisabled={disabled}
+					isInvalid={isInvalid}
+					_hover={{
+						borderColor: "primary.DEFAULT",
+						borderWidth: "1px",
+						borderStyle: "solid",
+					}}
+					_active={{
+						borderColor: "primary.DEFAULT",
+						borderWidth: "1px",
+						borderStyle: "solid",
+					}}
+					_focusVisible={{
+						borderColor: "primary.DEFAULT",
+						borderWidth: "1px",
+						borderStyle: "solid",
+					}}
+					_invalid={{
+						borderColor: "error",
+						borderWidth: "1px",
+						borderStyle: "solid",
+					}}
+					{...styles}
+				/>
+			</Flex>
+		);
+	}
+);
+Textarea.displayName = "Textarea";
 
 export default Textarea;
