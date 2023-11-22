5e4005c3b76de07c7b6ed16b863fe2d549b7b8fd
shubhmjain30 <shubhmjain30@gmail.com>
2023-11-22 11:00:14 +0530

    update: code refactored for filter & export

--- page-components/History/History.jsx
+++ page-components/History/History.jsx
@@ -8,7 +8,6 @@ import {
 } from "constants";
 import { useGlobalSearch, useSession, useUser } from "contexts";
 import { fetcher } from "helpers";
-import { formatDate } from "libs/dateFormat";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useForm, useWatch } from "react-hook-form";
@@ -16,6 +15,22 @@ import { saveDataToFile } from "utils/FileSave";
 import { HistoryTable, HistoryToolbar } from ".";
 
 const limit = tableRowLimit?.XLARGE; // Page size
+
+const action = {
+	FILTER: 0,
+	EXPORT: 1,
+};
+
+const export_type_options = [
+	{
+		value: "xlsx",
+		label: "Download Report(Excel)",
+	},
+	{
+		value: "pdf",
+		label: "Download Report(Pdf)",
+	},
+];
 
 /**
  * A History component shows transaction history
@@ -26,14 +41,25 @@ const limit = tableRowLimit?.XLARGE; // Page size
  */
 const History = () => {
 	const {
-		// handleSubmit,
-		register,
-		control,
-		formState: { errors, isSubmitting },
+		handleSubmit: handleSubmitFilter,
+		register: registerFilter,
+		control: controlFilter,
+		formState: { errors: errorsFilter, isSubmitting: isSubmittingFilter },
 	} = useForm();
 
-	const watcher = useWatch({
-		control,
+	const {
+		handleSubmit: handleSubmitExport,
+		register: registerExport,
+		control: controlExport,
+		formState: { errors: errorsExport, isSubmitting: isSubmittingExport },
+	} = useForm();
+
+	const watcherFilter = useWatch({
+		control: controlFilter,
+	});
+
+	const watcherExport = useWatch({
+		control: controlExport,
 	});
 
 	const formElements = {
@@ -45,20 +71,80 @@ const History = () => {
 		amount: "",
 		rr_no: "",
 	};
-	const [data, setData] = useState();
-	const [activePillIndex, setActivePillIndex] = useState(0);
-	const [searchValue, setSearchValue] = useState("");
-	const [currentPage, setCurrentPage] = useState(1);
-	const [formState, setFormState] = useState({ ...formElements });
-	const [clear, setClear] = useState(false);
+	const router = useRouter();
 	const { userData } = useUser();
 	const { accountDetails } = userData;
 	const { account_list } = accountDetails;
 	const { accessToken } = useSession();
-	const router = useRouter();
-	const [finalFormState, setFinalFormState] = useState({});
 	const { setSearchTitle } = useGlobalSearch();
+	const [data, setData] = useState();
+	const [searchValue, setSearchValue] = useState("");
+	const [currentPage, setCurrentPage] = useState(1);
+	const [, setFormState] = useState({ ...formElements });
+	const [finalFormState, setFinalFormState] = useState({});
+	const [clear, setClear] = useState(false);
 	const [loading, setLoading] = useState(false);
+	const [openModalId, setOpenModalId] = useState(null);
+
+	const history_filter_parameter_list = [
+		{
+			name: "tid",
+			label: "TID",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "account",
+			label: "Account Number",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "customer_mobile",
+			label: "Customer Mobile No.",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "start_date",
+			label: "From",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "tx_date",
+			label: "To",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "amount",
+			label: "Amount",
+			parameter_type_id: ParamType.NUMERIC,
+			inputLeftElement: <Icon name="rupee" size="sm" color="light" />,
+			validations: {
+				required: false,
+			},
+		},
+		{
+			name: "rr_no",
+			label: "Tracking Number",
+			parameter_type_id: ParamType.NUMERIC,
+			validations: {
+				required: false,
+			},
+		},
+	];
 
 	// Set GlobalSearch title
 	useEffect(() => {
@@ -73,7 +159,6 @@ const History = () => {
 	// or, a mobile number.
 	useEffect(() => {
 		const { search, ...others } = router.query;
-		console.log("others", others);
 		if (search || others) {
 			quickSearch(search, others);
 		}
@@ -86,9 +171,6 @@ const History = () => {
 		}
 		console.log("search value", e);
 	}
-	const handlePillClick = (index) => {
-		setActivePillIndex(index);
-	};
 
 	const hitQuery = (abortController, key) => {
 		console.log("[History] fetch started...", key);
@@ -145,79 +227,6 @@ const History = () => {
 		};
 	}, [currentPage, finalFormState]);
 
-	const onFilterSubmit = () => {
-		console.log("hitQuery inside onFilterSubmit");
-		// Get all non-empty values from formState and set in finalFormState
-		const _finalFormState = {};
-		Object.keys(formState).forEach((key) => {
-			if (formState[key]) {
-				_finalFormState[key] = formState[key];
-			}
-		});
-		setFinalFormState(_finalFormState);
-
-		onClose();
-		setClear(true);
-	};
-
-	const onFilterClear = () => {
-		setFormState({ ...formElements });
-		setFinalFormState({});
-		setSearchValue("");
-		setClear(false);
-	};
-
-	/**
-	 * Update formState with user inputs
-	 * @param {*} event
-	 */
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormState((prevFormState) => ({
-			...prevFormState,
-			[name]: value,
-		}));
-	};
-
-	const history_filter_parameter_list = [
-		{
-			name: "tid",
-			label: "TID",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-		{
-			name: "account",
-			label: "Account Number",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-		{
-			name: "customer_mobile",
-			label: "Customer Mobile No.",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-		{
-			name: "start_date",
-			label: "From",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-		{
-			name: "tx_date",
-			label: "To",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-		{
-			name: "amount",
-			label: "Amount",
-			parameter_type_id: ParamType.NUMERIC,
-			inputLeftElement: <Icon name="rupee" size="sm" color="light" />,
-		},
-		{
-			name: "rr_no",
-			label: "Tracking Number",
-			parameter_type_id: ParamType.NUMERIC,
-		},
-	];
-
 	/**
 	 * Search for a transaction based on the query. The query can be a transaction-id, account, amount, or, a mobile number.
 	 * @param {*} search
@@ -237,7 +246,9 @@ const History = () => {
 			setFinalFormState({
 				...{ tid, account, customer_mobile, amount },
 			});
-			onClose();
+
+			// onClose();
+			setOpenModalId(null);
 			setClear(true);
 			return;
 		}
@@ -266,19 +277,50 @@ const History = () => {
 		setFinalFormState({
 			[type]: search,
 		});
-		onClose();
+
+		// onClose();
+		setOpenModalId(null);
 		setClear(true);
 	};
 
-	const onReportDownload = (value) => {
-		const currentDate = new Date();
-		const formattedDate = formatDate(currentDate, "yyyy-MM-dd");
+	const onFilterSubmit = (data) => {
+		console.log("hitQuery inside onFilterSubmit", data);
 
-		const isNonEmpty = (value) => value !== "";
+		// Get all non-empty values from formState and set in finalFormState
+		const _finalFormState = {};
+		Object.keys(data).forEach((key) => {
+			if (data[key]) {
+				_finalFormState[key] = data[key];
+			}
+		});
+		setFinalFormState(_finalFormState);
+
+		// onClose();
+		setOpenModalId(null);
+		setClear(true);
+	};
+
+	const onFilterClear = () => {
+		setFormState({ ...formElements });
+		setFinalFormState({});
+		setSearchValue("");
+		setClear(false);
+	};
+
+	const onReportDownload = (data) => {
+		console.log("inside onReportDownload data", data);
+
+		// onClose();
+		setOpenModalId(null);
+
+		// const currentDate = new Date();
+		// const formattedDate = formatDate(currentDate, "yyyy-MM-dd");
+
+		const isNonEmpty = (data) => data !== "";
 
 		// Filter the keys in finalFormState based on non-empty values
-		const nonEmptyValues = Object.keys(finalFormState).filter((key) =>
-			isNonEmpty(finalFormState[key])
+		const nonEmptyValues = Object.keys(data).filter((key) =>
+			isNonEmpty(data[key])
 		);
 
 		// Create a new object with the non-empty values
@@ -288,9 +330,10 @@ const History = () => {
 		});
 
 		// Add current date if tx_date is not selected in filter by default
-		if (!filterParameter["tx_date"] !== undefined) {
-			filterParameter.tx_date = formattedDate;
-		}
+		// ? default one day if nothing
+		// if (!filterParameter["tx_date"] !== undefined) {
+		// 	filterParameter.tx_date = formattedDate;
+		// }
 
 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
 			headers: {
@@ -307,13 +350,11 @@ const History = () => {
 					account_list[0].id
 						? account_list[0]?.id
 						: null,
-				reporttype: value,
-				...filterParameter,
+				...data,
 			},
 			token: accessToken,
 		})
 			.then((data) => {
-				console.log(data);
 				const _blob = data?.file?.blob;
 				const _filename = data?.file?.name || "file";
 				const _type = data?.file["content-type"];
@@ -326,10 +367,46 @@ const History = () => {
 	};
 
 	const transactionList = data;
-	const [isOpen, setIsOpen] = useState(false);
 
-	const onClose = () => setIsOpen(false);
-	const onOpen = () => setIsOpen(true);
+	const actionBtnConfig = [
+		{
+			id: action.FILTER,
+			label: "Filter",
+			icon: "filter",
+			parameter_list: history_filter_parameter_list,
+			handleSubmit: handleSubmitFilter,
+			register: registerFilter,
+			control: controlFilter,
+			errors: errorsFilter,
+			isSubmitting: isSubmittingFilter,
+			formValues: watcherFilter,
+			handleFormSubmit: onFilterSubmit,
+			submitButtonText: "Apply",
+		},
+		{
+			id: action.EXPORT,
+			label: "Export",
+			icon: "file-download",
+			parameter_list: [
+				...history_filter_parameter_list,
+				{
+					name: "reporttype",
+					label: "Export Type",
+					parameter_type_id: ParamType.LIST,
+					list_elements: export_type_options,
+				},
+			],
+
+			handleSubmit: handleSubmitExport,
+			register: registerExport,
+			control: controlExport,
+			errors: errorsExport,
+			isSubmitting: isSubmittingExport,
+			formValues: watcherExport,
+			handleFormSubmit: onReportDownload,
+			submitButtonText: "Export",
+		},
+	];
 
 	return (
 		<>
@@ -353,35 +430,24 @@ const History = () => {
 			>
 				<HistoryToolbar
 					{...{
-						form_parameter_list: history_filter_parameter_list,
-						formValues: watcher,
-						register,
-						control,
-						errors,
-						isSubmitting,
-						activePillIndex,
-						pillsData,
-						handlePillClick,
 						searchValue,
 						onSearchSubmit,
-						handleChange,
 						clear,
-						formState,
-						isOpen,
-						onOpen,
-						onClose,
-						onFilterSubmit,
 						onFilterClear,
-						onReportDownload,
+						openModalId,
+						setOpenModalId,
+						actionBtnConfig,
 					}}
 				/>
 				<PrintReceipt heading="Transaction Receipt (copy)">
 					<HistoryTable
-						pageNumber={currentPage}
-						setPageNumber={setCurrentPage}
-						transactionList={transactionList}
-						tableRowLimit={limit}
-						loading={loading}
+						{...{
+							loading,
+							transactionList,
+							tableRowLimit: limit,
+							pageNumber: currentPage,
+							setPageNumber: setCurrentPage,
+						}}
 					/>
 				</PrintReceipt>
 			</Flex>
@@ -390,41 +456,3 @@ const History = () => {
 };
 
 export default History;
-
-// TODO: Get from HistoryTrxnList
-const pillsData = [
-	{ id: "1", name: "All" },
-	{ id: "2", name: "Send Cash" },
-	{ id: "3", name: "AePS" },
-	{ id: "4", name: "Indo-Nepal" },
-	{ id: "5", name: "Card Payment" },
-	{ id: "6", name: "UPI Payment" },
-	{ id: "7", name: "Bill Payment" },
-	{ id: "8", name: "Deposit" },
-];
-
-/*
-function Pill({ name, activePillIndex, index }) {
-	return (
-		<Box
-			h="23px"
-			w="100%"
-			whiteSpace="nowrap"
-			cursor={"pointer"}
-			px="10px"
-			border="1px solid #D2D2D2"
-			borderRadius="30px"
-			bg={activePillIndex === index ? "primary.DEFAULT" : "white"}
-			color={activePillIndex === index ? "white" : "light"}
-			_hover={{ bg: "primary.DEFAULT", color: "white" }}
-			display={"flex"}
-			justifyContent={"center"}
-			alignItems="center"
-		>
-			<Box display={"flex"} textAlign="justify">
-				<Text fontSize={"12px"}>{name}</Text>
-			</Box>
-		</Box>
-	);
-}
-*/
--- page-components/History/HistoryToolbar.jsx
+++ page-components/History/HistoryToolbar.jsx
@@ -1,5 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
-import { Button, Icon, Menus, Modal, SearchBar } from "components";
+import { Button, Icon, Modal, SearchBar } from "components";
 import { Form } from "tf-components";
 
 /**
@@ -7,51 +7,14 @@ import { Form } from "tf-components";
  * @example	`<HistoryToolbar></HistoryToolbar>` TODO: Fix example
  */
 const HistoryToolbar = ({
-	// activePillIndex,
-	// pillsData,
-	// handlePillClick,
-	form_parameter_list,
-	formValues,
-	register,
-	control,
-	errors,
-	// isSubmitting,
 	searchValue,
 	onSearchSubmit,
 	clear,
-	// handleChange,
-	// formState,
-	isOpen,
-	onOpen,
-	onClose,
-	onFilterSubmit,
 	onFilterClear,
-	onReportDownload,
+	openModalId,
+	setOpenModalId,
+	actionBtnConfig,
 }) => {
-	// const labelStyle = {
-	// 	fontSize: { base: "sm" },
-	// 	// color: "inputlabel",
-	// };
-
-	const menuList = [
-		{
-			id: 1,
-			value: "xlsx",
-			label: "Download Report(Excel)",
-			onClick: (value) => {
-				onReportDownload(value);
-			},
-		},
-		{
-			id: 2,
-			value: "pdf",
-			label: "Download Report(Pdf)",
-			onClick: (value) => {
-				onReportDownload(value);
-			},
-		},
-	];
-
 	return (
 		<Flex
 			justifyContent={"space-between"}
@@ -63,20 +26,7 @@ const HistoryToolbar = ({
 				},
 			}}
 		>
-			{/* <===========================Toggles Button ===============================> */}
-			{/* <Flex gap="8px" overflowX="auto" mt={{ base: "40px", lg: "0px" }}>
-                            {pillsData.map((pill, index) => (
-                                <Box key={index} onClick={() => handlePillClick(index)}>
-                                    <Pill
-                                        name={pill.name}
-                                        activePillIndex={activePillIndex}
-                                        index={index}
-                                    />
-                                </Box>
-                            ))}
-                        </Flex> */}
 			<Flex w="100%" gap="2" justify="flex-end" align="center">
-				{/* <==========Clear Filter Button =========> */}
 				{clear && (
 					<Button
 						size="xs"
@@ -88,7 +38,6 @@ const HistoryToolbar = ({
 					</Button>
 				)}
 
-				{/* <==========Search =========> */}
 				<SearchBar
 					type="number"
 					placeholder="Search by TID, Mobile, Account, etc"
@@ -97,119 +46,73 @@ const HistoryToolbar = ({
 					minSearchLimit={2}
 				/>
 
-				{/* <==========Filter Button =========> */}
-				<Button size="lg" onClick={onOpen}>
-					<Icon name="filter" width="18px" />
-					&nbsp;
-					<Text display={{ base: "none", md: "flex" }}>Filter</Text>
-				</Button>
-
-				{/* <========== Export Reports =========> */}
-				<Menus
-					as={Button}
-					type="everted"
-					size="lg"
-					title="Export"
-					menulist={menuList}
-					iconPos="left"
-					iconName="file-download"
-					iconStyles={{ width: "18px" }}
-					rounded="10px"
-					listStyles={{
-						width: "250px",
-						fontSize: "20px",
-					}}
-				/>
-
-				{/* <===================Filter Modal Code ==========================> */}
-				<Modal
-					isOpen={isOpen}
-					onClose={onClose}
-					title="Filter"
-					submitText="Apply Now"
-					isCentered={{ base: false, lg: true }}
-					onSubmit={onFilterSubmit}
-					motionPreset="slideInBottom"
-					scrollBehavior="inside"
-					size={{ base: "sm", md: "lg" }}
-				>
-					<form>
-						<Form
-							{...{
-								parameter_list: form_parameter_list,
-								register,
-								control,
-								formValues,
-								errors,
-								gap: "2",
-							}}
-						/>
-						{/* <Input
-								label="TID"
-								name="tid"
-								type="number"
-								labelStyle={labelStyle}
-								value={formState.tid}
-								onChange={handleChange}
-							/>
-
-							<Input
-								label="Account Number"
-								name="account"
-								type="number"
-								labelStyle={labelStyle}
-								value={formState.account}
-								onChange={handleChange}
-							/>
-							<Input
-								label="Customer Mobile No."
-								name="customer_mobile"
-								type="number"
-								labelStyle={labelStyle}
-								value={formState.customer_mobile}
-								onChange={handleChange}
-							/>
-							<Calenders
-								label="From"
-								name="start_date"
-								labelStyle={labelStyle}
-								value={formState.start_date}
-								onChange={handleChange}
-								mb={{ base: 2, "2xl": "1rem" }}
-							/>
-							<Calenders
-								label="To"
-								name="tx_date"
-								labelStyle={labelStyle}
-								value={formState.tx_date}
-								onChange={handleChange}
-								mb={{ base: 2, "2xl": "1rem" }}
-							/>
-							<Input
-								label="Amount"
-								name="amount"
-								type="number"
-								labelStyle={labelStyle}
-								value={formState.amount}
-								onChange={handleChange}
-								inputLeftElement={
-									<Icon
-										name="rupee"
-										height="14px"
-										color="light"
-									/>
+				{actionBtnConfig.map(
+					({
+						id,
+						label,
+						icon,
+						parameter_list,
+						handleSubmit,
+						register,
+						control,
+						errors,
+						isSubmitting,
+						formValues,
+						handleFormSubmit,
+						submitButtonText,
+					}) => (
+						<>
+							<Button
+								key={id}
+								size="lg"
+								onClick={() =>
+									setOpenModalId(
+										id === openModalId ? null : id
+									)
 								}
-							/>
-							<Input
-								label="Tracking Number"
-								name="rr_no"
-								labelStyle={labelStyle}
-								value={formState.rr_no}
-								onChange={handleChange}
-								type="number"
-							/> */}
-					</form>
-				</Modal>
+							>
+								<Icon name={icon} width="sm" />
+								&nbsp;
+								<Text display={{ base: "none", md: "flex" }}>
+									{label}
+								</Text>
+							</Button>
+							<Modal
+								isOpen={openModalId === id}
+								onClose={() => setOpenModalId(null)}
+								title={label}
+							>
+								<form onSubmit={handleSubmit(handleFormSubmit)}>
+									<Flex
+										direction="column"
+										w="100%"
+										gap="8"
+										mb="4"
+									>
+										<Form
+											{...{
+												parameter_list,
+												register,
+												control,
+												formValues,
+												errors,
+												gap: "2",
+											}}
+										/>
+										<Button
+											w="100%"
+											size="lg"
+											type="submit"
+											loading={isSubmitting}
+										>
+											{submitButtonText}
+										</Button>
+									</Flex>
+								</form>
+							</Modal>
+						</>
+					)
+				)}
 			</Flex>
 		</Flex>
 	);
