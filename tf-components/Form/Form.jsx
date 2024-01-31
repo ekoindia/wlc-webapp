665538c1353b01b0afc4c39b47cbce60f4a8df91
shubhmjain30 <shubhmjain30@gmail.com>
2024-01-31 15:22:31 +0530

    feat: labelStyle prop added in form

--- tf-components/Form/Form.jsx
+++ tf-components/Form/Form.jsx
@@ -33,6 +33,7 @@ const Form = ({
 					{
 						name,
 						label,
+						labelStyle,
 						required = true,
 						value,
 						disabled,
@@ -75,7 +76,10 @@ const Form = ({
 							return (
 								<div>
 									{label ? (
-										<InputLabel required={required}>
+										<InputLabel
+											required={required}
+											{...labelStyle}
+										>
 											{label}
 										</InputLabel>
 									) : null}
@@ -98,6 +102,7 @@ const Form = ({
 										type="number"
 										fontSize="sm"
 										disabled={disabled}
+										labelStyle={labelStyle}
 										{...rest}
 										{...register(name, {
 											..._validations,
@@ -146,6 +151,7 @@ const Form = ({
 													onChange,
 													required,
 													disabled,
+													labelStyle,
 												}}
 												{...rest}
 											/>
@@ -194,6 +200,7 @@ const Form = ({
 													onChange,
 													required,
 													disabled,
+													labelStyle,
 												}}
 												{...rest}
 											/>
@@ -246,6 +253,7 @@ const Form = ({
 																multiSelectRenderer,
 															required,
 															isMulti: true,
+															labelStyle,
 														}}
 														{...rest}
 													/>
@@ -294,6 +302,7 @@ const Form = ({
 															onChange,
 															options:
 																list_elements,
+															labelStyle,
 														}}
 														{...rest}
 													/>
@@ -342,6 +351,7 @@ const Form = ({
 																required,
 																options:
 																	list_elements,
+																labelStyle,
 															}}
 															{...rest}
 														/>
@@ -394,6 +404,7 @@ const Form = ({
 															value,
 															disabled,
 															onChange,
+															labelStyle,
 														}}
 														{...rest}
 													/>
@@ -433,6 +444,7 @@ const Form = ({
 											type="text"
 											fontSize="sm"
 											disabled={disabled}
+											labelStyle={labelStyle}
 											{...rest}
 											{...register(name, {
 												..._validations,
