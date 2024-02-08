a7b7e2f22cc182e4564c4afb2e458830184759ea
shubhmjain30 <shubhmjain30@gmail.com>
2024-02-08 12:23:18 +0530

    ui: welcome card display logic

--- page-components/LoginPanel/LoginPanel.jsx
+++ page-components/LoginPanel/LoginPanel.jsx
@@ -1,10 +1,16 @@
-import { Box, Flex, SlideFade, Text } from "@chakra-ui/react";
+import { Flex, SlideFade, Text } from "@chakra-ui/react";
 import { Icon } from "components";
 import { useOrgDetailContext, useSession } from "contexts";
 import { fadeIn } from "libs/chakraKeyframes";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Login, SocialVerify, VerifyOtp, WelcomeCard } from "./children";
+
+const FOUR_MINUTES_IN_MS = 240000;
+
+const formatMobileNumber = (mobile) => {
+	return mobile.toString().replace(/(\d{3})(\d{3})(\d{4})/, "$1 $2 $3");
+};
 
 /**
  * This is the main component where all the Login related component rendered.
@@ -20,11 +26,15 @@ const LoginPanel = () => {
 	const [loginType, setLoginType] = useState("Mobile");
 	const [lastMobileFormatted, setLastMobileFormatted] = useState("");
 	const [lastUserName, setLastUserName] = useState("");
-
+	const [showWelcomeCard, setShowWelcomeCard] = useState(true);
 	const { orgDetail } = useOrgDetailContext();
 	const { isLoggedIn } = useSession();
 
-	// Get last login mobile number from localstorage and set it as default value
+	useEffect(() => {
+		if (number?.formatted?.length > 0) setShowWelcomeCard(false);
+	}, [number?.formatted]);
+
+	// Get last login mobile number from local storage and set it as default value
 	useEffect(() => {
 		if (number?.formatted?.length > 0) return;
 
@@ -36,7 +46,7 @@ const LoginPanel = () => {
 			lastRoute?.meta?.step === "VERIFY_OTP" &&
 			lastRoute?.meta?.type === "Mobile" &&
 			lastRoute?.meta?.mobile?.formatted &&
-			lastRoute?.at > Date.now() - 240000
+			lastRoute?.at > Date.now() - FOUR_MINUTES_IN_MS
 		) {
 			// Was the user on enter-OTP screen in the last 4 mins?
 			// Take them back there without resending OTP...
@@ -47,10 +57,7 @@ const LoginPanel = () => {
 		} else if (lastLogin?.type !== "Google" && lastLogin?.mobile > 1) {
 			// Format mobile number in the following format: +91 123 456 7890
 			// TODO: Fix Input component so that this is not required
-			const formatted_mobile = lastLogin.mobile
-				.toString()
-				.replace(/(\d{3})(\d{3})(\d{4})/, "$1 $2 $3");
-
+			const formatted_mobile = formatMobileNumber(lastLogin.mobile);
 			setNumber({
 				original: lastLogin.mobile,
 				formatted: formatted_mobile,
@@ -64,7 +71,7 @@ const LoginPanel = () => {
 		}
 	}, []);
 
-	// Cache current OTP-Verification step in localstorage,
+	// Cache current OTP-Verification step in local storage,
 	// so that OTP Verification can be continued when app is closed on mobile.
 	useEffect(() => {
 		if (step === "VERIFY_OTP") {
@@ -85,14 +92,11 @@ const LoginPanel = () => {
 
 	return (
 		<Flex
-			// direction={{ base: "column-reverse", md: "row" }}
 			w="full"
 			h="100vh"
-			// h={{ base: "100%", md: "100vh" }}
 			align="center"
 			justify="center"
 			bg="accent.light"
-			// bgGradient="linear(to-b, primary.light, primary.dark)"
 			bgImage={{
 				base: "none",
 				lg: "url('/login_bg_2.opt.svg')",
@@ -100,11 +104,9 @@ const LoginPanel = () => {
 			backgroundRepeat="no-repeat"
 			backgroundPosition="center center"
 			backgroundSize="cover"
-			// position={{ base: "fixed", md: "none" }}
 			overflowY="auto"
 		>
 			<Flex
-				direction={{ base: "column-reverse", md: "row" }}
 				w={{ base: "100%", md: "80%" }}
 				h={{ base: "100vh", lg: "auto" }}
 				maxW="1000px"
@@ -114,76 +116,87 @@ const LoginPanel = () => {
 				animation={`${fadeIn} ease-out 1s`}
 			>
 				{/* Description Box */}
-				<WelcomeCard
-					logo="/favicon.svg"
-					header={`Welcome to ${orgDetail.app_name}`}
-					features={[
-						"Your business partner to grow your revenue and digitize your business",
-						"Start earning today from your shop, office, home or anywhere",
-					]}
-				/>
+				<Flex
+					display={{
+						base: showWelcomeCard ? "flex" : "none",
+						md: "flex",
+					}}
+					w="100%"
+					h="100%"
+				>
+					<WelcomeCard
+						logo="/favicon.svg"
+						header={`Welcome to ${orgDetail.app_name}`}
+						features={[
+							"Your business partner to grow your revenue and digitize your business",
+							"Start earning today from your shop, office, home or anywhere",
+						]}
+						onClick={() => setShowWelcomeCard(false)}
+					/>
+				</Flex>
 
 				{/* Login Box */}
-				<Box
+				<Flex
+					display={{
+						base: showWelcomeCard ? "none" : "block",
+						md: "block",
+					}}
 					flex={1}
+					w="100%"
 					minW={{ base: "300px", lg: "350px" }}
 					h={{ base: "100vh", lg: "auto" }}
-					px={{ base: 5, "2xl": 7 }}
-					py={{ base: 7, "2xl": 10 }}
-					// my={{ base: "auto", md: "0" }}
 					boxShadow="0px 3px 20px #00000005"
 					bg="white"
 				>
 					{step === "LOGIN" && (
 						<Login
-							setStep={setStep}
-							number={number}
-							setNumber={setNumber}
-							setEmail={setEmail}
-							setLoginType={setLoginType}
-							lastUserName={lastUserName}
-							lastMobileFormatted={lastMobileFormatted}
+							{...{
+								number,
+								setNumber,
+								setStep,
+								setEmail,
+								setLoginType,
+								lastUserName,
+								lastMobileFormatted,
+							}}
 						/>
 					)}
 					{step === "VERIFY_OTP" && (
 						<SlideFade offsetX={100} offsetY={0} in={true}>
-							<VerifyOtp
-								loginType={loginType}
-								setStep={setStep}
-								number={number}
-							/>
+							<VerifyOtp {...{ number, loginType, setStep }} />
 						</SlideFade>
 					)}
 					{step === "SOCIAL_VERIFY" && (
 						<SlideFade offsetX={100} offsetY={0} in={true}>
 							<SocialVerify
-								setStep={setStep}
-								number={number}
-								email={email}
-								setNumber={setNumber}
+								{...{ email, number, setNumber, setStep }}
 							/>
 						</SlideFade>
 					)}
-				</Box>
+				</Flex>
 			</Flex>
-			<Flex
-				pos="absolute"
-				bottom="6px"
-				right="6px"
-				fontWeight="medium"
-				fontSize="xs"
-				color={{ base: "dark", md: "white" }}
-				opacity="0.4"
-			>
-				<Link href="/privacy">
-					<Flex gap="1" align="center">
-						<Text display="inline" lineHeight="1">
-							Privacy Policy
-						</Text>
-						<Icon name="open-in-new" size="xs" />
-					</Flex>
-				</Link>
-			</Flex>
+
+			{/* Privacy Policy Link */}
+			{showWelcomeCard ? null : (
+				<Flex
+					pos="absolute"
+					bottom="6px"
+					right="6px"
+					fontWeight="medium"
+					fontSize="xs"
+					color={{ base: "dark", md: "white" }}
+					opacity="0.4"
+				>
+					<Link href="/privacy">
+						<Flex gap="1" align="center">
+							<Text display="inline" lineHeight="1">
+								Privacy Policy
+							</Text>
+							<Icon name="open-in-new" size="xs" />
+						</Flex>
+					</Link>
+				</Flex>
+			)}
 		</Flex>
 	);
 };
