3edf9acdbcf54afb639d9f3f7f663fad91423c84
shubhmjain30 <shubhmjain30@gmail.com>
2024-02-16 01:02:29 +0530

    feat: config added for cdm

---
+++ page-components/Admin/PricingCommission/Cdm/CdmBankConfig.js
@@ -0,0 +1,151 @@
+/**
+ * Represents the configuration for CDM banks.
+ * @typedef {Object} CdmBankConfig
+ * @property {number} bank_id - The ID of the bank.
+ * @property {string} bank_label - The label of the bank.
+ * @property {string} bank_name - The name of the bank.
+ * @property {Object} validation - The validation rules for the bank.
+ * @property {Object|boolean} validation.percentage - The percentage validation rules for the bank. Can be an object with `min` and `max` properties or `false` if not applicable.
+ * @property {Object|boolean} validation.fixed - The fixed validation rules for the bank. Can be an object with `min` and `max` properties or `false` if not applicable.
+ */
+export const CdmBankConfig = {
+	1: {
+		bank_id: 1,
+		bank_label: "SBI Counter & CDM",
+		bank_name: "SBI",
+		desc: "SBI CDM without Card & Counter Deposit without Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.08,
+				max: 2,
+			},
+			fixed: {
+				min: 25,
+				max: 200,
+			},
+		},
+	},
+	2: {
+		bank_id: 2,
+		bank_label: "ICICI Counter & CDM",
+		bank_name: "ICICI",
+		desc: "ICICI CDM without Card & Counter Deposit without Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.4,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	3: {
+		bank_id: 3,
+		bank_label: "SBI Counter Deposit (Challan)",
+		bank_name: "SBI-2",
+		desc: "SBI Counter Deposit with Challan Slip",
+		validation: {
+			percentage: false,
+			fixed: {
+				min: 55,
+				max: 200,
+			},
+		},
+	},
+	5: {
+		bank_id: 5,
+		bank_label: "Axis Counter & CDM",
+		bank_name: "Axis",
+		desc: "Axis CDM without Card & Counter Deposit without Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.4,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	// 7: {
+	// 	bank_id: 7,
+	// 	bank_label: "SBI-API",
+	// 	bank_name: "SBI-API-NEW",
+	// 	validation: {
+	// 		percentage: false, //?
+	// 		fixed: false,
+	// 	},
+	// },
+	8: {
+		bank_id: 8,
+		bank_label: "KVB Counter & CDM",
+		bank_name: "KYB",
+		desc: "KVB CDM with Card & Counter Deposit with Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.5,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	10: {
+		bank_id: 10,
+		bank_label: "ICICI Counter Deposit (Challan)",
+		bank_name: "ICICI-H2H",
+		desc: "ICICI Counter Deposit with Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.2,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	11: {
+		bank_id: 11,
+		bank_label: "IDBI Counter Deposit (Challan)",
+		bank_name: "IDBI",
+		desc: "IDBI Counter Deposit with Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.5,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	// 21: {
+	// 	bank_id: 21,
+	// 	bank_label: "Yes Bank Turbo (Virtual)",
+	// 	bank_name: "YES Bank",
+	// desc: "Yes Bank Turbo E-value Load",
+	// 	validation: {
+	// 		percentage: false, //?
+	// 		fixed: false,
+	// 	},
+	// },
+	30: {
+		bank_id: 30,
+		bank_label: "HDFC Counter & CDM",
+		bank_name: "HDFC Bank",
+		desc: "HDFC CDM without Card & Counter Deposit without Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.4,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+	31: {
+		bank_id: 31,
+		bank_label: "PNB Counter & CDM",
+		bank_name: "PNB",
+		desc: "PNB CDM without Card & Counter Deposit without Challan Slip",
+		validation: {
+			percentage: {
+				min: 0.05,
+				max: 2,
+			},
+			fixed: false,
+		},
+	},
+};
