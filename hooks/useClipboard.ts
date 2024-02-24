212740edea698aa373eaaed449c9b8bb951e209c
shubhmjain30 <shubhmjain30@gmail.com>
2024-02-24 14:14:23 +0530

    refactor: useClipboard hook now manages separate state for each text

--- hooks/useClipboard.ts
+++ hooks/useClipboard.ts
@@ -1,39 +1,48 @@
-import { useState } from "react";
-type CopyState = "READY" | "SUCCESS" | Error;
+import { useCallback, useState } from "react";
+type CopyState = { [key: string]: "READY" | "SUCCESS" | Error };
 
 /**
- * useClipboard - A hook to copy text to the clipboard
- * @param param0
+ * useClipboard - A hook to copy text to the clipboard. It manages a separate state for each text.
+ * @param {number} delay - The delay in milliseconds after which the state for a text is reset. Default is 2500.
+ * @returns {Function} copy - A function that takes a string and copies it to the clipboard.
+ * @returns {Object} state - An object that contains the state for each text. The state can be "READY", "SUCCESS", or an Error object.
  * @example
  * const { copy, state } = useClipboard();
  * copy("Text to copy");
- * if (state === "SUCCESS") {
+ * if (state["Text to copy"] === "SUCCESS") {
  *  alert("Copied to clipboard!");
  * }
  */
 const useClipboard = ({ delay = 2500 } = {}) => {
-	const [state, setState] = useState<CopyState>("READY");
-	const [copyTimeout, setCopyTimeout] =
-		useState<ReturnType<typeof setTimeout>>();
+	const [state, setState] = useState<CopyState>({});
+	console.log("state", state);
 
-	function handleCopyResult(result: CopyState) {
-		setState(result);
-		clearTimeout(copyTimeout);
-		setCopyTimeout(setTimeout(() => setState("READY"), delay));
-	}
+	const copy = useCallback(
+		(valueToCopy: string) => {
+			const copyText = async () => {
+				try {
+					await navigator.clipboard.writeText(valueToCopy);
+					setState((prev) => ({ ...prev, [valueToCopy]: "SUCCESS" }));
 
-	function copy(valueToCopy: string) {
-		if ("clipboard" in navigator) {
-			navigator.clipboard
-				.writeText(valueToCopy)
-				.then(() => handleCopyResult("SUCCESS"))
-				.catch(
-					(error) => error instanceof Error && handleCopyResult(error)
-				);
-		} else {
-			handleCopyResult(new Error("Clipboard is not supported"));
-		}
-	}
+					setTimeout(() => {
+						setState((prev) => {
+							const newState = { ...prev };
+							delete newState[valueToCopy];
+							return newState;
+						});
+					}, delay);
+				} catch (error) {
+					setState((prev) => ({
+						...prev,
+						[valueToCopy]: new Error(error.message),
+					}));
+				}
+			};
+
+			copyText();
+		},
+		[delay]
+	);
 
 	return { copy, state };
 };
