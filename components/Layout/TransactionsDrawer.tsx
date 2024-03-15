3edbc5df1b30587cdb4b94fe4ce1e1291472ffbd
shubhmjain30 <shubhmjain30@gmail.com>
2024-03-15 12:50:37 +0530

    refactor: TransactionDrawer code

--- components/Layout/TransactionsDrawer.tsx
+++ components/Layout/TransactionsDrawer.tsx
@@ -1,6 +1,5 @@
 import {
 	Avatar,
-	Box,
 	Divider,
 	Drawer,
 	DrawerContent,
@@ -10,243 +9,191 @@ import {
 	Text,
 	useDisclosure,
 } from "@chakra-ui/react";
-import { TransactionIds } from "constants/EpsTransactions";
 import { OtherMenuItems } from "constants/SidebarMenu";
+import { InteractionBehavior } from "constants/trxnFramework";
 import { useMenuContext } from "contexts";
 import useHslColor from "hooks/useHslColor";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { IcoButton, Icon } from "..";
+import { Details, Icon } from "..";
+
+interface InteractionList {
+	id: number;
+	behavior: number;
+	group_interaction_ids: string;
+	label: string;
+	icon: string;
+}
 
 /**
  * Component for displaying a drawer with transaction options.
  * @returns JSX.Element
  */
-const TransactionsDrawer = () => {
-	const router = useRouter();
+const TransactionsDrawer: React.FC = () => {
 	const { interactions } = useMenuContext();
-	let { role_tx_list, interaction_list } = interactions;
-	const btnRef = useRef();
-	const [billPaymentOptions, setBillPaymentOptions] = useState([]);
-	const [interactionList, setInteractionList] = useState([
+	const { interaction_list } = interactions;
+	const btnRef = useRef<HTMLButtonElement>(null);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [interactionList, setInteractionList] = useState<InteractionList[]>([
 		...interaction_list,
 	]);
-	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	useEffect(() => {
-		// Remove "other..." entries from the list
-		let _filteredInteractionList = interaction_list.filter(
-			(tx) => OtherMenuItems.indexOf(tx.id) === -1
+		let _interactionList = interaction_list.filter(
+			(tx: InteractionList) => OtherMenuItems.indexOf(tx.id) === -1
 		);
-
-		setInteractionList(_filteredInteractionList.slice(0, 3));
+		setInteractionList(_interactionList);
 	}, [interaction_list]);
 
-	useEffect(() => {
-		const billPayment = role_tx_list[TransactionIds.BILL_PAYMENT];
-		if (!billPayment) return;
-
-		const group_interaction_ids = billPayment.group_interaction_ids
-			.split(",")
-			.map(Number);
-		const bbps_tx_list = group_interaction_ids
-			.filter((id) => id in role_tx_list)
-			.map((id) => ({ id, ...role_tx_list[id] }));
-
-		setBillPaymentOptions(
-			bbps_tx_list.length > 8
-				? [
-						...bbps_tx_list.slice(0, 7),
-						{ id: null, label: "More", icon: "more-horiz" },
-				  ]
-				: bbps_tx_list
-		);
-	}, [role_tx_list]);
-
-	/**
-	 * Handles click on bill payment option.
-	 * @param {number | null} id - The id of the clicked option.
-	 */
-	const handleBillPaymentOptionClick = (id: number | null) => {
-		router.push(
-			`transaction/${TransactionIds.BILL_PAYMENT}/` + (id ? `${id}` : "")
-		);
-	};
-
 	return (
-		<>
-			<Flex
-				direction="column"
-				gap="1"
-				w="100%"
-				h="100%"
-				align="center"
-				justify="center"
-				onClick={onOpen}
-				_active={{ background: "transparent" }}
-			>
-				<Icon
-					ref={btnRef}
-					name="transaction"
-					size="sm"
-					color="light"
-					whileTap={{ scale: 0.6 }}
-				>
-					Open
-				</Icon>
-				<Text fontSize="10px" fontWeight="medium" noOfLines={2}>
-					Transaction
-				</Text>
-			</Flex>
-			<Drawer
-				isOpen={isOpen}
-				placement="bottom"
-				onClose={onClose}
-				finalFocusRef={btnRef}
-			>
-				<DrawerOverlay />
-				<DrawerContent
-					maxH="90%"
-					w="100%"
-					py="8px"
-					borderTopRadius="10px"
-					gap="2"
-					pb="56px"
-				>
-					<Flex
-						w="100%"
-						align="center"
-						justify="space-between"
-						pl="20px"
-					>
-						<Text fontSize="lg" fontWeight="semibold">
-							Start a Transaction
-						</Text>
-						<Flex
-							direction="row-reverse"
-							onClick={onClose}
-							w="20%"
-							pr="20px"
-						>
-							<Icon
-								size="xs"
-								name="close"
-								color="light"
-								_active={{ color: "error" }}
-							/>
-						</Flex>
-					</Flex>
-					<Divider />
-					{/* Common Transactions */}
-					<Flex direction="column" gap="2" pb="1" px="20px">
-						{interactionList?.map(({ label, icon, id }, index) => {
-							return (
-								<>
-									<Interaction
-										{...{ key: index, label, icon, id }}
-									/>
-									{interactionList.length - 1 !== index ? (
-										<Divider variant="dashed" />
-									) : null}
-								</>
-							);
-						})}
-					</Flex>
-					{/* Recharge & Bill Payments */}
-					<Flex direction="column" gap="4" px="20px">
-						<Text fontWeight="medium">
-							Recharge & Bill Payments
-						</Text>
-						<SimpleGrid
-							columns={4}
-							rowGap={4}
-							columnGap={6}
-							alignItems="flex-start"
-							justifyContent="center"
-						>
-							{billPaymentOptions?.map(
-								({ label, icon, id }, index) => (
-									<Box
-										key={index}
-										display="flex"
-										flexDirection="column"
-										alignItems="center"
-										justifyContent="center"
-									>
-										<IcoButton
-											title={label}
-											iconName={icon}
-											size="md"
-											theme="dark"
-											onClick={() =>
-												handleBillPaymentOptionClick(id)
-											}
-										/>
-										<Text
-											pt={{ base: "10px" }}
-											fontSize={{
-												base: "xs",
-												"2xl": "md",
-											}}
-											noOfLines={2}
-											textAlign="center"
-										>
-											{label}
-										</Text>
-									</Box>
-								)
+		<DrawerContainer {...{ isOpen, onOpen, onClose, btnRef }}>
+			{interactionList?.map(
+				(
+					{
+						id,
+						behavior,
+						group_interaction_ids,
+						label,
+						icon,
+					}: InteractionList,
+					index: number
+				) => {
+					return (
+						<>
+							{group_interaction_ids !== null &&
+							InteractionBehavior.GRID === +behavior ? (
+								<GridInteraction
+									{...{
+										key: index,
+										id,
+										group_interaction_ids,
+										label,
+										icon,
+										onClose,
+									}}
+								/>
+							) : (
+								<Interaction
+									{...{
+										key: index,
+										id,
+										label,
+										icon,
+										onClose,
+									}}
+								/>
 							)}
-						</SimpleGrid>
-					</Flex>
-				</DrawerContent>
-			</Drawer>
-		</>
+							{interactionList.length - 1 !== index && (
+								<Divider variant="dashed" />
+							)}
+						</>
+					);
+				}
+			)}
+		</DrawerContainer>
 	);
 };
 
 export default TransactionsDrawer;
 
-/**
- * Component for displaying an interaction.
- * @param {Object} props - The props.
- * @param {string} props.label - The label of the interaction.
- * @param {string} props.icon - The icon of the interaction.
- * @param {number} props.id - The id of the interaction.
- * @returns JSX.Element
- */
+const DrawerContainer = ({ isOpen, onOpen, onClose, btnRef, children }) => (
+	<>
+		<Flex
+			direction="column"
+			gap="1"
+			w="100%"
+			h="100%"
+			align="center"
+			justify="center"
+			onClick={onOpen}
+		>
+			<Icon ref={btnRef} name="transaction" size="sm" color="light" />
+			<Text fontSize="10px" fontWeight="medium" noOfLines={2}>
+				Transaction
+			</Text>
+		</Flex>
+		<Drawer
+			isOpen={isOpen}
+			placement="bottom"
+			onClose={onClose}
+			finalFocusRef={btnRef}
+		>
+			<DrawerOverlay />
+			<DrawerContent maxH="80%" w="100%" borderTopRadius="10px" pb="22px">
+				<DrawerHeader onClose={onClose} />
+				<Divider />
+				<Flex
+					className="customScrollbars"
+					direction="column"
+					overflowY="scroll"
+					gap="2"
+				>
+					<Flex direction="column" gap="2" py="2" px="20px">
+						{children}
+					</Flex>
+				</Flex>
+			</DrawerContent>
+		</Drawer>
+	</>
+);
+
+const DrawerHeader = ({ onClose }) => (
+	<Flex w="100%" align="center" justify="space-between" pl="20px" py="10px">
+		<Text fontSize="lg" fontWeight="semibold">
+			Start a Transaction
+		</Text>
+		<Flex direction="row-reverse" onClick={onClose} w="20%" pr="28px">
+			<Icon
+				size="xs"
+				name="close"
+				color="light"
+				_active={{ color: "error" }}
+			/>
+		</Flex>
+	</Flex>
+);
+
 const Interaction: React.FC<{
+	id: number;
 	label: string;
 	icon: string;
+	onClose: Function;
+}> = ({ id, label, icon, onClose }) => {
+	return <InteractionItem {...{ id, label, icon, onClose }} />;
+};
+
+const InteractionItem: React.FC<{
 	id: number;
-}> = ({ label, icon, id }) => {
+	label: string;
+	icon: string;
+	onClose: Function;
+	isGridInteraction?: boolean;
+}> = ({ id, label, icon, onClose, isGridInteraction }) => {
 	const { h } = useHslColor(label);
+
 	const router = useRouter();
+
 	const onInteractionClick = (id) => {
 		router.push(`transaction/${id}`);
+		onClose();
 	};
 	return (
 		<Flex
 			align="center"
 			gap="4"
-			py="2"
-			onClick={() => onInteractionClick(id)}
+			py="1"
+			onClick={
+				isGridInteraction
+					? null
+					: () => {
+							onInteractionClick(id);
+							onClose();
+					  }
+			}
 		>
 			<Flex gap="4" w="100%" align="center">
-				<Avatar
-					size={{ base: "sm", md: "md" }}
-					name={icon ? null : label}
-					border={`2px solid hsl(${h},80%,90%)`}
-					bg={`hsl(${h},80%,95%)`}
-					color={`hsl(${h},80%,30%)`}
-					icon={
-						<Icon
-							size="sm"
-							name={icon}
-							color={`hsl(${h},80%,30%)`}
-						/>
-					}
-				/>
-				<Text fontSize="sm  " fontWeight="medium">
+				<Icon name={icon} size="md" color={`hsl(${h},80%,30%)`} />
+				<Text fontSize="sm" fontWeight="medium">
 					{label}
 				</Text>
 			</Flex>
@@ -256,6 +203,116 @@ const Interaction: React.FC<{
 				color="light"
 				_active={{ color: `hsl(${h},80%,30%)` }}
 			/>
+		</Flex>
+	);
+};
+
+const GridInteraction: React.FC<{
+	id: number;
+	group_interaction_ids: string;
+	label: string;
+	icon: string;
+	onClose: Function;
+}> = ({ id, group_interaction_ids, label, icon, onClose }) => {
+	const { interactions } = useMenuContext();
+	const { role_tx_list } = interactions;
+
+	const groupInteractions = group_interaction_ids
+		.split(",")
+		.map(Number)
+		.filter((id) => id in role_tx_list)
+		.map((id) => ({ id, ...role_tx_list[id] }));
+
+	const isGridInteraction = groupInteractions?.length > 1;
+
+	return (
+		<Flex direction="column" gap="4">
+			<Details
+				summary={
+					<InteractionItem
+						{...{ id, label, icon, onClose, isGridInteraction }}
+					/>
+				}
+			>
+				<Flex id="test" direction="column" py="4">
+					<GridInteractionBox
+						{...{ id, groupInteractions, onClose }}
+					/>
+				</Flex>
+			</Details>
+		</Flex>
+	);
+};
+
+const GridInteractionBox: React.FC<{
+	id: number;
+	groupInteractions: any[];
+	onClose: Function;
+}> = ({ id, groupInteractions, onClose }) => {
+	return (
+		<SimpleGrid
+			columns={4}
+			rowGap="4"
+			alignItems="flex-start"
+			justifyContent="center"
+		>
+			{groupInteractions?.map(
+				({ id: group_interaction_id, label, icon }, index) => (
+					<GridInteractionItem
+						key={`${index}-${label}`}
+						{...{
+							id,
+							group_interaction_id,
+							label,
+							icon,
+							onClose,
+						}}
+					/>
+				)
+			)}
+		</SimpleGrid>
+	);
+};
+
+const GridInteractionItem: React.FC<{
+	id: number;
+	group_interaction_id: number;
+	label: string;
+	icon: string;
+	onClose: Function;
+}> = ({ id, group_interaction_id, label, icon, onClose }) => {
+	const router = useRouter();
+
+	const onInteractionClick = (id, group_interaction_id) => {
+		router.push(`transaction/${id}/${group_interaction_id || ""}`);
+		onClose();
+	};
+
+	return (
+		<Flex
+			direction="column"
+			align="center"
+			justify="center"
+			onClick={() => {
+				onInteractionClick(id, group_interaction_id);
+				onClose();
+			}}
+		>
+			<Avatar
+				size={{ base: "sm", md: "md" }}
+				name={icon ? null : label}
+				bg="inputlabel"
+				color="white"
+				icon={<Icon size="sm" name={icon} color="white" />}
+			/>
+			<Text
+				pt={{ base: "10px" }}
+				fontSize={{ base: "xs", "2xl": "md" }}
+				noOfLines={2}
+				textAlign="center"
+			>
+				{label}
+			</Text>
 		</Flex>
 	);
 };
