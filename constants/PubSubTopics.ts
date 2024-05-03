bc0b5fc277b5cbdc9bc24aff0b8d95ccd408a04d
Kumar Abhishek <manustays@users.noreply.github.com>
2024-05-03 13:41:50 +0530

    feat: add pubsub topic to show generic dialog based features from anywhere

--- constants/PubSubTopics.ts
+++ constants/PubSubTopics.ts
@@ -4,6 +4,14 @@
  */
 export const PubSubTopics = {
 	/**
+	 * Trigger global dialog-based features:
+	 * - Show "Raise Issue" dialog
+	 * - Show "Camera" dialog for image capture
+	 * - Show "File Viewer" dialog for viewing images, videos, or embedded web-pages
+	 */
+	SHOW_DIALOG_FEATURE: "show.dialog.feature",
+
+	/**
 	 * Android-action response listener.
 	 * Any response coming from the Android app will be pushed to this topic.
 	 */
