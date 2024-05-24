e6518ebcc65974998841f4613ac524b91b2c2af8
Kumar Abhishek <manustays@users.noreply.github.com>
2024-05-24 23:08:17 +0530

    feat: custom geolocation hook with locationWatcher

---
+++ hooks/useGeolocation.ts
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+
+interface UseGeolocationOptions {
+	highAccuracy?: boolean;
+	maximumAge?: number;
+	timeout?: number;
+	watchPosition?: boolean;
+	positionWatchTimeBuffer?: number;
+	decimalPlaces?: number;
+	maxWatchUpdates?: number;
+}
+
+interface GeolocationResult {
+	latitude: number | null;
+	longitude: number | null;
+	accuracy: number | null;
+	error: string | null;
+}
+
+const useGeolocation = ({
+	highAccuracy = false,
+	maximumAge = 60000,
+	timeout = Infinity,
+	watchPosition = false,
+	positionWatchTimeBuffer = 6000,
+	decimalPlaces = 6,
+	maxWatchUpdates = 0,
+}: UseGeolocationOptions): GeolocationResult => {
+	const [position, setPosition] = useState<GeolocationResult>({
+		latitude: null,
+		longitude: null,
+		accuracy: null,
+		error: null,
+	});
+	const [updateCount, setUpdateCount] = useState<number>(0);
+
+	useEffect(() => {
+		let watchId: number | null = null;
+		let interval: any = null;
+
+		const successHandler = (pos) => {
+			const {
+				coords: { latitude, longitude, accuracy },
+			} = pos;
+
+			setPosition({
+				latitude: parseFloat(latitude.toFixed(decimalPlaces)),
+				longitude: parseFloat(longitude.toFixed(decimalPlaces)),
+				accuracy,
+				error: null,
+			});
+
+			setUpdateCount((prevCount) => prevCount + 1);
+		};
+
+		const errorHandler = (error) => {
+			setPosition({
+				latitude: null,
+				longitude: null,
+				accuracy: null,
+				error: error.message,
+			});
+		};
+
+		const options = {
+			enableHighAccuracy: highAccuracy,
+			maximumAge,
+			timeout,
+		};
+
+		const stopWatching = () => {
+			if (watchId !== null) {
+				navigator.geolocation.clearWatch(watchId);
+				watchId = null;
+			}
+			if (interval !== null) {
+				clearInterval(interval);
+				interval = null;
+			}
+		};
+
+		if (watchPosition) {
+			watchId = navigator.geolocation.watchPosition(
+				successHandler,
+				errorHandler,
+				options
+			);
+
+			interval = setInterval(() => {
+				if (watchId !== null) {
+					navigator.geolocation.clearWatch(watchId);
+					watchId = navigator.geolocation.watchPosition(
+						successHandler,
+						errorHandler,
+						options
+					);
+				}
+			}, positionWatchTimeBuffer);
+
+			return () => {
+				stopWatching();
+			};
+		} else {
+			navigator.geolocation.getCurrentPosition(
+				successHandler,
+				errorHandler,
+				options
+			);
+		}
+
+		if (maxWatchUpdates > 0 && updateCount >= maxWatchUpdates) {
+			stopWatching();
+		}
+
+		return () => {
+			stopWatching();
+		};
+	}, [
+		highAccuracy,
+		maximumAge,
+		timeout,
+		watchPosition,
+		positionWatchTimeBuffer,
+		decimalPlaces,
+		maxWatchUpdates,
+		updateCount,
+	]);
+
+	return position;
+};
+
+export default useGeolocation;
