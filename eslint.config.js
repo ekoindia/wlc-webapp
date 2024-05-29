7c0968c49efe770c44f3e08d1281c0fb93a13a64
shubhmjain30 <shubhmjain30@gmail.com>
2024-05-29 10:47:38 +0530

    refactor: migrated from eslintrc.json to eslint.config.js due to deprecation in ESLint v9.0.0

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import jsdoc from "eslint-plugin-jsdoc";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+	// Register plugins
+	{
+		plugins: {
+			// next: next,
+			jsdoc: jsdoc,
+			prettier: prettier,
+			// storybook: storybook,
+		},
+	},
+
+	// Apply recommended configurations from plugins
+	// next.configs["core-web-vitals"],
+	jsdoc.configs["flat/recommended"],
+	// storybook.configs["recommended"],
+
+	// Global configuration
+	{
+		ignores: ["**/*.BAK.*", "**/*.stories.*", "**/*.mocks.*", "**/*.min.*"],
+
+		languageOptions: {
+			globals: {
+				React: "readonly",
+				describe: "readonly",
+				it: "readonly",
+				expect: "readonly",
+				JSX: "readonly",
+			},
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+
+		rules: {
+			"prettier/prettier": "error",
+			"comma-spacing": ["error", { before: false, after: true }],
+			"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+			"jsdoc/require-description": "warn",
+			semi: ["error", "always", { omitLastInOneLineBlock: true }],
+			"no-unused-vars": [
+				"error",
+				{
+					vars: "all",
+					args: "after-used",
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+				},
+			],
+			"no-warning-comments": [
+				"warn",
+				{
+					terms: ["todo", "fixme"],
+					decoration: ["/", "*", "["],
+				},
+			],
+		},
+	},
+
+	// Specific overrides for particular files
+	{
+		files: ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+		rules: {
+			"storybook/hierarchy-separator": "error",
+		},
+	},
+];
