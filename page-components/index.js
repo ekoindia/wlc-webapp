cc3a9c0ac71ecdb62e8fd77cb9998d6c5a68bab2
Kumar Abhishek <manustays@users.noreply.github.com>
2024-06-12 16:53:06 +0530

    feat: add LogonPanel component for login page
    which shows classic login card or the CMS page based on config

--- page-components/index.js
+++ page-components/index.js
@@ -3,6 +3,7 @@ export { Commissions } from "./Commissions";
 export { History } from "./History";
 export { Home } from "./Home";
 export { ImageEditor } from "./ImageEditor";
+export { LandingPanel } from "./LandingPanel";
 export { LoginPanel } from "./LoginPanel";
 export { Profile } from "./Profile";
 export { RaiseIssueCard } from "./RaiseIssueCard";
