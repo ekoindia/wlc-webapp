34fa5e7745bf1cbf390a6a7b0878467db434cd2c
Kumar Abhishek <manustays@users.noreply.github.com>
2024-06-27 14:07:49 +0530

    feat: separate Section component for Admin Configurations

---
+++ page-components/Admin/Configurations/Section.jsx
@@ -0,0 +1,31 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+/**
+ * Section card component for the configuration page
+ * @param root0
+ * @param root0.title
+ * @param root0.children
+ */
+export const Section = ({ title, children }) => {
+	return (
+		<Flex
+			direction="column"
+			gap={{ base: 4, md: 8 }}
+			bg="white"
+			borderRadius={6}
+			p={{ base: 4, md: 8 }}
+		>
+			{title ? (
+				<Text as="h2" fontSize="24px" fontWeight="600" color="#444">
+					{title}
+				</Text>
+			) : null}
+			<Flex
+				direction={{ base: "column", md: "row" }}
+				gap={{ base: 4, md: 8, lg: 12 }}
+			>
+				{children}
+			</Flex>
+		</Flex>
+	);
+};
