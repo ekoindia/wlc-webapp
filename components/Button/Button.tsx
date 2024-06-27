c7307fb9ab789c7a6721fd9ff2735c5edeb11251
Kumar Abhishek <manustays@users.noreply.github.com>
2024-06-27 13:59:29 +0530

    refactor: convert to Typescript

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,16 +1,43 @@
-import { Button as ChakraButton } from "@chakra-ui/react";
+import {
+	Button as ChakraButton,
+	ButtonProps as ChakraButtonProps,
+} from "@chakra-ui/react";
 import { Icon } from "components";
-import { forwardRef } from "react";
+import { ForwardedRef, forwardRef, isValidElement, ReactElement } from "react";
+
+interface ButtonProps extends ChakraButtonProps {
+	variant?:
+		| "primary"
+		| "accent"
+		| "outline"
+		| "success"
+		| "error"
+		| "ghost"
+		| "link"
+		| "solid"
+		| "accent_outline"
+		| "primary_outline"
+		| "unstyled";
+	disabled?: boolean;
+	loading?: boolean;
+	size?: "lg" | "md" | "sm" | "xs";
+	icon?: string | ReactElement<any, any>;
+	iconPosition?: "left" | "right";
+	iconSpacing?: string;
+	iconStyle?: React.CSSProperties;
+	onClick?: () => void;
+	children?: React.ReactNode;
+}
 
 /**
  * A <Button> component
  * @param 	{object}	prop	Properties passed to the component
- * @param	{string}	[prop.variant="accent"]	Button variant:	"primary" | "accent" | "outline" | "ghost" | "link" | "solid" | "unstyled"
- * @param	{boolean}	[prop.disabled=false]	Disable the button
- * @param	{boolean}	[prop.loading=false]	Show loading state
+ * @param	{string}	[prop.variant]	Button variant:	"primary" | "accent" | "outline" | "ghost" | "link" | "solid" | "unstyled"
+ * @param	{boolean}	[prop.disabled]	Disable the button
+ * @param	{boolean}	[prop.loading]	Show loading state
  * @param	{string}	[prop.size]	Size of the button: "lg" | "md" | "sm" | "xs"
  * @param	{string}	[prop.icon]	Icon to show in the button
- * @param	{string}	[prop.iconPosition="left"]	Position of the icon
+ * @param	{string}	[prop.iconPosition]	Position of the icon
  * @param	{string}	[prop.iconSpacing]	Spacing between the icon and the text
  * @param	{Function}	[prop.onClick]	Click handler
  * @param	{string}	[prop.children]	Children elements of the button
@@ -18,7 +45,7 @@ import { forwardRef } from "react";
  * @param	{React.Ref}	ref	Reference to the button element
  * @example	`<Button onClick={()=>{...}}>Click Me</Button>`
  */
-const Button = forwardRef(
+const Button = forwardRef<unknown, ButtonProps>(
 	(
 		{
 			variant = "accent",
@@ -34,11 +61,16 @@ const Button = forwardRef(
 			children,
 			...rest
 		},
-		ref
+		ref: ForwardedRef<unknown>
 	) => {
-		const IconComp = icon ? (
-			<Icon name={icon} size={size} {...iconStyle} />
-		) : null;
+		let IconComp = null;
+		if (icon) {
+			if (isValidElement(icon)) {
+				IconComp = icon;
+			} else if (typeof icon === "string") {
+				IconComp = <Icon name={icon} size={size} {...iconStyle} />;
+			}
+		}
 
 		const default_radius =
 			size === "lg"
@@ -57,8 +89,10 @@ const Button = forwardRef(
 				size={size}
 				onClick={onClick}
 				iconSpacing={children ? iconSpacing : null}
-				leftIcon={icon && iconPosition === "left" ? IconComp : null}
-				rightIcon={icon && iconPosition === "right" ? IconComp : null}
+				leftIcon={IconComp && iconPosition === "left" ? IconComp : null}
+				rightIcon={
+					IconComp && iconPosition === "right" ? IconComp : null
+				}
 				borderRadius={borderRadius || default_radius}
 				ref={ref}
 				{...rest}
