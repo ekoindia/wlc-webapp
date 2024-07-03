79e1932d7a6fd601926ec4971fb1617b29aee590
shubhmjain30 <shubhmjain30@gmail.com>
2024-07-03 20:32:38 +0530

    refactor: added transaction & more component in bottom-app-bar

--- components/BottomAppBar/More.tsx
+++ components/BottomAppBar/More.tsx
@@ -1,11 +1,48 @@
+import { Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { useMenuContext } from "contexts";
-import ContentDrawer from "./ContentDrawer";
+import { useRef } from "react";
+import { BottomAppBarDrawerList } from ".";
+import { Drawer, Icon, StatusCard } from "..";
 
+/**
+ * The More component renders a button that opens a drawer containing additional menu items.
+ */
 const More = () => {
+	const btnRef = useRef<HTMLButtonElement>(null);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	const { menuList, otherList } = useMenuContext();
 	const list = [...menuList, ...otherList];
 
-	return <ContentDrawer {...{ list, title: "More", icon: "others" }} />;
+	return (
+		<>
+			<Flex
+				direction="column"
+				gap="1"
+				w="100%"
+				h="100%"
+				align="center"
+				justify="center"
+				onClick={onOpen}
+			>
+				<Icon ref={btnRef} name="others" size="sm" color="light" />
+				<Text fontSize="10px" fontWeight="medium" noOfLines={2}>
+					More
+				</Text>
+			</Flex>
+			<Drawer
+				id="more-drawer"
+				title="More"
+				isOpen={isOpen}
+				onOpen={onOpen}
+				onClose={onClose}
+				isFullHeight={false}
+				finalFocusRef={btnRef}
+			>
+				<StatusCard />
+				<BottomAppBarDrawerList {...{ list, onClose }} />
+			</Drawer>
+		</>
+	);
 };
 
 export default More;
--- components/BottomAppBar/Transactions.tsx
+++ components/BottomAppBar/Transactions.tsx
@@ -1,12 +1,45 @@
+import { Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { useMenuContext } from "contexts";
-import ContentDrawer from "./ContentDrawer";
+import { useRef } from "react";
+import { BottomAppBarDrawerList } from ".";
+import { Drawer, Icon } from "..";
 
+/**
+ * The Transactions component renders a button that opens a drawer containing transactions.
+ */
 const Transactions = () => {
+	const btnRef = useRef<HTMLButtonElement>(null);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	const { trxnList: list } = useMenuContext();
+
 	return (
-		<ContentDrawer
-			{...{ list, title: "Transactions", icon: "transaction" }}
-		/>
+		<>
+			<Flex
+				direction="column"
+				gap="1"
+				w="100%"
+				h="100%"
+				align="center"
+				justify="center"
+				onClick={onOpen}
+			>
+				<Icon ref={btnRef} name="transaction" size="sm" color="light" />
+				<Text fontSize="10px" fontWeight="medium" noOfLines={2}>
+					Transactions
+				</Text>
+			</Flex>
+			<Drawer
+				id="transaction-drawer"
+				title="Transactions"
+				isOpen={isOpen}
+				onOpen={onOpen}
+				onClose={onClose}
+				isFullHeight={false}
+				finalFocusRef={btnRef}
+			>
+				<BottomAppBarDrawerList {...{ list, onClose }} />
+			</Drawer>
+		</>
 	);
 };
 
