0900b2856cc4054902624f47ce0b6121d08514be
shubhmjain30 <shubhmjain30@gmail.com>
2024-07-04 18:09:39 +0530

    refactor: logic for producing menuList, trxnList & otherList removed from MenuContext

--- contexts/MenuContext.js
+++ contexts/MenuContext.js
@@ -1,438 +1,140 @@
-import { ActionIcon, useKBarReady } from "components/CommandBar";
-import {
-	AdminBlacklistMenuItems,
-	AdminOtherMenuItems,
-	adminSidebarMenu,
-	Endpoints,
-	OtherMenuItems,
-	sidebarMenu,
-	TransactionIds,
-} from "constants";
 import { fetcher, processTransactionData } from "helpers";
-import { useFeatureFlag } from "hooks";
-import { Priority, useRegisterActions } from "kbar";
-import { useRouter } from "next/router";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
-import { limitText } from "utils/textFormat";
-import { useOrgDetailContext } from "./OrgDetailContext";
-import { useUser } from "./UserContext";
+import { useSession } from ".";
 
-/**
- * A helper function to create the KBar actions array from all the visible transactions from transaction_list which are part of role_transaction_list.
- * @param {Array} interaction_list - List of all transactions
- * @param {Object} role_tx_list - All transaction_ids that are allowed to the user (mapped to the trxn details)
- * @returns {Array} Array of KBar actions
- */
-const generateTransactionActions = (
-	interaction_list,
-	role_tx_list,
-	router,
-	is_other_list = false
-) => {
-	const getTxAction = (tx, parent_id, is_group) => {
-		const _id = "" + (parent_id ? `${parent_id}/` : "") + tx.id;
-		const desc = tx.description || tx.desc || "";
-
-		return {
-			id: "tx/" + _id,
-			name: tx.label,
-			subtitle: limitText(desc, 60),
-			// keywords: tx.label + " " + (tx.desc || "") + (tx.category || ""),
-			icon: (
-				<ActionIcon
-					icon={tx.icon}
-					ext_icon={tx.ext_icon}
-					name={tx.label}
-					style="filled"
-				/>
-			),
-			priority: parent_id ? Priority.HIGH : Priority.NORMAL,
-			// section: "Services",
-			perform: is_group ? null : () => router.push("/transaction/" + _id),
-			parent: parent_id
-				? "tx/" + parent_id
-				: is_other_list
-				? "others"
-				: "start-a-tx",
-		};
-	};
-
-	const trxnList = is_other_list
-		? [
-				{
-					id: "others",
-					name: "Others...",
-					// keywords: "dmt bbps recharge billpay product earn send cashin cashout transfer",
-					icon: (
-						<ActionIcon
-							icon="others"
-							// color="gray.500"
-							style="filled"
-						/>
-					),
-					// shortcut: ["$mod+/"],
-					// section: "Services",
-				},
-		  ]
-		: [
-				{
-					id: "start-a-tx",
-					name: "Start a Transaction...",
-					// keywords: "dmt bbps recharge billpay product earn send cashin cashout transfer",
-					icon: (
-						<ActionIcon
-							icon="transaction"
-							color="primary.light"
-							style="filled"
-						/>
-					),
-					shortcut: ["$mod+/"],
-					// section: "Services",
-				},
-		  ];
-
-	// Cache for transactions that contain sub-transactions
-	const trxnGroups = [];
-
-	// Cache of all trxn-ids already processed
-	const processedTrxns = {};
-
-	// Process main transactions
-	interaction_list.forEach((tx) => {
-		if (!tx) {
-			return;
-		}
-		if (tx.id in role_tx_list && !(tx.id in processedTrxns)) {
-			let is_group = false;
-			processedTrxns[tx.id] = true;
-			// Is this a transaction group (Grid) (i.e, contains sub-transactions)?
-			if (tx.behavior == 7 && tx?.group_interaction_ids?.length) {
-				is_group = true;
-				trxnGroups.push({
-					tx: tx,
-					parent: "" + tx.id,
-				});
-			}
-			trxnList.push(getTxAction(tx, null, is_group));
-		}
-	});
-
-	// Recusrively process transaction groups...
-	while (trxnGroups.length > 0) {
-		const group = trxnGroups.shift();
-		const { tx, parent } = group;
-		const group_interaction_ids = tx.group_interaction_ids.split(",");
-		group_interaction_ids.forEach((id) => {
-			const subTx = role_tx_list[id];
-			if (subTx && !(id in processedTrxns)) {
-				const thisTx = {
-					id: id,
-					...subTx,
-				};
-				processedTrxns[id] = true;
-				let is_group = false;
-				// Is this a transaction group (i.e, contains sub-transactions)?
-				if (
-					subTx.behavior == 7 &&
-					subTx?.group_interaction_ids?.length > 0
-				) {
-					is_group = true;
-
-					// Check if this group has all child transactions already processed
-					const group_tx_ids =
-						thisTx.group_interaction_ids.split(",");
-					let all_processed = true;
-					group_tx_ids.forEach((tx_id) => {
-						if (!(tx_id in processedTrxns)) {
-							all_processed = false;
-						}
-					});
-
-					if (all_processed) {
-						is_group = false;
-					} else {
-						trxnGroups.push({
-							tx: thisTx,
-							parent: "" + (parent ? `${parent}/` : "") + id,
-						});
-					}
-				}
-				trxnList.push(getTxAction(thisTx, parent, is_group));
-			}
-		});
-	}
-
-	return trxnList;
-};
-
-/**
- * A helper function to create the KBar actions array from all the visible left-menu links.
- * @param {Array} menu_list - List of all Menu items with a label and a link.
- * @param {Object} router - Next.js router object
- * @returns {Array} Array of KBar actions
- */
-const generateMenuLinkActions = (menu_list, router) => {
-	const menuLinkActions = [];
-
-	// get current route path from Nextjs router
-	const currentRoute = router.pathname;
-
-	menu_list.forEach((menu) => {
-		if (menu.link != currentRoute) {
-			menuLinkActions.push({
-				id: "menulnk/" + menu.name,
-				name: menu.name,
-				icon: (
-					<ActionIcon
-						icon={menu.icon}
-						name={menu.name}
-						style="filled"
-					/>
-				),
-				// section: "Services",
-				perform: () => router.push(menu.link),
-			});
-		}
-	});
-	// console.log("menuLinkActions", menuLinkActions, menu_list);
-	return menuLinkActions;
+const SESSION_STORAGE_KEYS = {
+	INTERACTION_LIST: "interaction_list",
+	ROLE_TX_LIST: "role_tx_list",
+	CACHE_ACCESS_TOKEN: "cache_access_token",
+	TRXN_TYPE_PROD_MAP: "trxn_type_prod_map",
 };
 
 const MenuContext = createContext();
 
+/**
+ * Fetches and processes transaction data.
+ * @param {string} accessToken - The access token for the API request.
+ * @returns {Promise<object>} The processed data.
+ */
+const fetchData = async (accessToken) => {
+	const response = await fetcher(
+		`${process.env.NEXT_PUBLIC_API_BASE_URL}/transactions/wlc`,
+		{
+			token: accessToken,
+		}
+	);
+	console.log("response", response);
+
+	if (!response.length) {
+		throw new Error("No data received from the API");
+	}
+
+	return processTransactionData(response);
+};
+
 const MenuProvider = ({ children }) => {
-	const router = useRouter();
-	const { ready } = useKBarReady();
-	const { isLoggedIn, isAdmin, isAdminAgentMode, accessToken } = useUser();
-	const { orgDetail } = useOrgDetailContext();
-	const { metadata } = orgDetail;
-	const disabledFeatures = metadata?.disabled_features;
-
-	const [loading, setLoading] = useState(false);
-
+	const { isLoggedIn, isAdmin, accessToken } = useSession();
 	const [interactions, setInteractions] = useState({
 		interaction_list: [],
 		role_tx_list: {},
 	});
+	const [loading, setLoading] = useState(false);
 
-	const [appLists, setAppLists] = useState({
-		menuList: [],
-		trxnList: [],
-		otherList: [],
-	});
+	useEffect(() => {
+		const loadMenuData = async () => {
+			try {
+				setLoading(true);
 
-	const [trxnActions, setTrxnActions] = useState([]);
-	const [otherActions, setOtherActions] = useState([]);
+				const localInteractionList =
+					sessionStorage.getItem(
+						SESSION_STORAGE_KEYS.INTERACTION_LIST
+					) || "[]";
+				const localRoleTxList =
+					sessionStorage.getItem(SESSION_STORAGE_KEYS.ROLE_TX_LIST) ||
+					"{}";
+				const cachedAccessToken =
+					sessionStorage.getItem(
+						SESSION_STORAGE_KEYS.CACHE_ACCESS_TOKEN
+					) || "-";
+				const trxnTypeProdMap =
+					sessionStorage.getItem(
+						SESSION_STORAGE_KEYS.TRXN_TYPE_PROD_MAP
+					) || "{}";
 
-	const [_isFeatureEnabled, checkFeatureFlag] = useFeatureFlag();
-
-	const fetchData = async () => {
-		setLoading(true);
-		try {
-			const data = await fetcher(
-				process.env.NEXT_PUBLIC_API_BASE_URL + "/transactions/wlc",
-				{ token: accessToken }
-			);
-			if (data.length) {
-				const processedData = processTransactionData(data);
-				setInteractions(processedData);
-				Object.keys(processedData).forEach((key) =>
-					sessionStorage.setItem(
-						key,
-						JSON.stringify(processedData[key])
-					)
-				);
-				sessionStorage.setItem("cache_access_token", accessToken);
-			}
-		} catch (err) {
-			console.error("MenuProvider error: ", err);
-		} finally {
-			setLoading(false);
-		}
-	};
-
-	const getCachedData = () => ({
-		interaction_list: JSON.parse(
-			sessionStorage.getItem("interaction_list") || "[]"
-		),
-		role_tx_list: JSON.parse(
-			sessionStorage.getItem("role_tx_list") || "{}"
-		),
-		trxn_type_prod_map: JSON.parse(
-			sessionStorage.getItem("trxn_type_prod_map") || "{}"
-		),
-	});
-
-	const onUserLogin = () => {
-		const cachedData = getCachedData();
-		const cache_access_token =
-			sessionStorage.getItem("cache_access_token") || "-";
-		if (
-			cache_access_token === accessToken &&
-			cachedData.interaction_list.length &&
-			Object.keys(cachedData.role_tx_list).length
-		) {
-			setInteractions(cachedData);
-			setLoading(false);
-		} else {
-			fetchData();
-		}
-	};
-
-	const processInteractions = () => {
-		const { interaction_list, role_tx_list } = interactions ?? {};
-		const _trxnList = [];
-		const _otherList = [];
-		let _filteredMenuList = [];
-
-		const _menuList =
-			isAdmin && isAdminAgentMode !== true
-				? adminSidebarMenu
-				: sidebarMenu;
-
-		// Filter out Disabled features (from org-metadata) & Feature Flags !!!
-		const _feat = disabledFeatures
-			? JSON.parse(disabledFeatures)?.features
-			: [];
-		_menuList.forEach((item) => {
-			// Skip if the feature is disabled (from org-metadata)
-			if (_feat.includes(item.id)) {
-				return;
-			}
-
-			// Skip if the feature is disabled (from feature-flags)
-			if (
-				item.featureFlag &&
-				checkFeatureFlag(item.featureFlag) !== true
-			) {
-				return;
-			}
-
-			// Else, add to the menu list
-			_filteredMenuList.push(item);
-		});
-
-		if (interaction_list?.length > 0) {
-			interaction_list.forEach((tx) => {
-				if (isAdmin) {
-					if (AdminOtherMenuItems.includes(tx.id)) {
-						_otherList.push(tx);
-					} else if (isAdminAgentMode) {
-						if (OtherMenuItems.includes(tx.id)) {
-							_otherList.push(tx);
-						} else if (!AdminBlacklistMenuItems.includes(tx.id)) {
-							_trxnList.push(tx);
-						}
-					}
+				if (
+					localInteractionList !== "[]" &&
+					localRoleTxList !== "{}" &&
+					cachedAccessToken === accessToken
+				) {
+					const contextData = {
+						interaction_list: JSON.parse(localInteractionList),
+						role_tx_list: JSON.parse(localRoleTxList),
+						trxn_type_prod_map: JSON.parse(trxnTypeProdMap),
+					};
+					setInteractions(contextData);
+					console.log(
+						"[MenuContext]: Data loaded from cache",
+						contextData
+					);
 				} else {
-					if (OtherMenuItems.includes(tx.id)) {
-						_otherList.push(tx);
-					} else {
-						_trxnList.push(tx);
-					}
+					const processedData = await fetchData(accessToken);
+					setInteractions(processedData);
+
+					sessionStorage.setItem(
+						SESSION_STORAGE_KEYS.INTERACTION_LIST,
+						JSON.stringify(processedData.interaction_list)
+					);
+					sessionStorage.setItem(
+						SESSION_STORAGE_KEYS.ROLE_TX_LIST,
+						JSON.stringify(processedData.role_tx_list)
+					);
+					sessionStorage.setItem(
+						SESSION_STORAGE_KEYS.TRXN_TYPE_PROD_MAP,
+						JSON.stringify(processedData.trxn_type_prod_map)
+					);
+					sessionStorage.setItem(
+						SESSION_STORAGE_KEYS.CACHE_ACCESS_TOKEN,
+						accessToken
+					);
+
+					console.log(
+						"[MenuContext]: Data loaded from API",
+						processedData
+					);
 				}
-			});
-
-			let manageMyAccount = {
-				id: TransactionIds.MANAGE_MY_ACCOUNT,
-				...role_tx_list[TransactionIds.MANAGE_MY_ACCOUNT],
-			};
-
-			if (manageMyAccount?.is_visible === 1) {
-				manageMyAccount = null;
+			} catch (error) {
+				console.error("MenuProvider error:", error);
+			} finally {
+				setLoading(false);
 			}
+		};
 
-			setAppLists({
-				menuList: _filteredMenuList,
-				trxnList: _trxnList,
-				otherList: [
-					...[
-						{
-							icon: "transaction-history",
-							label: "Transaction History",
-							description:
-								"Statement of your previous transactions",
-							link: (isAdmin ? "/admin" : "") + Endpoints.HISTORY,
-						},
-					],
-					..._otherList,
-					...(manageMyAccount ? [manageMyAccount] : []),
-				],
-			});
-
-			if (ready) {
-				let _otherActions = generateTransactionActions(
-					[..._otherList, manageMyAccount],
-					role_tx_list,
-					router,
-					true // is-other-list
-				);
-
-				let _menuLinkActions = generateMenuLinkActions(
-					_menuList,
-					router
-				);
-
-				setOtherActions([..._otherActions, ..._menuLinkActions]);
-
-				setTrxnActions(
-					isAdmin && isAdminAgentMode !== true
-						? []
-						: generateTransactionActions(
-								_trxnList,
-								role_tx_list,
-								router
-						  )
-				);
-			}
-		}
-	};
-
-	useEffect(() => {
 		if (isLoggedIn) {
-			onUserLogin();
+			loadMenuData();
 		}
-	}, [isLoggedIn, accessToken]);
-
-	useEffect(() => {
-		processInteractions();
-	}, [
-		interactions?.interaction_list,
-		interactions?.role_tx_list,
-		isAdmin,
-		isAdminAgentMode,
-		ready,
-	]);
-
-	useRegisterActions(
-		[...trxnActions, ...otherActions],
-		[[...trxnActions, ...otherActions]]
-	);
+	}, [isLoggedIn, isAdmin, accessToken]);
 
 	const value = useMemo(() => {
-		const { menuList, trxnList, otherList } = appLists;
 		return {
 			interactions,
-			menuList,
-			trxnList,
-			otherList,
 			loading,
 		};
-	}, [
-		interactions,
-		appLists?.menuList,
-		appLists?.trxnList,
-		appLists?.otherList,
-		loading,
-	]);
+	}, [interactions, loading]);
 
 	return (
 		<MenuContext.Provider value={value}>{children}</MenuContext.Provider>
 	);
 };
 
+/**
+ * Custom hook to use the MenuContext.
+ * @returns {object} The context value.
+ */
 const useMenuContext = () => {
 	const context = useContext(MenuContext);
+	if (!context) {
+		throw new Error("useMenuContext must be used within a MenuProvider");
+	}
 	return context;
 };
 
