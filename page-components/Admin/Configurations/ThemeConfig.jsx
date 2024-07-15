46e8b7956294322251689636c958424d4f04222c
shubhmjain30 <shubhmjain30@gmail.com>
2024-07-16 00:37:23 +0530

    feat: api call to save theme metadata added

--- page-components/Admin/Configurations/ThemeConfig.jsx
+++ page-components/Admin/Configurations/ThemeConfig.jsx
@@ -1,10 +1,11 @@
-import { Box, Flex, Text, useToken } from "@chakra-ui/react";
+import { Box, Flex, Text, useDisclosure, useToken } from "@chakra-ui/react";
 import {
 	Button,
 	ColorPair,
 	ColorPickerWidget,
 	Icon,
 	InputLabel as Label,
+	Modal,
 	Radio,
 } from "components";
 import { colorThemes, Endpoints, TransactionIds } from "constants";
@@ -13,8 +14,7 @@ import { fetcher } from "helpers";
 import { useFeatureFlag } from "hooks";
 import { useEffect, useState } from "react";
 import { generateShades } from "utils";
-
-const bgTransition = "background 0.5s ease-in";
+import { AppPreview } from ".";
 
 /**
  * Component to configure the theme colors of the app.
@@ -27,6 +27,7 @@ const ThemeConfig = () => {
 	const [landingPageStyle, setLandingPageStyle] = useState("");
 	const [isConfigEnabled] = useFeatureFlag("CUSTOM_THEME");
 	const { accessToken } = useSession();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	// Get current theme color values
 	const [
@@ -161,6 +162,7 @@ const ThemeConfig = () => {
 			};
 
 	const handleSubmit = () => {
+		console.log("SUBMIT");
 		fetcher(
 			process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION_JSON,
 			{
@@ -181,6 +183,7 @@ const ThemeConfig = () => {
 		)
 			.then((res) => {
 				console.log("res", res);
+				onClose();
 			})
 			.catch((err) => {
 				console.error("err", err);
@@ -188,7 +191,11 @@ const ThemeConfig = () => {
 	};
 
 	return (
-		<Flex direction="column" gap={{ base: 4, md: 8 }}>
+		<Flex
+			direction="column"
+			gap={{ base: 4, md: 8 }}
+			px={{ base: 4, md: 0 }}
+		>
 			<Section title="Colors">
 				<AppPreview
 					primary={selectedTheme?.primary || primary}
@@ -197,7 +204,7 @@ const ThemeConfig = () => {
 					navStyle={navStyle}
 				/>
 
-				<Flex direction="column" gap={6}>
+				<Flex direction="column" gap="6">
 					<Box>
 						<Radio
 							label="Select Menu Bar Style"
@@ -319,10 +326,36 @@ const ThemeConfig = () => {
 				/>
 			</Section>
 
-			<Button onClick={() => handleSubmit()}>HIT ME</Button>
+			<Button onClick={() => onOpen()}>Save</Button>
+
+			<Modal
+				{...{
+					id: "theme-consent",
+					size: "sm",
+					title: "Attention",
+					isOpen,
+					onClose,
+				}}
+			>
+				<Flex direction="column" gap="4">
+					<Flex direction="column" gap="2">
+						<Text>
+							You are about to change theme for all your network.
+							All your users will see the new colors after next
+							login.
+						</Text>
+						<Text fontWeight="semibold">
+							Are you sure you want to continue?
+						</Text>
+					</Flex>
+					<Button onClick={() => handleSubmit()}>Apply</Button>
+				</Flex>
+			</Modal>
 		</Flex>
 	);
 };
+
+export default ThemeConfig;
 
 /**
  * Section card component
@@ -350,154 +383,6 @@ const Section = ({ title, children }) => {
 			>
 				{children}
 			</Flex>
-		</Flex>
-	);
-};
-
-/**
- * Component to show a 270px x 180px preview of the website using the theme colors. Show the primary and accent colors. Show the white top-bar, the primary left menu with a few dummy menu items
- * @param {object} props
- * @param {string} props.primary - Primary color
- * @param {string} props.primaryDark - Primary Dark color
- * @param {string} props.accent - Accent color
- * @param {string} props.navStyle - Navigation style (light/dark)
- */
-const AppPreview = ({ primary, primaryDark, accent, navStyle }) => {
-	const w = "300px",
-		h = "200px";
-
-	// if (!primary || !accent) return <Box w={w} h={h} />;
-
-	return (
-		<Flex
-			direction="column"
-			w={w}
-			h={h}
-			border="1px solid #999"
-			borderRadius={6}
-			overflow="hidden"
-			fontSize="5px"
-			shadow="base"
-			pointerEvents="none"
-			userSelect="none"
-		>
-			{primary && accent ? (
-				<>
-					<Flex
-						bg={navStyle === "light" ? primary : "white"}
-						h="8%"
-						minH="8%"
-						w="100%"
-						align="center"
-						px="1em"
-						fontSize="6px"
-						fontWeight="700"
-						color={navStyle === "light" ? "#FFFFFF90" : "#666"}
-						transition={bgTransition}
-					>
-						Logo
-					</Flex>
-
-					<Flex direction="row" w="100%" h="100%" flex="1">
-						{/* Left Menu */}
-						<Flex
-							bg={navStyle === "light" ? "white" : primary}
-							w="25%"
-							h="100%"
-							direction="column"
-							color={navStyle === "light" ? "#222" : "white"}
-							transition={bgTransition}
-						>
-							{/* Left Menu Items */}
-							<MenuItem
-								item="₹10,000"
-								primaryDark={primaryDark}
-							/>
-							{["Home", "Transaction", "Others"].map(
-								(item, i) => (
-									<MenuItem
-										key={i}
-										item={item}
-										primaryDark={primaryDark}
-										accent={accent}
-										selected={i === 1}
-									/>
-								)
-							)}
-						</Flex>
-
-						{/* Right Pane */}
-						<Flex
-							direction="column"
-							flex="1"
-							align="center"
-							bg="bg"
-							w="30px"
-							h="100%"
-							p="3%"
-						>
-							{/* Show a white transaction card with a button (rounded box) at the bottom in accent color */}
-							<Flex
-								bg="white"
-								w="100%"
-								h="80%"
-								direction="column"
-								borderRadius={3}
-								p="3%"
-								shadow="base"
-							>
-								<Text size="1.2em" fontWeight="500">
-									Transaction
-								</Text>
-								<Box flex="1"></Box>
-								{/* Button */}
-								<Flex
-									bg={accent}
-									w="20%"
-									h="10%"
-									align="center"
-									justify="center"
-									borderRadius={2}
-									px="4px"
-									color="white"
-									fontSize="0.9em"
-									transition={bgTransition}
-								>
-									Proceed
-								</Flex>
-							</Flex>
-						</Flex>
-					</Flex>
-				</>
-			) : (
-				<Box w={w} h={h} />
-			)}
-		</Flex>
-	);
-};
-
-/**
- * Menu Item component for preview
- * @param root0
- * @param root0.item
- * @param root0.primaryDark
- * @param root0.accent
- * @param root0.selected
- */
-const MenuItem = ({ item, primaryDark, accent, selected = false }) => {
-	return (
-		<Flex
-			direction="row"
-			bg={selected ? primaryDark : ""}
-			color={selected ? "white" : ""}
-			borderBottom="1px solid #999"
-			h="15px"
-			w="100%"
-			align="center"
-			transition={bgTransition}
-		>
-			<Box h="100%" w="3px" mr="3px" bg={selected ? accent : ""}></Box>
-			{item}
 		</Flex>
 	);
 };
@@ -567,5 +452,3 @@ const ColorSelector = ({ theme, i, isSelected, onSelect, ...rest }) => {
 		</Flex>
 	);
 };
-
-export default ThemeConfig;
