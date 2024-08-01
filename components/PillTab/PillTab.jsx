ec2697ca64c9c122239572e2a8c9410c55763fb2
shubhmjain30 <shubhmjain30@gmail.com>
2024-08-01 22:29:24 +0530

    refactor: default value for visible

--- components/PillTab/PillTab.jsx
+++ components/PillTab/PillTab.jsx
@@ -37,7 +37,7 @@ const PillTab = ({ list, currTab, onClick, ...rest }) => {
 			justify={{ base: "space-between", md: "flex-start" }}
 			{...rest}
 		>
-			{tabList?.map(({ label, visible }, index) => {
+			{tabList?.map(({ label, visible = true }, index) => {
 				if (!visible) return;
 				const isActive = index === currTab;
 				return (
