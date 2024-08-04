193d8d979823ae70b36ea44d8059a07ad3126095
shubhmjain30 <shubhmjain30@gmail.com>
2024-08-04 14:44:05 +0530

    refactor: showing others as grid interaction

--- components/BottomAppBar/More.tsx
+++ components/BottomAppBar/More.tsx
@@ -1,4 +1,7 @@
 import { Box, Flex, Text, useDisclosure } from "@chakra-ui/react";
+import { Endpoints } from "constants/EndPoints";
+import { InteractionBehavior } from "constants/trxnFramework";
+import { useUser } from "contexts/UserContext";
 import { useNavigationLists } from "hooks";
 import { useRef } from "react";
 import { BottomAppBarDrawerList } from ".";
@@ -7,14 +10,47 @@ import { Drawer, Icon, StatusCard } from "..";
 // Ignore both home & dashboard in more option as it is already visible
 const IGNORE_LIST = [1, 8];
 
+// Generates a grid interaction object by consolidating IDs from a list of interactions.
+const generateGridInteraction = (list) => {
+	const gridInteractions = {
+		behavior: InteractionBehavior.GRID,
+		group_interaction_ids: "",
+		label: "Others",
+		icon: "others",
+	};
+
+	list.forEach((listElement) => {
+		if (listElement?.id) {
+			if (gridInteractions.group_interaction_ids) {
+				gridInteractions.group_interaction_ids += ",";
+			}
+			gridInteractions.group_interaction_ids += listElement.id;
+		}
+	});
+
+	return { gridInteractions };
+};
+
 /**
  * The More component renders a button that opens a drawer containing additional menu items.
  */
 const More = () => {
 	const btnRef = useRef<HTMLButtonElement>(null);
+	const { isAdmin } = useUser();
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const { menuList, otherList } = useNavigationLists(IGNORE_LIST);
-	const list = [...menuList, ...otherList];
+	const { gridInteractions: other } = generateGridInteraction(otherList);
+	const list = [
+		...menuList,
+		// Adding Transaction History, as it will not be found in role_tx_list when handling group_interaction_ids, due to which Transaction History appears as a separate item outside of the "Others" option.
+		{
+			icon: "transaction-history",
+			label: "Transaction History",
+			description: "Statement of your previous transactions",
+			link: `${isAdmin ? "/admin" : ""}${Endpoints.HISTORY}`,
+		},
+		other,
+	];
 
 	return (
 		<>
