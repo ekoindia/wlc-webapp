82f39290cc225b2eeafed8ddf6e320132c3b4440
shubhmjain30 <shubhmjain30@gmail.com>
2024-08-11 13:20:58 +0530

    build: file type changed to ts to avoid type export build error

--- components/BottomAppBar/index.ts
+++ components/BottomAppBar/index.ts
@@ -2,7 +2,7 @@ export { default as BottomAppBar } from "./BottomAppBar";
 export { default as More } from "./More";
 export { default as Transactions } from "./Transactions";
 export {
-	BottomAppBarItem,
 	useAccordionMenuConverter,
 	useBottomAppBarItems,
+	type BottomAppBarItem,
 } from "./useBottomAppBar";
