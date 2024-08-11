1e033d4f48d22ca2c78e2361a42be3256e39dd18
shubhmjain30 <shubhmjain30@gmail.com>
2024-08-11 12:44:40 +0530

    chore: removed AccordionMenu, BottomAppBarDrawerList & BottomAppBarUtils from BottomAppBar

--- components/BottomAppBar/index.js
+++ components/BottomAppBar/index.js
@@ -1,6 +1,7 @@
-// export { default as AccordionMenu } from "./AccordionMenu";
 export { default as BottomAppBar } from "./BottomAppBar";
-export { default as BottomAppBarDrawerList } from "./BottomAppBarDrawerList";
 export { default as More } from "./More";
 export { default as Transactions } from "./Transactions";
-export { useBottomBarItems } from "./useBottomBarItems";
+export {
+	convertToAccordionMenuList,
+	useBottomBarItems,
+} from "./useBottomBarItems";
