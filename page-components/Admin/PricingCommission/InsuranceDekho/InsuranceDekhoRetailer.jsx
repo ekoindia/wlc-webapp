368f529479fbee8abfe65852cdbdcfc2662f70ba
shubhmjain30 <shubhmjain30@gmail.com>
2024-10-16 15:38:45 +0530

    feat: insurance dekho commission configuration page

---
+++ page-components/Admin/PricingCommission/InsuranceDekho/InsuranceDekhoRetailer.jsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const InsuranceDekhoRetailer = () => {
+	return (
+		<Flex direction="column" gap="8">
+			<Text>Commission should be set to 62.5%.</Text>
+		</Flex>
+	);
+};
+
+export default InsuranceDekhoRetailer;
