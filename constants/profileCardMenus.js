0b23913c46d9bfd3f77c8d7bb553fd6136f8d354
Kumar Abhishek <manustays@users.noreply.github.com>
2024-11-07 14:00:18 +0530

    feat: hide profile menu items based on role

--- constants/profileCardMenus.js
+++ constants/profileCardMenus.js
@@ -1,5 +1,9 @@
 import { TransactionIds } from "./EpsTransactions";
 
+/**
+ * Configure the profile menu items for the admin.
+ * It will be shown in the top-right profile dropdown menu (before the "Logout" menu-item).
+ */
 export const adminProfileMenu = [
 	// {
 	// 	title: "Business Contact",
@@ -19,9 +23,14 @@ export const adminProfileMenu = [
 	// },
 ];
 
+/**
+ * Configure the profile menu items for the user (retailer, distributor, etc.)
+ * It will be shown in the top-right profile dropdown menu (before the "Logout" menu-item).
+ */
 export const profileMenu = [
 	{
 		title: "Settings",
 		link: "/transaction/" + TransactionIds.MANAGE_MY_ACCOUNT,
+		trxn_id: TransactionIds.MANAGE_MY_ACCOUNT, // Used to check for allowed role
 	},
 ];
