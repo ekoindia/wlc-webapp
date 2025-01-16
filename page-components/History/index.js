356027596f7fa9634fca4bdffeb223478c0735b9
shubhmjain30 <shubhmjain30@gmail.com>
2025-01-17 01:53:53 +0530

    refactor: remove unused History components and related files

--- page-components/History/index.js
+++
@@ -1,4 +0,0 @@
-export { default as History } from "./History";
-export { HistoryPagination } from "./HistoryPagination";
-export { HistoryTable } from "./HistoryTable";
-export { default as HistoryToolbar } from "./HistoryToolbar";
