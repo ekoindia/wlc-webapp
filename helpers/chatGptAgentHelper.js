ba2c012b5588b25c39fc82c1d06b4a8a829ac3bc
Kumar Abhishek <manustays@users.noreply.github.com>
2025-02-22 00:19:43 +0530

    feat: add ChatGpt Agent URL for all Eloka admins

--- helpers/chatGptAgentHelper.js
+++ helpers/chatGptAgentHelper.js
@@ -1,5 +1,5 @@
 /**
- * Get the chat GPT agent URL.
+ * Returns the URL of the ChatGPT Custom Agent for the given organization and user type.
  * @param {object} props
  * @param {number} props.orgId - The organization ID
  * @param {boolean} props.isAdmin - If the user is an admin
@@ -7,12 +7,11 @@
 export const getChatGptAgentUrl = ({ orgId, isAdmin }) => {
 	let gptAgentUrl = null;
 	if (orgId === 287) {
-		// 287 = Eko Kiosk
+		// 287 = Eko Kiosk (For all agents)
 		gptAgentUrl = "g-6790d44389448191805b94259a16b196-eko-sbi-kiosk-helper";
 	} else if (isAdmin) {
-		// Other Admins...
-		gptAgentUrl = null;
-		// gptAgentUrl = "g-6790d44389448191805b94259a16b196-eko-sbi-kiosk-helper";
+		// Other Eloka Admins...
+		gptAgentUrl = "g-67b81fe9146c8191bd036d3ca423b2d4-eko-eloka-helper";
 	}
 	return gptAgentUrl ? `https://chatgpt.com/g/${gptAgentUrl}/` : null;
 };
