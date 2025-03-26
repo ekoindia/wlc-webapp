84a8dbf3971437befbd56b485ff6a558780f400d
Kumar Abhishek <manustays@users.noreply.github.com>
2025-03-26 13:35:05 +0530

    feat: add ToggleColumns component for managing column visibility in Transaction History table

--- page-components/History/index.js
+++ page-components/History/index.js
@@ -2,3 +2,4 @@ export { default as History } from "./History";
 export { HistoryPagination } from "./HistoryPagination";
 export { HistoryTable } from "./HistoryTable";
 export { default as HistoryToolbar } from "./HistoryToolbar";
+export { default as ToggleColumns } from "./ToggleColumns";
