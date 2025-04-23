947a5fc04c24e049606acc8adf5c21d0b5a650fe
Kumar Abhishek <manustays@users.noreply.github.com>
2025-04-23 13:15:57 +0530

    feat: enhance ResponseSection to include optional index display

--- page-components/products/common/ResponseSection.jsx
+++ page-components/products/common/ResponseSection.jsx
@@ -1,23 +1,46 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 /**
  * A reusable section container for API response cards
  * @param {object} props
  * @param {string} props.heading - The heading of the section
+ * @param {number} [props.index] - If provided, shows the index of the section. Used when the section represents an item of an array.
  * @param {React.ReactNode} props.children - The content of the section
  * @returns {JSX.Element} The rendered section
  */
-const ResponseSection = ({ heading, children }) => {
+const ResponseSection = ({ heading, index, children }) => {
 	return (
 		<Box
-			mb={4}
+			// mb={4}
+			// mt={4}
 			borderLeft="4px"
 			borderLeftColor="primary.DEFAULT"
-			pl={3}
-			ml={3}
+			p="0.5em 0.5em 0.5em 1em"
+			m="1em 0 0.5em 0.5em"
+			// ml={3}
 		>
-			<Text fontWeight="bold" fontSize="md" mb={2} color="primary.dark">
-				{heading}
-			</Text>
+			<Flex direction="row" align="center" mb={2}>
+				{/* Show an index, if provided, as a number within a colored circle before the section heading */}
+				{index !== undefined && (
+					<Flex
+						as="span"
+						bg="primary.DEFAULT"
+						color="white"
+						borderRadius="full"
+						width="20px"
+						height="20px"
+						justify="center"
+						align="center"
+						fontSize="sm"
+						mr={2}
+					>
+						{index}
+					</Flex>
+				)}
+				{/* Section heading */}
+				<Text fontWeight="bold" fontSize="md" color="primary.dark">
+					{heading}
+				</Text>
+			</Flex>
 			{children}
 		</Box>
 	);
