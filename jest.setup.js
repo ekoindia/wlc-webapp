880966173fb636a9ce8a5e48bdf90d057b77e0a4
Kumar Abhishek <manustays@users.noreply.github.com>
2025-04-24 13:50:14 +0530

    feat: add mock for Chakra UI's useBreakpointValue and useMediaQuery in Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,25 @@ import mockRouter from "next-router-mock";
 jest.mock("next/router", () => mockRouter);
 // This is needed for mocking 'next/link':
 jest.mock("next/dist/client/router", () => mockRouter);
+
+// Rule: Add mock for Chakra UI's useBreakpointValue hook
+jest.mock("@chakra-ui/media-query", () => ({
+	useBreakpointValue: jest.fn((values, options) => {
+		// Return the base value or fallback value
+		if (values && typeof values === "object" && "base" in values) {
+			return values.base;
+		} else if (options?.fallback) {
+			return options.fallback;
+		}
+		return (
+			values?.md ||
+			values?.lg ||
+			values?.sm ||
+			values?.xs ||
+			values?.xl ||
+			values?.xxl ||
+			""
+		); // Default fallback
+	}),
+	useMediaQuery: jest.fn(() => [true]),
+}));
