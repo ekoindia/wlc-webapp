08e70d4f6939890ce5e63333af2fc473075ea90f
nitika <nitika.rana@eko.co.in>
2025-05-02 19:21:37 +0530

    feat: update kyc verification APIs

--- page-components/products/kyc/pan/PanVerification.tsx
+++ page-components/products/kyc/pan/PanVerification.tsx
@@ -21,12 +21,12 @@ const PanSubProducts: {
 		icon: "verified-user",
 		url: "pan/advanced",
 	},
-	{
-		label: "Bulk PAN Verification",
-		desc: "Verify multiple PAN numbers in a batch",
-		icon: "toc",
-		url: "pan/bulk",
-	},
+	// {
+	// 	label: "Bulk PAN Verification",
+	// 	desc: "Verify multiple PAN numbers in a batch",
+	// 	icon: "toc",
+	// 	url: "pan/bulk",
+	// },
 ];
 
 export const PanVerification = (): JSX.Element => {
