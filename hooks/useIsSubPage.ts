db7e5f09cfc81c6c9dad11f45c500762d73ae74f
shubhmjain30 <shubhmjain30@gmail.com>
2025-05-09 11:54:04 +0530

    feat: add useIsSubPage hook and corresponding tests

---
+++ hooks/useIsSubPage.ts
@@ -0,0 +1,27 @@
+import { useUser } from "contexts";
+import { useRouter } from "next/router";
+
+/**
+ * Determines if the current page is a subpage based on the URL structure.
+ *
+ * A subpage is identified if the pathname contains more than one segment
+ * (e.g., `/parent/child` or `/dashboard/settings`).
+ * @returns {boolean} - `true` if the current page is a subpage, otherwise `false`.
+ */
+const useIsSubPage = (): boolean => {
+	const { pathname } = useRouter();
+	const { isAdmin } = useUser();
+
+	// Remove leading and trailing slashes, then split by "/"
+	const pathArray = pathname.replace(/^\/|\/$/g, "").split("/");
+
+	// If the user is an admin, treat any path with more than two segment as a subpage
+	if (isAdmin) {
+		return pathArray.length > 2;
+	}
+
+	// For non-admin users, determine subpage based on the path structure
+	return pathArray.length > 1;
+};
+
+export default useIsSubPage;
