c814b5c12816cb60d671722a77a7aeef97618cb4
shubhmjain30 <shubhmjain30@gmail.com>
2025-05-21 17:21:40 +0530

    feat: implement throttling for download button to prevent excessive API calls

--- page-components/Admin/PricingCommission/DownloadPricing.jsx
+++ page-components/Admin/PricingCommission/DownloadPricing.jsx
@@ -1,42 +1,52 @@
 import { Button } from "components";
-import { useSession } from "contexts";
 import { Endpoints, TransactionTypes } from "constants";
+import { useSession } from "contexts";
 import { fetcher } from "helpers";
-import { saveDataToFile } from "utils";
+import { useCallback } from "react";
+import { saveDataToFile, throttle } from "utils";
 
 /**
  * A button component that downloads the current pricing data.
  */
 export const DownloadPricing = () => {
 	const { accessToken } = useSession();
-	const handleClick = () => {
-		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
-			headers: {
-				"tf-is-file-download": "1",
-			},
-			body: {
-				interaction_type_id: TransactionTypes.DOWNLOAD_EXISTING_PRICING,
-			},
-			token: accessToken,
-		})
-			.then((data) => {
-				const _blob = data?.file?.blob;
-				const _filename = data?.file?.name || "file";
-				const _type = data?.file["content-type"];
-				const _b64 = true;
-				saveDataToFile(_blob, _filename, _type, _b64);
-			})
-			.catch((err) => {
-				console.error("Error: ", err);
-			});
-	};
+
+	const throttledDownload = useCallback(
+		throttle(() => {
+			fetcher(
+				process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION,
+				{
+					headers: {
+						"tf-is-file-download": "1",
+					},
+					body: {
+						interaction_type_id:
+							TransactionTypes.DOWNLOAD_EXISTING_PRICING,
+					},
+					token: accessToken,
+				}
+			)
+				.then((data) => {
+					const _blob = data?.file?.blob;
+					const _filename = data?.file?.name || "file";
+					const _type = data?.file["content-type"];
+					const _b64 = true;
+					saveDataToFile(_blob, _filename, _type, _b64);
+				})
+				.catch((err) => {
+					console.error("Error: ", err);
+				});
+		}, 5000),
+		[]
+	);
+
 	return (
 		<Button
 			size={{ base: "sm", md: "md" }}
 			icon="file-download"
 			iconStyle={{ size: { base: "xs", md: "sm" } }}
 			iconSpacing="2"
-			onClick={handleClick}
+			onClick={throttledDownload}
 		>
 			Existing Pricing
 		</Button>
