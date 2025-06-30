5f2402b3ddac471b32e23936199c3da7efc569aa
shubhmjain30 <shubhmjain30@gmail.com>
2025-06-30 15:17:19 +0530

    ui: enhance Bbps component layout with PageTitle and Flex
    - Added PageTitle for better context and improved layout using Flex
      to organize the InfoTileGrid. This enhances the user interface
      and provides a clearer structure for the Bharat Bill Payment System
      page.

--- page-components/products/bbps/Bbps.tsx
+++ page-components/products/bbps/Bbps.tsx
@@ -1,6 +1,14 @@
-import { InfoTileGrid } from "components";
+import { Flex } from "@chakra-ui/react";
+import { InfoTileGrid, PageTitle } from "components";
 import { BbpsProducts } from "./BbpsProducts";
 
 export const Bbps = (): JSX.Element => {
-	return <InfoTileGrid list={BbpsProducts} />;
+	return (
+		<>
+			<PageTitle title="Bharat Bill Payment System" />
+			<Flex direction="column" gap={4} mx={{ base: "4", md: "0" }}>
+				<InfoTileGrid list={BbpsProducts} />
+			</Flex>
+		</>
+	);
 };
