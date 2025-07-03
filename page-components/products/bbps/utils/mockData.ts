abe66fcd8e70afe4f4a92ab6079fcb2205610180
shubhmjain30 <shubhmjain30@gmail.com>
2025-07-03 10:42:30 +0530

    feat: add mockBillPaymentResponse for payment processing
    - Introduce a new mock response for bill payment to simulate payment
      processing scenarios.
    - Update existing mock data to reflect accurate due dates for bills,
      enhancing the realism of the mock API responses.

--- page-components/products/bbps/utils/mockData.ts
+++ page-components/products/bbps/utils/mockData.ts
@@ -1,4 +1,8 @@
-import { BillFetchResponse, PaymentStatusData } from "../context/types";
+import {
+	BillFetchResponse,
+	BillPaymentResponse,
+	PaymentStatusData,
+} from "../context/types";
 
 /**
  * Mock response for BBPS bill fetch API
@@ -25,7 +29,7 @@ export const mockBillFetchResponse: BillFetchResponse = {
 				billDate: "01-08-2023",
 				amount_multiple: "100.00",
 				billNumber: "HGE1VT54UCC6DB27VW6JB0",
-				billDueDate: "30-12-2099",
+				billDueDate: "11-08-2023",
 				filler1: "Sam",
 				filler2: null,
 				minBillPayAmount: "50.00",
@@ -38,7 +42,7 @@ export const mockBillFetchResponse: BillFetchResponse = {
 				billDate: "01-08-2023",
 				amount_multiple: "500.00",
 				billNumber: "HGE1VT54UCC6DB27VW6JB1",
-				billDueDate: "30-12-2099",
+				billDueDate: "12-08-2023",
 				filler1: "Sam",
 				filler2: null,
 				minBillPayAmount: "500.00",
@@ -51,7 +55,7 @@ export const mockBillFetchResponse: BillFetchResponse = {
 				billDate: "01-08-2023",
 				amount_multiple: "100.00",
 				billNumber: "HGE1VT54UCC6DB27VW6JB2",
-				billDueDate: "30-12-2099",
+				billDueDate: "13-08-2023",
 				filler1: "Sam",
 				filler2: null,
 				minBillPayAmount: "500.00",
@@ -61,10 +65,10 @@ export const mockBillFetchResponse: BillFetchResponse = {
 				maxBillPayAmount: "5000.00",
 				authenticator4: null,
 				bharatBillReferenceNumber: "HGE1VT54UCC6DB27VW6JB3",
-				billDate: "01-08-2023",
+				billDate: "10-08-2023",
 				amount_multiple: "100.00",
 				billNumber: "HGE1VT54UCC6DB27VW6JB3",
-				billDueDate: "30-12-2099",
+				billDueDate: "11-08-2023",
 				filler1: "Sam",
 				filler2: null,
 				minBillPayAmount: "500.00",
@@ -79,6 +83,187 @@ export const mockBillFetchResponse: BillFetchResponse = {
 	response_type_id: 1052,
 	message: "Due Bill Amount For utility",
 	status: 0,
+};
+
+export const mockBillPaymentResponse: BillPaymentResponse = {
+	sourceid: "EKO01",
+	customerid: "EKO9466009091",
+	paymentid: "HGAFP016920000334231",
+	objectid: "payment",
+	currency: "356",
+	billerid: "LOAN00000NATOI",
+	validationid: "HGE1VT5K29G5EOUB6KJ6",
+	source_ref_no: "HG1751397691264",
+	payment_amount: "8000.00",
+	biller_name: "Loan API 1 1",
+	biller_category: "Loan",
+	authenticators: [
+		{
+			parameter_name: "Loan Account Number",
+			value: "LOAN041101",
+		},
+	],
+	payment_type: "instapay",
+	txn_date_time: "02-07-2025 00:50:50",
+	biller_status: "PENDING",
+	payment_account: {
+		objectid: "paymentaccount",
+		payment_method: "Cash",
+		remarks: "Agent Cash mode",
+	},
+	billlist: [
+		{
+			objectid: "bill",
+			billid: "HGE1VT5K29G5EOUB6KJ6B0",
+			billerid: "LOAN00000NATOI",
+			sourceid: "EKO01",
+			billstatus: "PENDING",
+			authenticators: [
+				{
+					parameter_name: "Loan Account Number",
+					value: "LOAN041101",
+				},
+			],
+			billnumber: "LOAN001",
+			billperiod: "August",
+			net_billamount: "2000.00",
+			description: "ABC 123 @#$",
+			additional_details: [
+				{
+					seq: "1",
+					label: "FetchAdditionalInfo",
+					value: "Fetch Additional Info",
+				},
+			],
+			validationid: "HGE1VT5K29G5EOUB6KJ6",
+			customer_name: "Sam",
+			max_pay_amount: "2500.00",
+			min_pay_amount: "50.00",
+			amount_multiple: "100.00",
+			billamount: "2000.00",
+			billdate: "01-08-2023",
+			billduedate: "30-12-2099",
+		},
+		{
+			objectid: "bill",
+			billid: "HGE1VT5K29G5EOUB6KJ6B1",
+			billerid: "LOAN00000NATOI",
+			sourceid: "EKO01",
+			billstatus: "PENDING",
+			authenticators: [
+				{
+					parameter_name: "Loan Account Number",
+					value: "LOAN041101",
+				},
+			],
+			billnumber: "LOAN001",
+			billperiod: "August",
+			net_billamount: "2000.00",
+			description: "Advance Payment",
+			additional_details: [
+				{
+					seq: "1",
+					label: "FetchAdditionalInfo",
+					value: "Fetch Additional Info",
+				},
+			],
+			validationid: "HGE1VT5K29G5EOUB6KJ6",
+			customer_name: "Sam",
+			max_pay_amount: "10000.00",
+			min_pay_amount: "500.00",
+			amount_multiple: "500.00",
+			billamount: "2000.00",
+			billdate: "01-08-2023",
+			billduedate: "30-12-2099",
+		},
+		{
+			objectid: "bill",
+			billid: "HGE1VT5K29G5EOUB6KJ6B2",
+			billerid: "LOAN00000NATOI",
+			sourceid: "EKO01",
+			billstatus: "PENDING",
+			authenticators: [
+				{
+					parameter_name: "Loan Account Number",
+					value: "LOAN041101",
+				},
+			],
+			billnumber: "LOAN001",
+			billperiod: "August",
+			net_billamount: "2000.00",
+			description: "Part Payment",
+			additional_details: [
+				{
+					seq: "1",
+					label: "FetchAdditionalInfo",
+					value: "Fetch Additional Info",
+				},
+			],
+			validationid: "HGE1VT5K29G5EOUB6KJ6",
+			customer_name: "Sam",
+			max_pay_amount: "3000.00",
+			min_pay_amount: "500.00",
+			amount_multiple: "100.00",
+			billamount: "2000.00",
+			billdate: "01-08-2023",
+			billduedate: "30-12-2099",
+		},
+		{
+			objectid: "bill",
+			billid: "HGE1VT5K29G5EOUB6KJ6B3",
+			billerid: "LOAN00000NATOI",
+			sourceid: "EKO01",
+			billstatus: "PENDING",
+			authenticators: [
+				{
+					parameter_name: "Loan Account Number",
+					value: "LOAN041101",
+				},
+			],
+			billnumber: "LOAN001",
+			billperiod: "August",
+			net_billamount: "2000.00",
+			description: "Foreclosure",
+			additional_details: [
+				{
+					seq: "1",
+					label: "FetchAdditionalInfo",
+					value: "Fetch Additional Info",
+				},
+			],
+			validationid: "HGE1VT5K29G5EOUB6KJ6",
+			customer_name: "Sam",
+			max_pay_amount: "5000.00",
+			min_pay_amount: "500.00",
+			amount_multiple: "100.00",
+			billamount: "2000.00",
+			billdate: "01-08-2023",
+			billduedate: "30-12-2099",
+		},
+	],
+	payment_status: "PAID",
+	debit_amount: "8000.00",
+	cou_conv_fee: "0.00",
+	bou_conv_fee: "0.00",
+	bbps_ref_no: "BD015183BAFAAAAAB04R",
+	payment_amount_breakup: [
+		{
+			billid: "HGE1VT5K29G5EOUB6KJ6B0",
+			bill_payment_amount: "2000.00",
+		},
+		{
+			billid: "HGE1VT5K29G5EOUB6KJ6B1",
+			bill_payment_amount: "2000.00",
+		},
+		{
+			billid: "HGE1VT5K29G5EOUB6KJ6B2",
+			bill_payment_amount: "2000.00",
+		},
+		{
+			billid: "HGE1VT5K29G5EOUB6KJ6B3",
+			bill_payment_amount: "2000.00",
+		},
+	],
 };
 
 /**
