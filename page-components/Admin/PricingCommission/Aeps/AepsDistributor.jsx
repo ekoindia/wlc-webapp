9eb2de213ae3c9ca7ef2c0324cdf37aa8a4d5073
shubhmjain30 <shubhmjain30@gmail.com>
2025-08-25 12:30:28 +0530

    refactor: Comment out unused imports and code

--- page-components/Admin/PricingCommission/Aeps/AepsDistributor.jsx
+++ page-components/Admin/PricingCommission/Aeps/AepsDistributor.jsx
@@ -1,273 +1,273 @@
-import { Flex, useToast } from "@chakra-ui/react";
-import { ActionButtonGroup, Icon } from "components";
-import {
-	Endpoints,
-	ParamType,
-	productPricingCommissionValidationConfig,
-	products,
-	TransactionTypes,
-} from "constants";
-import { useSession } from "contexts/";
-import { fetcher } from "helpers";
-import { useRefreshToken } from "hooks";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useForm, useWatch } from "react-hook-form";
-import { Form } from "tf-components/Form";
+// import { Flex, useToast } from "@chakra-ui/react";
+// import { ActionButtonGroup, Icon } from "components";
+// import {
+// 	Endpoints,
+// 	ParamType,
+// 	productPricingCommissionValidationConfig,
+// 	products,
+// 	TransactionTypes,
+// } from "constants";
+// import { useSession } from "contexts/";
+// import { fetcher } from "helpers";
+// import { useRefreshToken } from "hooks";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import { useForm, useWatch } from "react-hook-form";
+// import { Form } from "tf-components/Form";
 
-const PRICING_TYPE = {
-	PERCENT: "1",
-	FIXED: "0",
-};
+// const PRICING_TYPE = {
+// 	PERCENT: "1",
+// 	FIXED: "0",
+// };
 
-const getStatus = (status) => {
-	switch (status) {
-		case 0:
-			return "success";
-		default:
-			return "error";
-	}
-};
+// const getStatus = (status) => {
+// 	switch (status) {
+// 		case 0:
+// 			return "success";
+// 		default:
+// 			return "error";
+// 	}
+// };
 
-const pricing_type_list = [
-	{ value: PRICING_TYPE.PERCENT, label: "Percentage (%)" },
-	{ value: PRICING_TYPE.FIXED, label: "Fixed (₹)" },
-];
+// const pricing_type_list = [
+// 	{ value: PRICING_TYPE.PERCENT, label: "Percentage (%)" },
+// 	{ value: PRICING_TYPE.FIXED, label: "Fixed (₹)" },
+// ];
 
-const _multiselectRenderer = {
-	value: "user_code",
-	label: "name",
-};
+// const _multiselectRenderer = {
+// 	value: "user_code",
+// 	label: "name",
+// };
 
-const AepsDistributor = () => {
-	const { slabs, serviceCode } = products.AEPS;
-	const { PERCENT, FIXED } =
-		productPricingCommissionValidationConfig.AEPS.DISTRIBUTOR;
+// const AepsDistributor = () => {
+// 	const { slabs, serviceCode } = products.AEPS;
+// 	const { PERCENT, FIXED } =
+// 		productPricingCommissionValidationConfig.AEPS.DISTRIBUTOR;
 
-	const {
-		handleSubmit,
-		register,
-		control,
-		formState: {
-			errors,
-			isValid,
-			isDirty,
-			isSubmitting,
-			isSubmitSuccessful,
-		},
-		reset,
-		trigger,
-	} = useForm({
-		mode: "onChange",
-		defaultValues: {
-			pricing_type: "1", //check if product details can store this
-		},
-	});
+// 	const {
+// 		handleSubmit,
+// 		register,
+// 		control,
+// 		formState: {
+// 			errors,
+// 			isValid,
+// 			isDirty,
+// 			isSubmitting,
+// 			isSubmitSuccessful,
+// 		},
+// 		reset,
+// 		trigger,
+// 	} = useForm({
+// 		mode: "onChange",
+// 		defaultValues: {
+// 			pricing_type: "1", //check if product details can store this
+// 		},
+// 	});
 
-	const watcher = useWatch({
-		control,
-	});
-	const toast = useToast();
-	const router = useRouter();
-	const { accessToken } = useSession();
-	const { generateNewToken } = useRefreshToken();
-	const [slabOptions, setSlabOptions] = useState([]);
-	const [multiSelectOptions, setMultiSelectOptions] = useState([]);
+// 	const watcher = useWatch({
+// 		control,
+// 	});
+// 	const toast = useToast();
+// 	const router = useRouter();
+// 	const { accessToken } = useSession();
+// 	const { generateNewToken } = useRefreshToken();
+// 	const [slabOptions, setSlabOptions] = useState([]);
+// 	const [multiSelectOptions, setMultiSelectOptions] = useState([]);
 
-	const min =
-		watcher["pricing_type"] === PRICING_TYPE.PERCENT
-			? PERCENT.min
-			: FIXED.min;
+// 	const min =
+// 		watcher["pricing_type"] === PRICING_TYPE.PERCENT
+// 			? PERCENT.min
+// 			: FIXED.min;
 
-	const max =
-		watcher["pricing_type"] === PRICING_TYPE.PERCENT
-			? PERCENT.max
-			: FIXED.max;
+// 	const max =
+// 		watcher["pricing_type"] === PRICING_TYPE.PERCENT
+// 			? PERCENT.max
+// 			: FIXED.max;
 
-	let prefix = "";
-	let suffix = "";
+// 	let prefix = "";
+// 	let suffix = "";
 
-	if (watcher["pricing_type"] === PRICING_TYPE.PERCENT) {
-		suffix = "%";
-	} else {
-		prefix = "₹";
-	}
+// 	if (watcher["pricing_type"] === PRICING_TYPE.PERCENT) {
+// 		suffix = "%";
+// 	} else {
+// 		prefix = "₹";
+// 	}
 
-	const aeps_distributor_parameter_list = [
-		{
-			name: "CspList",
-			label: "Select Distributor",
-			parameter_type_id: ParamType.LIST,
-			is_multi: true,
-			list_elements: multiSelectOptions,
-			multiSelectRenderer: _multiselectRenderer,
-		},
-		{
-			name: "select",
-			label: "Select Slab",
-			parameter_type_id: ParamType.LIST,
-			list_elements: slabOptions,
-			meta: {
-				force_dropdown: true,
-			},
-		},
-		{
-			name: "pricing_type",
-			label: `Select Commission Type`,
-			parameter_type_id: ParamType.LIST,
-			list_elements: pricing_type_list,
-			// defaultValue: PRICING_TYPE.PERCENT,
-		},
-		{
-			name: "actual_pricing",
-			label: `Define Commission (Exclusive of GST)`,
-			parameter_type_id: ParamType.NUMERIC, //ParamType.MONEY
-			helperText: `Minimum: ${prefix}${min}${suffix} - Maximum: ${prefix}${max}${suffix}`,
-			validations: {
-				// required: true,
-				min: min,
-				max: max,
-			},
-			inputRightElement: (
-				<Icon
-					name={
-						watcher["pricing_type"] == PRICING_TYPE.PERCENT
-							? "percent_bg"
-							: watcher["pricing_type"] == PRICING_TYPE.FIXED
-								? "rupee_bg"
-								: null
-					}
-					size="23px"
-					color="primary.DEFAULT"
-				/>
-			),
-		},
-	];
+// 	const aeps_distributor_parameter_list = [
+// 		{
+// 			name: "CspList",
+// 			label: "Select Distributor",
+// 			parameter_type_id: ParamType.LIST,
+// 			is_multi: true,
+// 			list_elements: multiSelectOptions,
+// 			multiSelectRenderer: _multiselectRenderer,
+// 		},
+// 		{
+// 			name: "select",
+// 			label: "Select Slab",
+// 			parameter_type_id: ParamType.LIST,
+// 			list_elements: slabOptions,
+// 			meta: {
+// 				force_dropdown: true,
+// 			},
+// 		},
+// 		{
+// 			name: "pricing_type",
+// 			label: `Select Commission Type`,
+// 			parameter_type_id: ParamType.LIST,
+// 			list_elements: pricing_type_list,
+// 			// defaultValue: PRICING_TYPE.PERCENT,
+// 		},
+// 		{
+// 			name: "actual_pricing",
+// 			label: `Define Commission (Exclusive of GST)`,
+// 			parameter_type_id: ParamType.NUMERIC, //ParamType.MONEY
+// 			helperText: `Minimum: ${prefix}${min}${suffix} - Maximum: ${prefix}${max}${suffix}`,
+// 			validations: {
+// 				// required: true,
+// 				min: min,
+// 				max: max,
+// 			},
+// 			inputRightElement: (
+// 				<Icon
+// 					name={
+// 						watcher["pricing_type"] == PRICING_TYPE.PERCENT
+// 							? "percent_bg"
+// 							: watcher["pricing_type"] == PRICING_TYPE.FIXED
+// 								? "rupee_bg"
+// 								: null
+// 					}
+// 					size="23px"
+// 					color="primary.DEFAULT"
+// 				/>
+// 			),
+// 		},
+// 	];
 
-	const buttonConfigList = [
-		{
-			type: "submit",
-			size: "lg",
-			label: "Save",
-			loading: isSubmitting,
-			disabled: !isValid || !isDirty,
-			styles: { h: "64px", w: { base: "100%", md: "200px" } },
-		},
-		{
-			variant: "link",
-			label: "Cancel",
-			onClick: () => router.back(),
-			styles: {
-				color: "primary.DEFAULT",
-				bg: { base: "white", md: "none" },
-				h: { base: "64px", md: "64px" },
-				w: { base: "100%", md: "auto" },
-				_hover: { textDecoration: "none" },
-			},
-		},
-	];
+// 	const buttonConfigList = [
+// 		{
+// 			type: "submit",
+// 			size: "lg",
+// 			label: "Save",
+// 			loading: isSubmitting,
+// 			disabled: !isValid || !isDirty,
+// 			styles: { h: "64px", w: { base: "100%", md: "200px" } },
+// 		},
+// 		{
+// 			variant: "link",
+// 			label: "Cancel",
+// 			onClick: () => router.back(),
+// 			styles: {
+// 				color: "primary.DEFAULT",
+// 				bg: { base: "white", md: "none" },
+// 				h: { base: "64px", md: "64px" },
+// 				w: { base: "100%", md: "auto" },
+// 				_hover: { textDecoration: "none" },
+// 			},
+// 		},
+// 	];
 
-	useEffect(() => {
-		const list = [];
+// 	useEffect(() => {
+// 		const list = [];
 
-		slabs.map((item, index) => {
-			const temp = { value: `${index}` };
+// 		slabs.map((item, index) => {
+// 			const temp = { value: `${index}` };
 
-			const label =
-				item.min == item.max
-					? `₹${item.min}`
-					: `₹${item.min} - ₹${item.max}`;
+// 			const label =
+// 				item.min == item.max
+// 					? `₹${item.min}`
+// 					: `₹${item.min} - ₹${item.max}`;
 
-			list.push({ ...temp, label });
-		});
+// 			list.push({ ...temp, label });
+// 		});
 
-		setSlabOptions(list);
-	}, []);
+// 		setSlabOptions(list);
+// 	}, []);
 
-	useEffect(() => {
-		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
-			headers: {
-				"tf-req-uri-root-path": "/ekoicici/v1",
-				"tf-req-uri": "/network/agent-list?usertype=1",
-				"tf-req-method": "GET",
-			},
-			token: accessToken,
-			generateNewToken,
-		})
-			.then((res) => {
-				const _agents = res?.data?.csp_list ?? [];
-				setMultiSelectOptions(_agents);
-			})
-			.catch((error) => {
-				console.error("📡Error:", error);
-			});
-	}, []);
+// 	useEffect(() => {
+// 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
+// 			headers: {
+// 				"tf-req-uri-root-path": "/ekoicici/v1",
+// 				"tf-req-uri": "/network/agent-list?usertype=1",
+// 				"tf-req-method": "GET",
+// 			},
+// 			token: accessToken,
+// 			generateNewToken,
+// 		})
+// 			.then((res) => {
+// 				const _agents = res?.data?.csp_list ?? [];
+// 				setMultiSelectOptions(_agents);
+// 			})
+// 			.catch((error) => {
+// 				console.error("📡Error:", error);
+// 			});
+// 	}, []);
 
-	useEffect(() => {
-		if (isSubmitSuccessful) {
-			reset(watcher);
-		}
-		if (isDirty && !isValid) {
-			trigger();
-		}
-	}, [isSubmitSuccessful, isValid]);
+// 	useEffect(() => {
+// 		if (isSubmitSuccessful) {
+// 			reset(watcher);
+// 		}
+// 		if (isDirty && !isValid) {
+// 			trigger();
+// 		}
+// 	}, [isSubmitSuccessful, isValid]);
 
-	const handleFormSubmit = (data) => {
-		const _finalData = { ...data };
+// 	const handleFormSubmit = (data) => {
+// 		const _finalData = { ...data };
 
-		const { min, max } = slabs[data?.select?.value] || {};
-		_finalData.min_slab_amount = min;
-		_finalData.max_slab_amount = max;
+// 		const { min, max } = slabs[data?.select?.value] || {};
+// 		_finalData.min_slab_amount = min;
+// 		_finalData.max_slab_amount = max;
 
-		const _CspList = data?.CspList?.map(
-			(item) => item[_multiselectRenderer.value]
-		);
+// 		const _CspList = data?.CspList?.map(
+// 			(item) => item[_multiselectRenderer.value]
+// 		);
 
-		if (watcher.operation_type != 3) {
-			_finalData.CspList = `${_CspList}`;
-		}
+// 		if (watcher.operation_type != 3) {
+// 			_finalData.CspList = `${_CspList}`;
+// 		}
 
-		delete _finalData.select;
+// 		delete _finalData.select;
 
-		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
-			body: {
-				interaction_type_id:
-					TransactionTypes.SET_COMMISSION_FOR_DISTRIBUTORS,
-				service_code: serviceCode,
-				communication: 1,
-				..._finalData,
-			},
-			token: accessToken,
-			generateNewToken,
-		})
-			.then((res) => {
-				toast({
-					title: res.message,
-					status: getStatus(res.status),
-					duration: 6000,
-					isClosable: true,
-				});
-			})
-			.catch((err) => {
-				console.error("error", err);
-			});
-	};
-	return (
-		<form onSubmit={handleSubmit(handleFormSubmit)}>
-			<Flex direction="column" gap="8">
-				<Form
-					{...{
-						parameter_list: aeps_distributor_parameter_list,
-						formValues: watcher,
-						control,
-						register,
-						errors,
-					}}
-				/>
-				<ActionButtonGroup {...{ buttonConfigList }} />
-			</Flex>
-		</form>
-	);
-};
+// 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
+// 			body: {
+// 				interaction_type_id:
+// 					TransactionTypes.SET_COMMISSION_FOR_DISTRIBUTORS,
+// 				service_code: serviceCode,
+// 				communication: 1,
+// 				..._finalData,
+// 			},
+// 			token: accessToken,
+// 			generateNewToken,
+// 		})
+// 			.then((res) => {
+// 				toast({
+// 					title: res.message,
+// 					status: getStatus(res.status),
+// 					duration: 6000,
+// 					isClosable: true,
+// 				});
+// 			})
+// 			.catch((err) => {
+// 				console.error("error", err);
+// 			});
+// 	};
+// 	return (
+// 		<form onSubmit={handleSubmit(handleFormSubmit)}>
+// 			<Flex direction="column" gap="8">
+// 				<Form
+// 					{...{
+// 						parameter_list: aeps_distributor_parameter_list,
+// 						formValues: watcher,
+// 						control,
+// 						register,
+// 						errors,
+// 					}}
+// 				/>
+// 				<ActionButtonGroup {...{ buttonConfigList }} />
+// 			</Flex>
+// 		</form>
+// 	);
+// };
 
-export default AepsDistributor;
+// export default AepsDistributor;
--- page-components/Admin/PricingCommission/ValidateUpiId.jsx
+++ page-components/Admin/PricingCommission/ValidateUpiId.jsx
@@ -1,272 +1,272 @@
-import { Flex, useToast } from "@chakra-ui/react";
-import { ActionButtonGroup, Icon } from "components";
-import {
-	Endpoints,
-	ParamType,
-	productPricingCommissionValidationConfig,
-	productPricingType,
-	products,
-} from "constants";
-import { useSession } from "contexts";
-import { fetcher } from "helpers";
-import { useRefreshToken } from "hooks";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useForm, useWatch } from "react-hook-form";
-import { Form } from "tf-components";
+// import { Flex, useToast } from "@chakra-ui/react";
+// import { ActionButtonGroup, Icon } from "components";
+// import {
+// 	Endpoints,
+// 	ParamType,
+// 	productPricingCommissionValidationConfig,
+// 	productPricingType,
+// 	products,
+// } from "constants";
+// import { useSession } from "contexts";
+// import { fetcher } from "helpers";
+// import { useRefreshToken } from "hooks";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import { useForm, useWatch } from "react-hook-form";
+// import { Form } from "tf-components";
 
-const OPERATION = {
-	VERIFICATION_SUBMIT: 2,
-	SUBMIT: 1,
-	FETCH: 0,
-};
+// const OPERATION = {
+// 	VERIFICATION_SUBMIT: 2,
+// 	SUBMIT: 1,
+// 	FETCH: 0,
+// };
 
-const operation_type_list = [
-	{ value: "3", label: "Whole Network" },
-	{ value: "2", label: "Distributor's Network" },
-	{ value: "1", label: "Individual Distributor/Retailer" },
-];
+// const operation_type_list = [
+// 	{ value: "3", label: "Whole Network" },
+// 	{ value: "2", label: "Distributor's Network" },
+// 	{ value: "1", label: "Individual Distributor/Retailer" },
+// ];
 
-const PRICING_TYPE = {
-	PERCENT: "0",
-	FIXED: "1",
-};
+// const PRICING_TYPE = {
+// 	PERCENT: "0",
+// 	FIXED: "1",
+// };
 
-const _multiselectRenderer = {
-	value: "user_code",
-	label: "name",
-};
+// const _multiselectRenderer = {
+// 	value: "user_code",
+// 	label: "name",
+// };
 
-const getStatus = (status) => {
-	switch (status) {
-		case 0:
-			return "success";
-		default:
-			return "error";
-	}
-};
+// const getStatus = (status) => {
+// 	switch (status) {
+// 		case 0:
+// 			return "success";
+// 		default:
+// 			return "error";
+// 	}
+// };
 
-const pricing_type_list = [
-	// { value: PRICING_TYPE.PERCENT, label: "Percentage (%)" },
-	{ value: PRICING_TYPE.FIXED, label: "Fixed (₹)" },
-];
+// const pricing_type_list = [
+// 	// { value: PRICING_TYPE.PERCENT, label: "Percentage (%)" },
+// 	{ value: PRICING_TYPE.FIXED, label: "Fixed (₹)" },
+// ];
 
-const ValidateUpiId = () => {
-	const { serviceCode, DEFAULT } = products.VALIDATE_UPI_ID;
-	const { FIXED } = productPricingCommissionValidationConfig.VALIDATE_UPI_ID;
-	const {
-		handleSubmit,
-		register,
-		control,
-		formState: {
-			errors,
-			isValid,
-			isDirty,
-			isSubmitting,
-			isSubmitSuccessful,
-		},
-		reset,
-		trigger,
-	} = useForm({
-		mode: "onChange",
-		defaultValues: {
-			operation_type: DEFAULT.operation_type,
-			pricing_type: DEFAULT.pricing_type,
-		},
-	});
+// const ValidateUpiId = () => {
+// 	const { serviceCode, DEFAULT } = products.VALIDATE_UPI_ID;
+// 	const { FIXED } = productPricingCommissionValidationConfig.VALIDATE_UPI_ID;
+// 	const {
+// 		handleSubmit,
+// 		register,
+// 		control,
+// 		formState: {
+// 			errors,
+// 			isValid,
+// 			isDirty,
+// 			isSubmitting,
+// 			isSubmitSuccessful,
+// 		},
+// 		reset,
+// 		trigger,
+// 	} = useForm({
+// 		mode: "onChange",
+// 		defaultValues: {
+// 			operation_type: DEFAULT.operation_type,
+// 			pricing_type: DEFAULT.pricing_type,
+// 		},
+// 	});
 
-	const toast = useToast();
-	const { accessToken } = useSession();
-	const router = useRouter();
-	const { generateNewToken } = useRefreshToken();
+// 	const toast = useToast();
+// 	const { accessToken } = useSession();
+// 	const router = useRouter();
+// 	const { generateNewToken } = useRefreshToken();
 
-	const [multiSelectLabel, setMultiSelectLabel] = useState();
-	const [multiSelectOptions, setMultiSelectOptions] = useState([]);
+// 	const [multiSelectLabel, setMultiSelectLabel] = useState();
+// 	const [multiSelectOptions, setMultiSelectOptions] = useState([]);
 
-	const watcher = useWatch({
-		control,
-	});
+// 	const watcher = useWatch({
+// 		control,
+// 	});
 
-	useEffect(() => {
-		if (isSubmitSuccessful) {
-			reset(watcher);
-		}
-		if (isDirty && !isValid) {
-			trigger();
-		}
-	}, [isSubmitSuccessful, isValid]);
+// 	useEffect(() => {
+// 		if (isSubmitSuccessful) {
+// 			reset(watcher);
+// 		}
+// 		if (isDirty && !isValid) {
+// 			trigger();
+// 		}
+// 	}, [isSubmitSuccessful, isValid]);
 
-	useEffect(() => {
-		if (watcher.operation_type && watcher.operation_type != "3") {
-			/* no need of api call when user clicked on product radio option in select_commission_for field as multiselect option is hidden for this */
+// 	useEffect(() => {
+// 		if (watcher.operation_type && watcher.operation_type != "3") {
+// 			/* no need of api call when user clicked on product radio option in select_commission_for field as multiselect option is hidden for this */
 
-			const _tf_req_uri =
-				watcher.operation_type === "2"
-					? "/network/agent-list?usertype=1"
-					: "/network/agent-list";
+// 			const _tf_req_uri =
+// 				watcher.operation_type === "2"
+// 					? "/network/agent-list?usertype=1"
+// 					: "/network/agent-list";
 
-			fetcher(
-				process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION,
-				{
-					headers: {
-						"tf-req-uri-root-path": "/ekoicici/v1",
-						"tf-req-uri": `${_tf_req_uri}`,
-						"tf-req-method": "GET",
-					},
-					token: accessToken,
-					generateNewToken,
-				}
-			)
-				.then((res) => {
-					const _agents = res?.data?.csp_list ?? [];
-					setMultiSelectOptions(_agents);
-				})
-				.catch((error) => {
-					console.error("📡Error:", error);
-				});
+// 			fetcher(
+// 				process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION,
+// 				{
+// 					headers: {
+// 						"tf-req-uri-root-path": "/ekoicici/v1",
+// 						"tf-req-uri": `${_tf_req_uri}`,
+// 						"tf-req-method": "GET",
+// 					},
+// 					token: accessToken,
+// 					generateNewToken,
+// 				}
+// 			)
+// 				.then((res) => {
+// 					const _agents = res?.data?.csp_list ?? [];
+// 					setMultiSelectOptions(_agents);
+// 				})
+// 				.catch((error) => {
+// 					console.error("📡Error:", error);
+// 				});
 
-			let _operationTypeList = operation_type_list.filter(
-				(item) => item.value == watcher.operation_type
-			);
-			let _label =
-				_operationTypeList.length > 0 && _operationTypeList[0].label;
+// 			let _operationTypeList = operation_type_list.filter(
+// 				(item) => item.value == watcher.operation_type
+// 			);
+// 			let _label =
+// 				_operationTypeList.length > 0 && _operationTypeList[0].label;
 
-			setMultiSelectLabel(_label);
-		}
-	}, [watcher.operation_type]);
+// 			setMultiSelectLabel(_label);
+// 		}
+// 	}, [watcher.operation_type]);
 
-	const min = FIXED.min;
+// 	const min = FIXED.min;
 
-	const max = FIXED.max;
+// 	const max = FIXED.max;
 
-	const prefix = "₹";
+// 	const prefix = "₹";
 
-	const validate_upi_pricing_parameter_list = [
-		{
-			name: "operation_type",
-			label: `Set Pricing For`,
-			parameter_type_id: ParamType.LIST,
-			list_elements: operation_type_list,
-			defaultValue: DEFAULT.operation_type,
-		},
-		{
-			name: "CspList",
-			label: `Select ${multiSelectLabel}`,
-			parameter_type_id: ParamType.LIST,
-			is_multi: true,
-			list_elements: multiSelectOptions,
-			visible_on_param_name: "operation_type",
-			visible_on_param_value: /1|2/,
-			multiSelectRenderer: _multiselectRenderer,
-		},
-		{
-			name: "pricing_type",
-			label: `Select ${productPricingType.ACCOUNT_VERIFICATION} Type`,
-			parameter_type_id: ParamType.LIST,
-			list_elements: pricing_type_list,
-			defaultValue: DEFAULT.pricing_type,
-		},
-		{
-			name: "actual_pricing",
-			label: `Define ${productPricingType.ACCOUNT_VERIFICATION} (GST Inclusive)`,
-			parameter_type_id: ParamType.NUMERIC, //ParamType.MONEY
-			helperText: `Minimum: ${prefix}${min} - Maximum: ${prefix}${max}`,
-			validations: {
-				// required: true,
-				min: min,
-				max: max,
-			},
-			inputRightElement: (
-				<Icon
-					name={
-						watcher["pricing_type"] == PRICING_TYPE.PERCENT
-							? "percent_bg"
-							: watcher["pricing_type"] == PRICING_TYPE.FIXED
-								? "rupee_bg"
-								: null
-					}
-					size="23px"
-					color="primary.DEFAULT"
-				/>
-			),
-		},
-	];
+// 	const validate_upi_pricing_parameter_list = [
+// 		{
+// 			name: "operation_type",
+// 			label: `Set Pricing For`,
+// 			parameter_type_id: ParamType.LIST,
+// 			list_elements: operation_type_list,
+// 			defaultValue: DEFAULT.operation_type,
+// 		},
+// 		{
+// 			name: "CspList",
+// 			label: `Select ${multiSelectLabel}`,
+// 			parameter_type_id: ParamType.LIST,
+// 			is_multi: true,
+// 			list_elements: multiSelectOptions,
+// 			visible_on_param_name: "operation_type",
+// 			visible_on_param_value: /1|2/,
+// 			multiSelectRenderer: _multiselectRenderer,
+// 		},
+// 		{
+// 			name: "pricing_type",
+// 			label: `Select ${productPricingType.ACCOUNT_VERIFICATION} Type`,
+// 			parameter_type_id: ParamType.LIST,
+// 			list_elements: pricing_type_list,
+// 			defaultValue: DEFAULT.pricing_type,
+// 		},
+// 		{
+// 			name: "actual_pricing",
+// 			label: `Define ${productPricingType.ACCOUNT_VERIFICATION} (GST Inclusive)`,
+// 			parameter_type_id: ParamType.NUMERIC, //ParamType.MONEY
+// 			helperText: `Minimum: ${prefix}${min} - Maximum: ${prefix}${max}`,
+// 			validations: {
+// 				// required: true,
+// 				min: min,
+// 				max: max,
+// 			},
+// 			inputRightElement: (
+// 				<Icon
+// 					name={
+// 						watcher["pricing_type"] == PRICING_TYPE.PERCENT
+// 							? "percent_bg"
+// 							: watcher["pricing_type"] == PRICING_TYPE.FIXED
+// 								? "rupee_bg"
+// 								: null
+// 					}
+// 					size="23px"
+// 					color="primary.DEFAULT"
+// 				/>
+// 			),
+// 		},
+// 	];
 
-	const buttonConfigList = [
-		{
-			type: "submit",
-			size: "lg",
-			label: "Save",
-			loading: isSubmitting,
-			disabled: !isValid || !isDirty,
-			styles: { h: "64px", w: { base: "100%", md: "200px" } },
-		},
-		{
-			variant: "link",
-			label: "Cancel",
-			onClick: () => router.back(),
-			styles: {
-				color: "primary.DEFAULT",
-				bg: { base: "white", md: "none" },
-				h: { base: "64px", md: "64px" },
-				w: { base: "100%", md: "auto" },
-				_hover: { textDecoration: "none" },
-			},
-		},
-	];
+// 	const buttonConfigList = [
+// 		{
+// 			type: "submit",
+// 			size: "lg",
+// 			label: "Save",
+// 			loading: isSubmitting,
+// 			disabled: !isValid || !isDirty,
+// 			styles: { h: "64px", w: { base: "100%", md: "200px" } },
+// 		},
+// 		{
+// 			variant: "link",
+// 			label: "Cancel",
+// 			onClick: () => router.back(),
+// 			styles: {
+// 				color: "primary.DEFAULT",
+// 				bg: { base: "white", md: "none" },
+// 				h: { base: "64px", md: "64px" },
+// 				w: { base: "100%", md: "auto" },
+// 				_hover: { textDecoration: "none" },
+// 			},
+// 		},
+// 	];
 
-	const handleFormSubmit = (data) => {
-		const _finalData = { ...data };
+// 	const handleFormSubmit = (data) => {
+// 		const _finalData = { ...data };
 
-		const _CspList = data?.CspList?.map(
-			(item) => item[_multiselectRenderer.value]
-		);
+// 		const _CspList = data?.CspList?.map(
+// 			(item) => item[_multiselectRenderer.value]
+// 		);
 
-		if (watcher.operation_type == "3") {
-			delete _finalData.CspList;
-		} else {
-			_finalData.CspList = `${_CspList}`;
-		}
+// 		if (watcher.operation_type == "3") {
+// 			delete _finalData.CspList;
+// 		} else {
+// 			_finalData.CspList = `${_CspList}`;
+// 		}
 
-		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
-			body: {
-				interaction_type_id: 754,
-				service_code: serviceCode,
-				operation: OPERATION.SUBMIT,
-				..._finalData,
-			},
-			token: accessToken,
-		})
-			.then((res) => {
-				toast({
-					title: res.message,
-					status: getStatus(res.status),
-					duration: 5000,
-					isClosable: true,
-				});
-			})
-			.catch((err) => {
-				console.error("error", err);
-			});
-	};
+// 		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
+// 			body: {
+// 				interaction_type_id: 754,
+// 				service_code: serviceCode,
+// 				operation: OPERATION.SUBMIT,
+// 				..._finalData,
+// 			},
+// 			token: accessToken,
+// 		})
+// 			.then((res) => {
+// 				toast({
+// 					title: res.message,
+// 					status: getStatus(res.status),
+// 					duration: 5000,
+// 					isClosable: true,
+// 				});
+// 			})
+// 			.catch((err) => {
+// 				console.error("error", err);
+// 			});
+// 	};
 
-	return (
-		<form onSubmit={handleSubmit(handleFormSubmit)}>
-			<Flex direction="column" gap="8">
-				<Form
-					{...{
-						parameter_list: validate_upi_pricing_parameter_list,
-						register,
-						control,
-						formValues: watcher,
-						errors,
-					}}
-				/>
+// 	return (
+// 		<form onSubmit={handleSubmit(handleFormSubmit)}>
+// 			<Flex direction="column" gap="8">
+// 				<Form
+// 					{...{
+// 						parameter_list: validate_upi_pricing_parameter_list,
+// 						register,
+// 						control,
+// 						formValues: watcher,
+// 						errors,
+// 					}}
+// 				/>
 
-				<ActionButtonGroup {...{ buttonConfigList }} />
-			</Flex>
-		</form>
-	);
-};
+// 				<ActionButtonGroup {...{ buttonConfigList }} />
+// 			</Flex>
+// 		</form>
+// 	);
+// };
 
-export { ValidateUpiId };
+// export { ValidateUpiId };
