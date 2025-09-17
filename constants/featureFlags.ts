151a2c4ddd689fa62508ceca85c0aaa917b3c2b3
Kumar Abhishek <manustays@users.noreply.github.com>
2025-09-17 08:24:03 +0530

    feat: launch admin-dashboard-for-subnetwork-owners for all distributors & super-distributors

--- constants/featureFlags.ts
+++ constants/featureFlags.ts
@@ -43,22 +43,6 @@ export const FeatureFlags: Record<string, FeatureFlagType> = {
 		},
 	},
 
-	// Show Admin-like dashboard to other sub-network owners like (Super)Distributor
-	ADMIN_DASHBOARD_FOR_SUBNETWORK: {
-		enabled: true,
-		forUserType: [1, 7], // 7 = SuperDistributor, 4 = FOS
-		// forEnv: ["development", "staging"],
-		envConstraints: {
-			production: {
-				forOrgId: [
-					ORG_ID.EKOSTORE,
-					...ORG_ID.EKOTESTS,
-					...ORG_ID.DASHBOARD_V2,
-				],
-			},
-		},
-	},
-
 	// Inventory Management for (Super)Distributors
 	INVENTORY_MANAGEMENT_FOR_SUBNETWORK: {
 		enabled: true,
@@ -88,6 +72,13 @@ export const FeatureFlags: Record<string, FeatureFlagType> = {
 	// MARK: 🚩BETA Flags
 	// Feature Enabled only for certain orgs/users in production
 	// Put all UAT/Beta testing flags in this section.
+
+	// Show Admin-like (business) dashboard to other sub-network owners like (Super)Distributor
+	// TODO: Rename to ADMIN_BUSINESS_DASHBOARD_FOR_SUBNETWORK & introduce another flag for ADMIN_ONBOARDING_DASHBOARD_FOR_SUBNETWORK
+	ADMIN_DASHBOARD_FOR_SUBNETWORK: {
+		enabled: true,
+		forUserType: [1, 7], // 1 = Dist, 7 = SuperDistributor, 4 = FOS
+	},
 
 	// Feature to Raise Generic Issues (from Top-Right  Menu)...
 	RAISE_ISSUE_GENERIC: {
@@ -170,23 +161,15 @@ export const FeatureFlags: Record<string, FeatureFlagType> = {
 		},
 	},
 
-	// New Dashboard Features (Graphs & updated UI)
-	DASHBOARD_V2: {
-		enabled: true,
-		envConstraints: {
-			production: {
-				forOrgId: [
-					ORG_ID.EKOSTORE,
-					...ORG_ID.EKOTESTS,
-					...ORG_ID.DASHBOARD_V2,
-				],
-			},
-		},
-	},
-
 	// ------------------------------------------------------------------------
 	// MARK: 🚩Production Flags
 	// Put all production flags (visible to all relevant users) in this section.
+
+	// New Dashboard Features (Graphs & updated UI)
+	// TODO: Remove this flag after the new dashboard is fully rolled out to all users.
+	DASHBOARD_V2: {
+		enabled: true,
+	},
 
 	// Open ChatGPT Agent in new tab
 	// To answer Admin's queries related to Eloka products and features.
