18ce3258ed9769b6d4ee90dfb8ea72fb43309920
shubhmjain30 <shubhmjain30@gmail.com>
2025-09-22 16:11:41 +0530

    refactor: replace fetch with dataURLtoBlob for image capture
    - Updated the image capture logic in the Camera component to use the
      dataURLtoBlob utility function instead of fetching the image source.
    - This change improves performance and simplifies the code by removing
      the need for an additional network request.

--- components/Camera/Camera.tsx
+++ components/Camera/Camera.tsx
@@ -5,6 +5,7 @@ import { useImageEditor } from "hooks";
 import React, { useCallback, useRef, useState } from "react";
 import { MdCamera, MdCameraswitch, MdClose } from "react-icons/md";
 import Webcam from "react-webcam";
+import { dataURLtoBlob } from "utils";
 import {
 	FACING_MODE_ENVIRONMENT,
 	FACING_MODE_USER,
@@ -269,7 +270,7 @@ const Camera = ({
 	 */
 	const capture = useCallback(async () => {
 		const imageSrc = webcamRef?.current?.getScreenshot();
-		const blob = await fetch(imageSrc).then((res) => res.blob());
+		const blob = dataURLtoBlob(imageSrc);
 		const timestamp = new Date().toLocaleString().replace(/[^0-9]+/g, "_");
 		const filename = `Cam_${timestamp}.${blob.type.split("/")[1] || "jpg"}`;
 		const imageFile = new File([blob], filename, { type: blob.type });
