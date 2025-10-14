306e061ace720d80867513287c5019baf7937859
shubhmjain30 <shubhmjain30@gmail.com>
2025-10-14 16:37:05 +0530

    refactor: remove unused useFormSubmission hook
    - The useFormSubmission hook has been removed as it was no longer needed
      in the onboarding process. This helps to clean up the codebase and
      reduce unnecessary complexity.

--- components/OnboardingWidget/hooks/index.ts
+++ components/OnboardingWidget/hooks/index.ts
@@ -2,7 +2,6 @@ export { useAndroidIntegration } from "./useAndroidIntegration";
 export { useDigilockerApi } from "./useDigilockerApi";
 export { useEsignIntegration } from "./useEsignIntegration";
 export { useFileUpload } from "./useFileUpload";
-export { useFormSubmission } from "./useFormSubmission";
 export { useKycFormSubmission } from "./useKycFormSubmission";
 export { useOnboardingApiSubmission } from "./useOnboardingApiSubmission";
 export { useOnboardingState } from "./useOnboardingState";
