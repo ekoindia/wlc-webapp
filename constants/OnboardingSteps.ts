d38e1a566419ccb012180d04db9adbf96123b0e2
shubhmjain30 <shubhmjain30@gmail.com>
2025-11-05 10:49:53 +0530

    ui: update primary CTA text for distributor steps
    - Changed the primary call-to-action text from "Start Location Capture"
      to "Capture Location" to enhance clarity and improve user experience
      during the onboarding process.

--- constants/OnboardingSteps.ts
+++ constants/OnboardingSteps.ts
@@ -242,7 +242,7 @@ export const distributorStepsData: OnboardingStep[] = [
 		isVisible: true,
 		stepStatus: 0,
 		role: 13000,
-		primaryCTAText: "Start Location Capture",
+		primaryCTAText: "Capture Location ",
 		description: "",
 		form_data: {},
 		success_message: "Location captured successfully.",
