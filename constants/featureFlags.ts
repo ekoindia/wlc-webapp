2d2d791e62f64fbb292efcab608a06cdc14706af
shubhmjain30 <shubhmjain30@gmail.com>
2025-12-11 19:40:44 +0530

    chore: comment out Satin Prod feature flag in featureFlags.ts
    - Temporarily disable the Satin Prod feature flag by commenting it out.
    - This change is made to prevent unintended access to the feature during
      the current deployment phase.

--- constants/featureFlags.ts
+++ constants/featureFlags.ts
@@ -39,6 +39,7 @@ export const FeatureFlags: Record<string, FeatureFlagType> = {
 					ORG_ID.EKOSTORE,
 					...ORG_ID.EKOTESTS,
 					...ORG_ID.DASHBOARD_V2,
+					// 3, // Satin Prod
 				],
 			},
 		},
