a9e47450a04fa97d3fb3e6ea75d8836d6e2265c8
Kumar Abhishek <manustays@users.noreply.github.com>
2026-01-28 16:43:09 +0530

    feat: fetch and display user's GSTIN in AccountSummary component
    - Implemented fetching of GSTIN using the API and updated the state
      accordingly. This allows users to see their GSTIN directly in the
      AccountSummary component.
    - Adjusted the rendering logic to ensure the widget only displays if
      GSTIN or connected bank accounts are available.

--- page-components/Profile/AccountSummary/AccountSummary.jsx
+++ page-components/Profile/AccountSummary/AccountSummary.jsx
@@ -1,19 +1,48 @@
 import { Box, Divider, Flex, Text } from "@chakra-ui/react";
-import { useUser } from "contexts/UserContext";
+import { Endpoints } from "constants/EndPoints";
+import { useSession } from "contexts/UserContext";
 import { useWallet } from "contexts/WalletContext";
+import { fetcher } from "helpers";
 import { WidgetBase } from "page-components/Home";
+import { useEffect, useState } from "react";
 
 /**
  * A AccountSummary page-component
  */
 const AccountSummary = () => {
-	const { userData, isAdmin } = useUser();
-	const { accountDetails } = userData || {};
+	const { isAdmin, accessToken } = useSession();
 	const { accountList } = useWallet();
-	const bankAccounts = accountList?.filter((acc) => acc.type_id == 3);
+
+	const [gstin, setGstin] = useState("");
+
+	// Fetch user's GSTIN
+	useEffect(() => {
+		fetcher(process.env.NEXT_PUBLIC_API_BASE_URL + Endpoints.TRANSACTION, {
+			token: accessToken,
+			body: {
+				interaction_type_id: 373, // Get GSTIN Details
+			},
+			controller: undefined,
+		})
+			.then((res) => {
+				if (res?.data?.gstin) {
+					setGstin(res.data.gstin);
+				}
+			})
+			.catch((error) => {
+				console.error("[AccountSummary] Get GSTIN Error:", error);
+			});
+	}, []);
 
 	// TODO: Allow for Non-Admins as well
 	if (!isAdmin) {
+		return null;
+	}
+
+	const bankAccounts = accountList?.filter((acc) => acc.type_id == 3);
+
+	// If no GSTIN and no bank accounts, don't show the widget
+	if (!gstin && (!bankAccounts || bankAccounts.length === 0)) {
 		return null;
 	}
 
@@ -23,22 +52,25 @@ const AccountSummary = () => {
 			// iconName="mode-edit"
 			// linkOnClick={() => onOpen()}
 		>
-			<Flex direction="column" fontSize="sm">
-				<Text>GSTIN</Text>
-				<Text
-					fontSize="md"
-					fontWeight="semibold"
-					color="primary.DEFAULT"
-				>
-					{accountDetails?.gst || "N/A"}
-				</Text>
-			</Flex>
-
-			<Divider my="14px" />
+			{gstin ? (
+				<>
+					<Flex direction="column" fontSize="sm">
+						<Text>GSTIN</Text>
+						<Text
+							fontSize="md"
+							fontWeight="semibold"
+							color="primary.DEFAULT"
+						>
+							{gstin}
+						</Text>
+					</Flex>
+					<Divider my="14px" />
+				</>
+			) : null}
 
 			{bankAccounts && bankAccounts.length > 0 ? (
 				<Flex direction="column">
-					<Text>Connected Bank Accounts</Text>
+					<Text>Connected Bank Account(s)</Text>
 					<Box
 						fontSize="md"
 						fontWeight="semibold"
