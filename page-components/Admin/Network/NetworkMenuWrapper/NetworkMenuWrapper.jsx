2ebd5590e55fd20e5f028106d367254e0f91423a
Mohit Madaan <mohit.madaan@eko.co.in>
2026-02-18 16:44:00 +0000

    fix: delete demo user

--- page-components/Admin/Network/NetworkMenuWrapper/NetworkMenuWrapper.jsx
+++ page-components/Admin/Network/NetworkMenuWrapper/NetworkMenuWrapper.jsx
@@ -168,15 +168,15 @@ const NetworkMenuWrapper = ({
 				process.env.NEXT_PUBLIC_API_BASE_URL +
 					Endpoints.TRANSACTION_JSON,
 				{
-					method: "POST",
 					headers: {
-						"Content-Type": "application/json",
 						"tf-req-uri-root-path": "/ekoicici/v1",
 						"tf-req-uri": `/network/agent`,
-						"tf-req-method": "DELETE",
+						"tf-req-method": "POST",
 					},
 					body: {
 						csp_code: eko_code,
+						source: "WLC",
+						version: "v1",
 					},
 					token: accessToken,
 				}
