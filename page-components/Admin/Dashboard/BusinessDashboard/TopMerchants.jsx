be10c272c2926ae6aca852b10fd68d3bedfb8a34
Kumar Abhishek <manustays@users.noreply.github.com>
2026-02-22 22:30:23 +0530

    Update TopMerchants.jsx

--- page-components/Admin/Dashboard/BusinessDashboard/TopMerchants.jsx
+++ page-components/Admin/Dashboard/BusinessDashboard/TopMerchants.jsx
@@ -159,9 +159,9 @@ const TopMerchants = ({
 	}, [topMerchantsData, totalBusiness]);
 
 	/**
-	 * Filter product list based on available products from earning overview cache
+	 * Filter product list based on available products from earning overview cache.
+	 * TODO: Improve/refactor this logic. This part seems duplicate which is being repeated in multiple components where we are showing product filter. Seems too much code to produce a cached list of products to filter from.
 	 */
-	// TODO: Improve/refactor this logic. This part seems duplicate which is being repeated in multiple components where we are showing product filter. Seems too much code to produce a cached list of products to filter from.
 	useEffect(() => {
 		if (!dateFrom || !dateTo) return;
 
